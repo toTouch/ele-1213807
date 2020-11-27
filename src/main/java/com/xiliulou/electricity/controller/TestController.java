39c28cc7ce9d0cd4a50e797dc06c4297369d0b49
Eclair <1269029723@qq.com>
2020-11-27 11:21:36 +0800

    用户登录

--- src/main/java/com/xiliulou/electricity/controller/TestController.java
+++ src/main/java/com/xiliulou/electricity/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.xiliulou.electricity.controller;
 
+import com.xiliulou.electricity.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,8 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+	@Autowired
+	TestService testService;
+
 	@GetMapping("/test/hello")
 	public String getSTrintg() {
+		return "nihao";
+	}
+
+	@GetMapping("/test/lock")
+	public String name(@RequestParam("test") String test) {
+		System.out.println(test);
+		testService.test(test);
 		return "nihao";
 	}
 }
---
+++ src/main/java/com/xiliulou/electricity/token/config/TokenWebSecurityConfig.java
@@ -0,0 +1,76 @@
+package com.xiliulou.electricity.token.config;
+
+import com.xiliulou.security.component.CustomAccessDeniedHandler;
+import com.xiliulou.security.component.CustomAuthenticationEntryPoint;
+import com.xiliulou.security.component.CustomPasswordEncoder;
+import com.xiliulou.security.component.CustomUsernamePasswordAuthenticationFilter;
+import com.xiliulou.security.component.JwtTokenManager;
+import com.xiliulou.security.component.TokenLogoutHandler;
+import com.xiliulou.security.config.TokenConfig;
+import com.xiliulou.security.service.CustomUserDetailServiceImpl;
+import com.xiliulou.security.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author: eclair
+ * @Date: 2020/11/25 10:48
+ * @Description:
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class TokenWebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Qualifier("userDetailServiceImpl")
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new CustomPasswordEncoder();
+	}
+
+	@Bean
+	public JwtTokenManager jwtTokenManager() {
+		return new JwtTokenManager();
+	}
+
+	@Bean
+	public TokenConfig tokenConfig() {
+		return new TokenConfig();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+				.accessDeniedHandler(new CustomAccessDeniedHandler())
+				.and().csrf().disable()
+				.authorizeRequests()
+				.antMatchers("/auth/token/**", "/actuator/**", "/error").permitAll()
+				.anyRequest().authenticated()
+				.and().logout().logoutUrl("/auth/token/logout")
+				.addLogoutHandler(new TokenLogoutHandler())
+				.and().httpBasic().and().addFilter(new CustomUsernamePasswordAuthenticationFilter(jwtTokenManager(), authenticationManager()));
+
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/css/**");
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+}
