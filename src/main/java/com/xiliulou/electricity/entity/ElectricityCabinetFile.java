4adbb63295a9875ea48e9d324eb2e62c4fa522f5
liXueChun <2628585226@qq.com>
2020-11-30 10:47:57 +0800

    测试

--- src/main/java/com/xiliulou/electricity/entity/ElectricityCabinetFile.java
+++ src/main/java/com/xiliulou/electricity/entity/ElectricityCabinetFile.java
@@ -1,37 +1,7 @@
 package com.xiliulou.electricity.entity;
-
-
-
-
-
-                                    
-                                            
-                                            
-                                            
-                                            
-                                            
-                                            
-                                            
-                                            
-                                            
-                                            
-                    
-                                    
-                                            
-                                            
-                                            
-                                            
-                                            
-                                            
-                                            
-                                            
-                                            
-                                            
-                    
-
-                                            import com.baomidou.mybatisplus.annotation.IdType;
-                                            import com.baomidou.mybatisplus.annotation.TableId;
-                                            import lombok.AllArgsConstructor;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
