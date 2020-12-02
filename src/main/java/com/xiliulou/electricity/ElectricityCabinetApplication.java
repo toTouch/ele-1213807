8feea47e710d2227a6de62d9965e227480e75698
liXueChun <2628585226@qq.com>
2020-12-02 17:05:30 +0800

    测试

--- src/main/java/com/xiliulou/electricity/ElectricityCabinetApplication.java
+++ src/main/java/com/xiliulou/electricity/ElectricityCabinetApplication.java
@@ -22,7 +22,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 @EnableRedis
-@EnableIot
+/*@EnableIot*/
 @EnableStorage
 @EnableXllRestTemplate
 @EnableDynamicDataSource
