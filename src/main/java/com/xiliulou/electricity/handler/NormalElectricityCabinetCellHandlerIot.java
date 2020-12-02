119910c1f2fe898126cdd5032b506f6ddfad3dde
liXueChun <2628585226@qq.com>
2020-12-02 15:46:32 +0800

    换电柜操作模板

---
+++ src/main/java/com/xiliulou/electricity/handler/NormalElectricityCabinetCellHandlerIot.java
@@ -0,0 +1,29 @@
+package com.xiliulou.electricity.handler;
+import com.xiliulou.iot.entity.HardwareCommandQuery;
+import com.xiliulou.iot.entity.ReceiverMessage;
+import com.xiliulou.iot.entity.SendHardwareMessage;
+import com.xiliulou.iot.service.AbstractIotMessageHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import shaded.org.apache.commons.lang3.tuple.Pair;
+
+
+/**
+ * @author: lxc
+ * @Date: 2020/12/2 17:02
+ * @Description:
+ */
+@Service
+@Slf4j
+public class NormalElectricityCabinetCellHandlerIot extends AbstractIotMessageHandler {
+
+	@Override
+	protected Pair<SendHardwareMessage, String> generateMsg(HardwareCommandQuery hardwareCommandQuery) {
+		return null;
+	}
+
+	@Override
+	protected boolean receiveMessageProcess(ReceiverMessage receiverMessage) {
+		return false;
+	}
+}
---
+++ src/main/java/com/xiliulou/electricity/queue/ElectricityCabinetOperateQueueHandler.java
@@ -0,0 +1,49 @@
+package com.xiliulou.electricity.queue;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+/**
+ * @author: lxc
+ * @Date: 2020/12/3 08:31
+ * @Description:
+ */
+@Service
+@Slf4j
+public class ElectricityCabinetOperateQueueHandler {
+
+	ExecutorService executorService = Executors.newFixedThreadPool(20);
+	ExecutorService startService = Executors.newFixedThreadPool(1);
+	private volatile boolean shutdown = false;
+
+	@EventListener({WebServerInitializedEvent.class})
+	public void startHandleLockerOperate() {
+		initElectricityCabinetOperate();
+	}
+
+	private void initElectricityCabinetOperate() {
+		log.info("初始化寄存柜操作响应处理器");
+		startService.execute(() -> {
+			while (!shutdown) {
+				try {
+					executorService.execute(() -> {
+					});
+
+				} catch (Exception e) {
+					log.error("LOCKER OPERATE QUEUE ERROR! ", e);
+				}
+
+			}
+		});
+	}
+
+
+	public void shutdown() {
+		shutdown = true;
+		executorService.shutdown();
+	}
+}
