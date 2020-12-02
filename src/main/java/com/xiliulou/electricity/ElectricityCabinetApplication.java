b1e8ba4b4bb37cf7dd48041764b4791f81dd1c50
yg <1030245747@qq.com>
2020-12-02 16:48:12 +0800

    支付

--- src/main/java/com/xiliulou/electricity/ElectricityCabinetApplication.java
+++ src/main/java/com/xiliulou/electricity/ElectricityCabinetApplication.java
@@ -4,6 +4,7 @@ import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import com.xiliulou.cache.redis.EnableRedis;
 import com.xiliulou.core.http.resttemplate.EnableXllRestTemplate;
 import com.xiliulou.db.dynamic.annotation.EnableDynamicDataSource;
+import com.xiliulou.pay.EnablePay;
 import com.xiliulou.storage.EnableStorage;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -24,6 +25,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableXllRestTemplate
 @EnableDynamicDataSource
 @MapperScan("com.xiliulou.**.mapper")
+@EnablePay
 public class ElectricityCabinetApplication {
     public static void main(String[] args) {
         SpringApplication.run(ElectricityCabinetApplication.class, args);
