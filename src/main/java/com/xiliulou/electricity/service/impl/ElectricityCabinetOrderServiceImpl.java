d3ebe56c9c0111b821c538528156e88dbc9d2f73
liXueChun <2628585226@qq.com>
2020-12-16 14:47:53 +0800

    下单bug

--- src/main/java/com/xiliulou/electricity/service/impl/ElectricityCabinetOrderServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/ElectricityCabinetOrderServiceImpl.java
@@ -360,7 +360,7 @@ public class ElectricityCabinetOrderServiceImpl implements ElectricityCabinetOrd
         if (successTotal == 0 || countTotal == 0) {
             return BigDecimal.valueOf(0);
         }
-        return BigDecimal.valueOf(successTotal).multiply(BigDecimal.valueOf(100).divide(BigDecimal.valueOf(countTotal),BigDecimal.ROUND_HALF_UP));
+        return BigDecimal.valueOf(successTotal).multiply(BigDecimal.valueOf(100).divide(BigDecimal.valueOf(countTotal),BigDecimal.ROUND_HALF_EVEN));
     }
 
     @Override
