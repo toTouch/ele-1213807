4b649b09d2dd0780071e954c0eeb1f77a8a7bf6f
Eclair <1269029723@qq.com>
2020-12-16 19:09:49 +0800

    增加请求日志

--- src/main/java/com/xiliulou/electricity/service/token/WxProThirdAuthenticationServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/token/WxProThirdAuthenticationServiceImpl.java
@@ -239,6 +239,8 @@ public class WxProThirdAuthenticationServiceImpl implements ThirdAuthenticationS
 				.updateTime(System.currentTimeMillis())
 				.phone(wxMinProPhoneResultDTO.getPurePhoneNumber())
 				.uid(insert.getUid())
+				.accessToken("")
+				.refreshToken("")
 				.thirdNick("")
 				.thirdId(result.getOpenid())
 				.source(UserOauthBind.SOURCE_WX_PRO)
