b949e861e525572cf2737decb8dfde7403875bb7
liXueChun <2628585226@qq.com>
2020-12-17 19:33:27 +0800

    修改配置

--- src/main/java/com/xiliulou/electricity/filter/RequestFilter.java
+++ src/main/java/com/xiliulou/electricity/filter/RequestFilter.java
@@ -30,72 +30,69 @@ import java.util.Objects;
 @Component("newRequestInterceptor")
 public class RequestFilter implements Filter {
 
-	private static final String REQ_TIME = "req_time";
+    private static final String REQ_TIME = "req_time";
 
-	public void afterCompletion(HttpServletRequest request) {
-		Object attribute = request.getAttribute(REQ_TIME);
-		if (Objects.isNull(attribute) || StrUtil.isEmpty(attribute.toString())) {
-			return;
-		}
-		Long startTime = null;
-		try {
-			startTime = Long.parseLong(attribute.toString());
-		} catch (Exception e) {
-			log.error("parse startTime error!param={}", attribute, e);
-		}
-		if (Objects.isNull(startTime)) {
-			return;
-		}
-		Long uid = SecurityUtils.getUid();
-		log.info("uid={},method={},uri={},time={}", uid, request.getMethod(), request.getRequestURI(), System.currentTimeMillis() - startTime);
+    public void afterCompletion(HttpServletRequest request) {
+        Object attribute = request.getAttribute(REQ_TIME);
+        if (Objects.isNull(attribute) || StrUtil.isEmpty(attribute.toString())) {
+            return;
+        }
+        Long startTime = null;
+        try {
+            startTime = Long.parseLong(attribute.toString());
+        } catch (Exception e) {
+            log.error("parse startTime error!param={}", attribute, e);
+        }
+        if (Objects.isNull(startTime)) {
+            return;
+        }
+        Long uid = SecurityUtils.getUid();
+        log.info("uid={},method={},uri={},time={}", uid, request.getMethod(), request.getRequestURI(), System.currentTimeMillis() - startTime);
 
-	}
+    }
 
-	private String getRequestBody(HttpServletRequest request) {
-		int contentLength = request.getContentLength();
-		if (contentLength <= 0) {
-			return "";
-		}
-		try {
-			return IOUtils.toString(request.getReader());
-		} catch (IOException e) {
-			log.error("获取请求体失败", e);
-			return "";
-		}
-	}
+    private String getRequestBody(HttpServletRequest request) {
+        int contentLength = request.getContentLength();
+        if (contentLength <= 0) {
+            return "";
+        }
+        try {
+            return IOUtils.toString(request.getReader());
+        } catch (IOException e) {
+            log.error("获取请求体失败", e);
+            return "";
+        }
+    }
 
-	@Override
-	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-		HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-		String header = httpServletRequest.getHeader(HttpHeaders.CONTENT_TYPE);
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String header = httpServletRequest.getHeader(HttpHeaders.CONTENT_TYPE);
 
-		if(header.startsWith(MediaType.MULTIPART_FORM_DATA_VALUE)) {
-			filterChain.doFilter(httpServletRequest, servletResponse);
-
-
-		} else {
-			httpServletRequest = new BodyReaderHttpServletRequestWrapper(httpServletRequest);
-
-			if (Objects.isNull(httpServletRequest.getAttribute(REQ_TIME)) || StrUtil.isEmpty(httpServletRequest.getAttribute(REQ_TIME).toString())) {
-				httpServletRequest.setAttribute(REQ_TIME, System.currentTimeMillis());
-			}
-
-			Long uid = SecurityUtils.getUid();
-			if (StrUtil.isEmpty(header)) {
-				log.info("uid={},method={},uri={},params={}", uid, httpServletRequest.getMethod(), httpServletRequest.getRequestURI(), JsonUtil.toJson(httpServletRequest.getParameterMap()));
-			} else {
-				if (header.startsWith(MediaType.APPLICATION_JSON_VALUE)) {
-					log.info("uid={},method={},uri={},params={}", uid, httpServletRequest.getMethod(), httpServletRequest.getRequestURI(), getRequestBody(httpServletRequest));
-				} else {
-					log.info("uid={},method={},uri={},params={}", uid, httpServletRequest.getMethod(), httpServletRequest.getRequestURI(), JsonUtil.toJson(httpServletRequest.getParameterMap()));
-				}
-
-			}
-			filterChain.doFilter(httpServletRequest, servletResponse);
-
-		}
-		afterCompletion(httpServletRequest);
+        if (Objects.isNull(httpServletRequest.getAttribute(REQ_TIME)) || StrUtil.isEmpty(httpServletRequest.getAttribute(REQ_TIME).toString())) {
+            httpServletRequest.setAttribute(REQ_TIME, System.currentTimeMillis());
+        }
 
 
-	}
+        Long uid = SecurityUtils.getUid();
+
+        if (StrUtil.isEmpty(header) || header.startsWith(MediaType.MULTIPART_FORM_DATA_VALUE)) {
+            log.info("uid={},method={},uri={},params={}", uid, httpServletRequest.getMethod(), httpServletRequest.getRequestURI(), JsonUtil.toJson(httpServletRequest.getParameterMap()));
+            filterChain.doFilter(httpServletRequest, servletResponse);
+        }  else {
+            httpServletRequest = new BodyReaderHttpServletRequestWrapper(httpServletRequest);
+
+            if (header.startsWith(MediaType.APPLICATION_JSON_VALUE)) {
+                log.info("uid={},method={},uri={},params={}", uid, httpServletRequest.getMethod(), httpServletRequest.getRequestURI(), getRequestBody(httpServletRequest));
+            } else {
+                log.info("uid={},method={},uri={},params={}", uid, httpServletRequest.getMethod(), httpServletRequest.getRequestURI(), JsonUtil.toJson(httpServletRequest.getParameterMap()));
+            }
+
+            filterChain.doFilter(httpServletRequest, servletResponse);
+
+        }
+        afterCompletion(httpServletRequest);
+
+
+    }
 }
