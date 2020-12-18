945ea45afaf7f31aabb16e44541fa0938a8de2a7
liXueChun <2628585226@qq.com>
2020-12-18 10:53:08 +0800

    修改配置

--- src/main/java/com/xiliulou/electricity/controller/admin/ElectricityCabinetFileAdminController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/ElectricityCabinetFileAdminController.java
@@ -43,12 +43,12 @@ public class ElectricityCabinetFileAdminController {
     ElectricityCabinetFileService electricityCabinetFileService;
     @Autowired
     StorageConfig storageConfig;
-    /*@Autowired
-    GetStorageService getStorageService;
-    StorageService storageService=getStorageService.getStorageService(storageConfig.getIsUseOSS());*/
-    @Qualifier("minioService")
     @Autowired
-    StorageService storageService;
+    GetStorageService getStorageService;
+    StorageService storageService=getStorageService.getStorageService(storageConfig.getIsUseOSS());
+   /* @Qualifier("minioService")
+    @Autowired
+    StorageService storageService;*/
 
     //通知前端是aili还是oss
     @GetMapping("/admin/electricityCabinetFileService/noticeIsOss")
