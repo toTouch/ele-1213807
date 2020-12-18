569d3117e39d50fffb01699cca530abdcf468225
Eclair <1269029723@qq.com>
2020-12-18 13:04:49 +0800

    bug fix

--- src/main/java/com/xiliulou/electricity/web/entity/BodyReaderHttpServletRequestWrapper.java
+++ src/main/java/com/xiliulou/electricity/web/entity/BodyReaderHttpServletRequestWrapper.java
@@ -1,6 +1,9 @@
 package com.xiliulou.electricity.web.entity;
 
+import cn.hutool.core.util.StrUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ReadListener;
 import javax.servlet.ServletException;
@@ -9,6 +12,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.Part;
+
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -20,110 +24,113 @@ import java.util.Collection;
 
 @Slf4j
 public class BodyReaderHttpServletRequestWrapper extends HttpServletRequestWrapper {
-    private final byte[] body;
-    private final Collection<Part> parts;
+	private final byte[] body;
+	private final Collection<Part> parts;
 
-    public BodyReaderHttpServletRequestWrapper(HttpServletRequest request) throws IOException, ServletException {
-        super(request);
-        String sessionStream = getBodyString(request);
-        body = sessionStream.getBytes(StandardCharsets.UTF_8);
-        parts = request.getParts();
-    }
+	public BodyReaderHttpServletRequestWrapper(HttpServletRequest request) throws IOException, ServletException {
+		super(request);
+		String sessionStream = getBodyString(request);
+		body = sessionStream.getBytes(StandardCharsets.UTF_8);
+		if (StrUtil.isNotEmpty(request.getHeader(HttpHeaders.CONTENT_TYPE)) && request.getHeader(HttpHeaders.CONTENT_TYPE).startsWith(MediaType.MULTIPART_FORM_DATA_VALUE)) {
+			parts = request.getParts();
+		} else {
+			parts = null;
+		}
+	}
 
-    @Override
-    public Collection<Part> getParts() {
-        return parts;
-    }
+	@Override
+	public Collection<Part> getParts() {
+		return parts;
+	}
 
+	/**
+	 * 获取请求Body
+	 *
+	 * @param request
+	 * @return
+	 */
+	public String getBodyString(final ServletRequest request) {
+		StringBuilder sb = new StringBuilder();
+		InputStream inputStream = null;
+		BufferedReader reader = null;
+		try {
+			inputStream = cloneInputStream(request.getInputStream());
+			reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+			String line = "";
+			while ((line = reader.readLine()) != null) {
+				sb.append(line);
+			}
+		} catch (IOException e) {
+			log.error("ERROR", e);
+		} finally {
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (IOException e) {
+					log.error("ERROR", e);
+				}
+			}
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					log.error("ERROR", e);
+				}
+			}
+		}
+		return sb.toString();
+	}
 
-    /**
-     * 获取请求Body
-     *
-     * @param request
-     * @return
-     */
-    public String getBodyString(final ServletRequest request) {
-        StringBuilder sb = new StringBuilder();
-        InputStream inputStream = null;
-        BufferedReader reader = null;
-        try {
-            inputStream = cloneInputStream(request.getInputStream());
-            reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-            String line = "";
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-            }
-        } catch (IOException e) {
-           log.error("ERROR",e);
-        } finally {
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    log.error("ERROR",e);
-                }
-            }
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    log.error("ERROR",e);
-                }
-            }
-        }
-        return sb.toString();
-    }
+	/**
+	 * Description: 复制输入流
+	 *
+	 * @param inputStream
+	 * @return
+	 */
+	public InputStream cloneInputStream(ServletInputStream inputStream) {
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		byte[] buffer = new byte[1024];
+		int len;
+		try {
+			while ((len = inputStream.read(buffer)) > -1) {
+				byteArrayOutputStream.write(buffer, 0, len);
+			}
+			byteArrayOutputStream.flush();
+		} catch (IOException e) {
+			log.error("ERROR", e);
+		}
+		return new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+	}
 
-    /**
-     * Description: 复制输入流
-     *
-     * @param inputStream
-     * @return
-     */
-    public InputStream cloneInputStream(ServletInputStream inputStream) {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        int len;
-        try {
-            while ((len = inputStream.read(buffer)) > -1) {
-                byteArrayOutputStream.write(buffer, 0, len);
-            }
-            byteArrayOutputStream.flush();
-        } catch (IOException e) {
-            log.error("ERROR",e);
-        }
-        return new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
-    }
+	@Override
+	public BufferedReader getReader() throws IOException {
+		return new BufferedReader(new InputStreamReader(getInputStream()));
+	}
 
-    @Override
-    public BufferedReader getReader() throws IOException {
-        return new BufferedReader(new InputStreamReader(getInputStream()));
-    }
+	@Override
+	public ServletInputStream getInputStream() throws IOException {
+		final ByteArrayInputStream bais = new ByteArrayInputStream(body);
 
-    @Override
-    public ServletInputStream getInputStream() throws IOException {
-        final ByteArrayInputStream bais = new ByteArrayInputStream(body);
+		return new ServletInputStream() {
 
-        return new ServletInputStream() {
+			@Override
+			public int read() throws IOException {
+				return bais.read();
+			}
 
-            @Override
-            public int read() throws IOException {
-                return bais.read();
-            }
+			@Override
+			public boolean isFinished() {
+				return false;
+			}
 
-            @Override
-            public boolean isFinished() {
-                return false;
-            }
+			@Override
+			public boolean isReady() {
+				return false;
+			}
 
-            @Override
-            public boolean isReady() {
-                return false;
-            }
-
-            @Override
-            public void setReadListener(ReadListener readListener) {
-            }
-        };
-    }
+			@Override
+			public void setReadListener(ReadListener readListener) {
+			}
+		};
+	}
 }
