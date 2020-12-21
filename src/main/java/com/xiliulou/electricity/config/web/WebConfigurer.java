69283c508b7fae97a191843b1315fb76f0086f23
liXueChun <2628585226@qq.com>
2020-12-21 16:00:48 +0800

    滑动验证码测试

--- src/main/java/com/xiliulou/electricity/config/web/WebConfigurer.java
+++ src/main/java/com/xiliulou/electricity/config/web/WebConfigurer.java
@@ -1,8 +1,14 @@
 package com.xiliulou.electricity.config.web;
 
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Auther: eclair
@@ -15,5 +21,17 @@ public class WebConfigurer implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 
+	}
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+		List<MediaType> mediaTypes = new ArrayList<>(16);
+		mediaTypes.add(MediaType.APPLICATION_ATOM_XML);
+		mediaTypes.add(MediaType.APPLICATION_CBOR);
+		mediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
+		mediaTypes.add(MediaType.APPLICATION_JSON);
+		mediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
+		converter.setSupportedMediaTypes(mediaTypes);
+		converters.add(converter);
 	}
 }
