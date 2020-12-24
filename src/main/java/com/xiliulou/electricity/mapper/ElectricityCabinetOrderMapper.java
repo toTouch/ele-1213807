8e28971b5e2e25fe87260f63efdd52fc66899c4b
liXueChun <2628585226@qq.com>
2020-12-24 11:09:32 +0800

    修改bug

--- src/main/java/com/xiliulou/electricity/mapper/ElectricityCabinetOrderMapper.java
+++ src/main/java/com/xiliulou/electricity/mapper/ElectricityCabinetOrderMapper.java
@@ -28,8 +28,6 @@ public interface ElectricityCabinetOrderMapper extends BaseMapper<ElectricityCab
     /**
      * 查询指定行数据
      *
-     * @param offset 查询起始位置
-     * @param limit 查询条数
      * @return 对象列表
      */
     List<ElectricityCabinetOrderVO> queryList(@Param("query") ElectricityCabinetOrderQuery electricityCabinetOrderQuery);
