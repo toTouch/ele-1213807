e3e751c856db179bf0ec75a24f0a2a9eb6509ff5
liXueChun <2628585226@qq.com>
2021-01-05 18:24:21 +0800

    登录

--- src/main/java/com/xiliulou/electricity/service/token/LoginSuccessPostProcessor.java
+++ src/main/java/com/xiliulou/electricity/service/token/LoginSuccessPostProcessor.java
@@ -5,11 +5,8 @@ import com.xiliulou.security.authentication.AuthenticationSuccessPostProcessor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
-
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Slf4j
 @Service
@@ -17,7 +14,7 @@ public class LoginSuccessPostProcessor implements AuthenticationSuccessPostProce
 
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication, Integer type) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication, Integer type) {
         log.info("登录成功");
     }
 }
