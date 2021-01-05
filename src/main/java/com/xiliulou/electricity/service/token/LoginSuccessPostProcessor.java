0d6527efa3b51ddd112f3a82177f0dcd778e8294
liXueChun <2628585226@qq.com>
2021-01-05 19:06:36 +0800

    登录

--- src/main/java/com/xiliulou/electricity/service/token/LoginSuccessPostProcessor.java
+++ src/main/java/com/xiliulou/electricity/service/token/LoginSuccessPostProcessor.java
@@ -1,13 +1,15 @@
 package com.xiliulou.electricity.service.token;
+
+
 import com.xiliulou.core.json.JsonUtil;
 import com.xiliulou.security.authentication.AuthenticationSuccessPostProcessor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 @Slf4j
 @Service
@@ -15,9 +17,27 @@ public class LoginSuccessPostProcessor implements AuthenticationSuccessPostProce
 
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication, Integer type) throws UnknownHostException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication, Integer type) {
         log.info("authentication is --> {}",JsonUtil.toJson(authentication));
-        InetAddress ip = InetAddress.getLocalHost();
+        String ip = request.getHeader("X-Requested-For");
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Forwarded-For");
+        }
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
         log.info("ip is -->{}",ip);
     }
 }
