6f1133645d5c9835891e5b39e7715bff0865c3d1
liXueChun <2628585226@qq.com>
2021-01-05 16:50:38 +0800

    登录

--- src/main/java/com/xiliulou/electricity/handler/LoginSuccessHandler.java
+++ src/main/java/com/xiliulou/electricity/handler/LoginSuccessHandler.java
@@ -2,15 +2,14 @@ package com.xiliulou.electricity.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
-public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     /**
      * Called when a user has been successfully authenticated.
      *
