8f03115ed480a2ba014242cce822fc5fc0938f1a
liXueChun <2628585226@qq.com>
2021-01-05 18:40:32 +0800

    登录

--- src/main/java/com/xiliulou/electricity/service/token/LoginSuccessPostProcessor.java
+++ src/main/java/com/xiliulou/electricity/service/token/LoginSuccessPostProcessor.java
@@ -1,6 +1,7 @@
 package com.xiliulou.electricity.service.token;
 
 
+import com.xiliulou.core.json.JsonUtil;
 import com.xiliulou.security.authentication.AuthenticationSuccessPostProcessor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -16,6 +17,11 @@ public class LoginSuccessPostProcessor implements AuthenticationSuccessPostProce
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication, Integer type) {
-        log.info("登录成功");
+        log.info(JsonUtil.toJson(authentication));
+        if (request.getHeader("x-forwarded-for") == null) {
+            log.info(request.getRemoteAddr());
+        }else {
+            log.info(request.getHeader("x-forwarded-for"));
+        }
     }
 }
