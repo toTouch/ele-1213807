86e5628c49f26c1cec5211fb79109268dd841c65
liXueChun <2628585226@qq.com>
2021-01-05 19:02:32 +0800

    登录

--- src/main/java/com/xiliulou/electricity/service/token/LoginSuccessPostProcessor.java
+++ src/main/java/com/xiliulou/electricity/service/token/LoginSuccessPostProcessor.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Slf4j
 @Service
@@ -17,27 +19,9 @@ public class LoginSuccessPostProcessor implements AuthenticationSuccessPostProce
 
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication, Integer type) {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication, Integer type) throws UnknownHostException {
         log.info("authentication is --> {}",JsonUtil.toJson(authentication));
-        String ip = request.getHeader("X-Requested-For");
-        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("X-Forwarded-For");
-        }
-        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
-        }
-        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_CLIENT_IP");
-        }
-        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-        }
-        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
-        }
+        InetAddress ip = InetAddress.getLocalHost();
         log.info("ip is -->{}",ip);
     }
 }
