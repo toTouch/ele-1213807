c696acb7bfe071ca1aaf4d3e517070fbbaed1f81
liXueChun <2628585226@qq.com>
2021-01-14 16:04:46 +0800

    修改bug

--- src/main/java/com/xiliulou/electricity/controller/admin/ElectricityCabinetAdminController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/ElectricityCabinetAdminController.java
@@ -1,5 +1,4 @@
 package com.xiliulou.electricity.controller.admin;
-
 import cn.hutool.core.util.StrUtil;
 import com.xiliulou.core.web.R;
 import com.xiliulou.electricity.query.EleOuterCommandQuery;
@@ -11,7 +10,6 @@ import com.xiliulou.electricity.validator.UpdateGroup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Objects;
 
 /**
