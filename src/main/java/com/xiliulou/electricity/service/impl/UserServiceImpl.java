94c8ea8b4b7b91a7b002306d03afd715769ac86d
liXueChun <2628585226@qq.com>
2021-01-15 11:31:32 +0800

    修改bug

--- src/main/java/com/xiliulou/electricity/service/impl/UserServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/UserServiceImpl.java
@@ -335,6 +335,7 @@ public class UserServiceImpl implements UserService {
         User updateUser = new User();
         updateUser.setUid(oldUser.getUid());
         updateUser.setLoginPwd(passwordQuery.getPassword());
+        updateUser.setUpdateTime(System.currentTimeMillis());
         Integer update = updateUser(updateUser, oldUser);
 
         return update > 0 ? Triple.of(true, null, null) : Triple.of(false, null, "修改密码失败!");
