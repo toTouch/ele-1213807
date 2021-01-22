ea4dc9fd2b410a32ccf40df9e1f5196101a71b39
liXueChun <2628585226@qq.com>
2021-01-22 14:47:29 +0800

    门店绑定电柜

--- src/main/java/com/xiliulou/electricity/controller/admin/ElectricityCabinetBatteryAdminController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/ElectricityCabinetBatteryAdminController.java
@@ -1,6 +1,4 @@
 package com.xiliulou.electricity.controller.admin;
-
-
 import com.xiliulou.core.web.R;
 import com.xiliulou.electricity.entity.ElectricityBattery;
 import com.xiliulou.electricity.query.ElectricityBatteryQuery;
@@ -9,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Objects;
 
 /**
