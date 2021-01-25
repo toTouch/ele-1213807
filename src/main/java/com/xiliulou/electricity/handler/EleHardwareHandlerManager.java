451750081d5076b9e86f522f5325d15f15d08d31
liXueChun <2628585226@qq.com>
2021-01-25 15:56:13 +0800

    修改bug

--- src/main/java/com/xiliulou/electricity/handler/EleHardwareHandlerManager.java
+++ src/main/java/com/xiliulou/electricity/handler/EleHardwareHandlerManager.java
@@ -13,7 +13,6 @@ import shaded.org.apache.commons.lang3.StringUtils;
 import shaded.org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
