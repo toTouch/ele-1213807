b350824588dcfbb3a497eb68636dab50af8c97ad
yg <1030245747@qq.com>
2021-03-02 14:03:39 +0800

    请求日志

--- src/main/java/com/xiliulou/electricity/filter/RequestFilter.java
+++ src/main/java/com/xiliulou/electricity/filter/RequestFilter.java
@@ -41,7 +41,7 @@ public class RequestFilter implements Filter {
             return;
         }
         Long uid = SecurityUtils.getUid();
-        log.info("uid={},method={},uri={},time={}毫秒", uid, request.getMethod(), request.getRequestURI(), System.currentTimeMillis() - startTime);
+        log.info("uid={},method={},uri={},time={} ms", uid, request.getMethod(), request.getRequestURI(), System.currentTimeMillis() - startTime);
 
     }
 
