aa715d023c310b7a934882ce1826eaee8b294e8a
liXueChun <2628585226@qq.com>
2021-03-03 09:33:22 +0800

    租电池

--- src/main/java/com/xiliulou/electricity/controller/user/EleUserAuthUserController.java
+++ src/main/java/com/xiliulou/electricity/controller/user/EleUserAuthUserController.java
@@ -11,7 +11,6 @@ import com.xiliulou.electricity.utils.SecurityUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import javax.annotation.Resource;
 import java.util.List;
 import java.util.Objects;
 
