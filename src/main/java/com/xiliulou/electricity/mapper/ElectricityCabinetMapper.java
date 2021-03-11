c5a6abcab91e6dc305b9013adce25c83066dbd35
liXueChun <2628585226@qq.com>
2021-03-11 14:51:48 +0800

    修改bug

--- src/main/java/com/xiliulou/electricity/mapper/ElectricityCabinetMapper.java
+++ src/main/java/com/xiliulou/electricity/mapper/ElectricityCabinetMapper.java
@@ -52,5 +52,4 @@ public interface ElectricityCabinetMapper extends BaseMapper<ElectricityCabinet>
 
     IPage listByStoreId(Page page,@Param("query") ElectricityCabinetQuery electricityCabinetQuery);
 
-    List<ElectricityCabinetVO> showInfoByDistanceAndStoreId(@Param("query") ElectricityCabinetQuery electricityCabinetQuery);
 }
