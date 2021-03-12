ff06b1beb251012eb00e30c90554edea1a4f665d
liXueChun <2628585226@qq.com>
2021-03-12 11:39:00 +0800

    修改bug

--- src/main/java/com/xiliulou/electricity/mapper/ElectricityCabinetOrderOperHistoryMapper.java
+++ src/main/java/com/xiliulou/electricity/mapper/ElectricityCabinetOrderOperHistoryMapper.java
@@ -1,14 +1,11 @@
 package com.xiliulou.electricity.mapper;
-
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.xiliulou.electricity.entity.ElectricityCabinetOrderOperHistory;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xiliulou.electricity.query.ElectricityCabinetOrderOperHistoryQuery;
-import com.xiliulou.electricity.vo.ElectricityCabinetOrderOperHistoryVO;
 import org.apache.ibatis.annotations.Param;
 
-import java.util.List;
 
 /**
  * 订单的操作历史记录(TElectricityCabinetOrderOperHistory)表数据库访问层
