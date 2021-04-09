f6bfb50aaf8155872d9b1cd7372206b1a1f5d716
liXueChun <2628585226@qq.com>
2021-04-09 16:15:31 +0800

    电池上报

--- src/main/java/com/xiliulou/electricity/query/BatteryReportQuery.java
+++ src/main/java/com/xiliulou/electricity/query/BatteryReportQuery.java
@@ -1,12 +1,6 @@
 package com.xiliulou.electricity.query;
 import lombok.Data;
 
-/**
- * 用户列表(TUserInfo)实体类
- *
- * @author makejava
- * @since 2020-12-07 15:00:00
- */
 @Data
 public class BatteryReportQuery {
     /**
