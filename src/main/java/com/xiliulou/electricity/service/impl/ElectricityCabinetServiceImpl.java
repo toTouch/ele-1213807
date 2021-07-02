811e61d68085118788819c64d9eaabee40dce7ea
dongfangbubai <15371639767.163.com>
2021-07-02 17:53:24 +0800

    修改bug

--- src/main/java/com/xiliulou/electricity/service/impl/ElectricityCabinetServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/ElectricityCabinetServiceImpl.java
@@ -835,6 +835,7 @@ public class ElectricityCabinetServiceImpl implements ElectricityCabinetService 
 
 		//我的电池
 		Double battery=null;
+		log.info("franchiseeUserInfo is -->{}",franchiseeUserInfo);
 		if (Objects.nonNull(franchiseeUserInfo.getNowElectricityBatterySn()) && Objects.equals(userInfo.getServiceStatus(), FranchiseeUserInfo.STATUS_IS_BATTERY)) {
 			ElectricityBattery electricityBattery = electricityBatteryService.queryBySn(franchiseeUserInfo.getNowElectricityBatterySn());
 			if (Objects.nonNull(electricityBattery)) {
