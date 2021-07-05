4954517219f7611835dc90c2a2a8cc1466b9969a
dongfangbubai <15371639767.163.com>
2021-07-05 14:20:30 +0800

    修改bug

--- src/main/java/com/xiliulou/electricity/handler/NormalEleCellHandlerIot.java
+++ src/main/java/com/xiliulou/electricity/handler/NormalEleCellHandlerIot.java
@@ -51,7 +51,70 @@ public class NormalEleCellHandlerIot extends AbstractIotMessageHandler {
 
     @Override
     protected boolean receiveMessageProcess(ReceiverMessage receiverMessage) {
+        ElectricityCabinet electricityCabinet = electricityCabinetService.queryFromCacheByProductAndDeviceName(receiverMessage.getProductKey(), receiverMessage.getDeviceName());
+        if (Objects.isNull(electricityCabinet)) {
+            log.error("ELE ERROR! no product and device ,p={},d={}", receiverMessage.getProductKey(), receiverMessage.getDeviceName());
+            return false;
+        }
+        EleCellVO eleCellVo = JsonUtil.fromJson(receiverMessage.getOriginContent(), EleCellVO.class);
+        if (Objects.isNull(eleCellVo)) {
+            log.error("ele cell error! no eleCellVo,{}", receiverMessage.getOriginContent());
+            return false;
+        }
+        String cellNo = eleCellVo.getCell_no();
+        if (StringUtils.isEmpty(cellNo)) {
+            log.error("ele cell error! no eleCellVo,{}", receiverMessage.getOriginContent());
+            return false;
+        }
+        ElectricityCabinetBox electricityCabinetBox = new ElectricityCabinetBox();
+        electricityCabinetBox.setElectricityCabinetId(electricityCabinet.getId());
+        electricityCabinetBox.setCellNo(cellNo);
+        String isLock = eleCellVo.getIs_lock();
+        if (StringUtils.isNotEmpty(isLock)) {
+            electricityCabinetBox.setIsLock(Integer.valueOf(isLock));
+        }
+        String isFan = eleCellVo.getIs_fan();
+        if (StringUtils.isNotEmpty(isFan)) {
+            electricityCabinetBox.setIsFan(Integer.valueOf(isFan));
+        }
+        String temperature = eleCellVo.getTemperature();
+        if (StringUtils.isNotEmpty(temperature)) {
+            electricityCabinetBox.setTemperature(temperature);
+        }
+        String isHeat = eleCellVo.getIs_heat();
+        if (StringUtils.isNotEmpty(isHeat)) {
+            electricityCabinetBox.setIsHeat(Integer.valueOf(isHeat));
+        }
+        String isLight = eleCellVo.getIs_light();
+        if (StringUtils.isNotEmpty(isLight)) {
+            electricityCabinetBox.setIsLight(Integer.valueOf(isLight));
+        }
+        String isForbidden = eleCellVo.getIs_forbidden();
+        if (StringUtils.isNotEmpty(isForbidden)) {
+            electricityCabinetBox.setUsableStatus(Integer.valueOf(isForbidden));
+        }
+        electricityCabinetBoxService.modifyByCellNo(electricityCabinetBox);
         return true;
     }
 }
+
+@Data
+class EleCellVO {
+    //仓门号
+    private String cell_no;
+    //门锁状态
+    private String is_lock;
+    //风扇状态
+    private String is_fan;
+    //温度
+    private String temperature;
+    //加热状态
+    private String is_heat;
+    //指示灯状态
+    private String is_light;
+    //可用禁用
+    private String is_forbidden;
+
+}
+
 
