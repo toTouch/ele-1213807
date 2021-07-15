da7155c15996b754a1a5c0e9adbb5ef74d378250
dongfangbubai <15371639767.163.com>
2021-07-15 15:59:49 +0800

    修改bug

--- src/main/java/com/xiliulou/electricity/query/ElectricityMemberCardOrderQuery.java
+++ src/main/java/com/xiliulou/electricity/query/ElectricityMemberCardOrderQuery.java
@@ -1,6 +1,10 @@
 package com.xiliulou.electricity.query;
 
+import com.xiliulou.electricity.validator.UpdateGroup;
 import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 /**
  * @author: Miss.Li
@@ -10,13 +14,16 @@ import lombok.Data;
 @Data
 public class ElectricityMemberCardOrderQuery {
 
-	//卡
+	//月卡
+	@NotNull(message = "月卡不能为空!", groups = {UpdateGroup.class})
 	private Integer memberId;
 
 	//三元组
+	@NotEmpty(message = "三元组不能为空!", groups = {UpdateGroup.class})
 	private String productKey;
 
 	//三元组
+	@NotEmpty(message = "三元组不能为空!", groups = {UpdateGroup.class})
 	private String deviceName;
 
 	//优惠券
