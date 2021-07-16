2051bf187c93734fe24eed85218ad1180e06ef8d
dongfangbubai <15371639767.163.com>
2021-07-16 11:12:45 +0800

    修改bug

--- src/main/java/com/xiliulou/electricity/controller/user/JsonUserJoinShareActivityRecordController.java
+++ src/main/java/com/xiliulou/electricity/controller/user/JsonUserJoinShareActivityRecordController.java
@@ -1,11 +1,14 @@
 package com.xiliulou.electricity.controller.user;
 
+import com.alibaba.fastjson.JSONObject;
+import com.xiliulou.core.json.JsonUtil;
 import com.xiliulou.core.web.R;
 import com.xiliulou.electricity.entity.JoinShareActivityRecord;
 import com.xiliulou.electricity.service.JoinShareActivityRecordService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.Objects;
 
 /**
  * 参与邀请活动记录(JoinShareActivityRecord)表控制层
@@ -41,6 +44,16 @@ public class JsonUserJoinShareActivityRecordController {
         return joinShareActivityRecordService.joinActivity(activityId,uid);
     }
 
+    public static void main(String[] args) {
+        String result1="{\"errcode\":40001,\"errmsg\":\"invalid credential, access_token is invalid or not latest rid: 60f0f179-5deeae17-5d58cbd2\"}";
+        Object object1 = JsonUtil.fetchObject(result1, "/errcode");
+        System.out.println(object1);
+
+
+        String result2="12334444";
+        Object object2 = JsonUtil.fetchObject(result2, "/errcode");
+        System.out.println(object2);
+    }
 
 
 
