c9db0801cf3348b078ba32937ed56f3f9cc2735e
dongfangbubai <15371639767.163.com>
2021-07-22 16:13:00 +0800

    换电订单状态修改

--- src/main/java/com/xiliulou/electricity/service/impl/RentBatteryOrderServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/RentBatteryOrderServiceImpl.java
@@ -755,12 +755,25 @@ public class RentBatteryOrderServiceImpl implements RentBatteryOrderService {
 
 		String status = rentBatteryOrder.getStatus();
 
-		Integer cellNo=rentBatteryOrder.getCellNo();
+		//开门中
+		if (rentBatteryOrder.getOrderSeq()>RentBatteryOrder.STATUS_INIT
+				&& rentBatteryOrder.getOrderSeq()<RentBatteryOrder.STATUS_OPEN_SUCCESS) {
+			status = rentBatteryOrder.getCellNo() + "号仓门开门中";
+		}
+
+
+		//开门成功
+		if (Objects.equals(rentBatteryOrder.getStatus(), RentBatteryOrder.RENT_OPEN_SUCCESS)
+				|| Objects.equals(rentBatteryOrder.getStatus(), RentBatteryOrder.RETURN_OPEN_SUCCESS)) {
+			status = rentBatteryOrder.getCellNo() + "号仓门开门成功，电池检测中";
+		}
+
+
+
+
 
 		//订单状态
 		map.put("status", status);
-
-		map.put("cellNo", cellNo);
 
 		//是否出错 0--未出错 1--出错
 		Integer type = 0;
