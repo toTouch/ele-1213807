73e051afe03e4a81ea8b9daaac170b3800a6608a
dongfangbubai <15371639767.163.com>
2021-08-11 11:27:44 +0800

    线程池

--- src/main/java/com/xiliulou/electricity/controller/admin/XllThreadPoolExecutorsController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/XllThreadPoolExecutorsController.java
@@ -3,6 +3,7 @@ package com.xiliulou.electricity.controller.admin;
 import com.xiliulou.core.thread.XllThreadPoolExecutors;
 import com.xiliulou.core.web.R;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,16 +16,23 @@ public class XllThreadPoolExecutorsController {
 
 
 	//
-	@GetMapping(value = "/admin/getRunningExe")
-	public R getRunningExe() {
-		return R.ok(XllThreadPoolExecutors.getRunningExe());
-	}
-
-	//
 	@GetMapping(value = "/admin/getAllRunningExecutorServices")
 	public R getAllRunningExecutorServices() {
 		return R.ok(XllThreadPoolExecutors.getAllRunningExecutorServices());
 	}
 
+
+	//
+	@GetMapping(value = "/admin/getRunningExe")
+	public R getRunningExe() {
+		return R.ok(XllThreadPoolExecutors.getRunningExe());
+	}
+
+
+	//
+	@GetMapping(value = "/admin/getRunningExeByName")
+	public R getRunningExeByName(@RequestParam("name") String name) {
+		return R.ok(XllThreadPoolExecutors.getRunningExeByName(name));
+	}
 
 }
