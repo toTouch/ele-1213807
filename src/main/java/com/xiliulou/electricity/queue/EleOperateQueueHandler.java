78281305c00f7c8bd51e582ac28e60f490a982f9
dongfangbubai <15371639767.163.com>
2021-08-13 09:48:46 +0800

    线程池优化

--- src/main/java/com/xiliulou/electricity/queue/EleOperateQueueHandler.java
+++ src/main/java/com/xiliulou/electricity/queue/EleOperateQueueHandler.java
@@ -90,6 +90,12 @@ public class EleOperateQueueHandler {
 
 					EleOpenDTO finalOpenDTO = eleOpenDTO;
 					executorService.execute(() -> {
+						//线程池等待执行 3 ms
+						try {
+							Thread.sleep(30);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
 						handleOrderAfterOperated(finalOpenDTO);
 					});
 
