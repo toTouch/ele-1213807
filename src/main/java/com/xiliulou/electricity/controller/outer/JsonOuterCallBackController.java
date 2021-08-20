ce5275ed221d3180c6328c1efabd470e77833448
dongfangbubai <15371639767.163.com>
2021-08-20 18:49:26 +0800

    测试

--- src/main/java/com/xiliulou/electricity/controller/outer/JsonOuterCallBackController.java
+++ src/main/java/com/xiliulou/electricity/controller/outer/JsonOuterCallBackController.java
@@ -1,15 +1,24 @@
 package com.xiliulou.electricity.controller.outer;
 
+import cn.hutool.core.util.ObjectUtil;
+import com.xiliulou.core.web.R;
 import com.xiliulou.pay.weixinv3.query.WechatV3OrderCallBackQuery;
 import com.xiliulou.pay.weixinv3.query.WechatV3RefundOrderCallBackQuery;
 import com.xiliulou.pay.weixinv3.rsp.WechatV3CallBackResult;
 import com.xiliulou.pay.weixinv3.service.WechatV3PostProcessHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @program: XILIULOU
@@ -22,6 +31,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class JsonOuterCallBackController {
     @Autowired
     WechatV3PostProcessHandler wechatV3PostProcessHandler;
+    @Qualifier("newRedisTemplate")
+    @Autowired
+    RedisTemplate redisTemplate;
 
     /**
      * 微信支付通知
@@ -45,5 +57,22 @@ public class JsonOuterCallBackController {
         wechatV3RefundOrderCallBackQuery.setTenantId(tenantId);
         wechatV3PostProcessHandler.postProcessAfterWechatRefund(wechatV3RefundOrderCallBackQuery);
         return WechatV3CallBackResult.success();
+    }
+
+    //测试redis
+    @GetMapping("/outer/redis/test")
+    public R redisTest(){
+        String key = "redis_test";
+        RedisScript redisScript = RedisScript.of("local times = redis.call('incr',KEYS[1]) if times == 1 then redis.call('expire',KEYS[1],ARGV[1]) end if times > tonumber(ARGV[2]) then return 0 end return 1",
+                Long.class);
+
+        List<String> list = new ArrayList<>(1);
+        list.add(key);
+        Object result = redisTemplate.execute(redisScript, list, 60, 10);
+
+        if (ObjectUtil.equal(1L, result)) {
+            return R.ok();
+        }
+        return R.fail("失败");
     }
 }
