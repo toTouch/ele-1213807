e12d83834dad99f5e4175bfd51a0f94e77f73109
dongfangbubai <15371639767.163.com>
2021-10-09 09:32:40 +0800

    故障上报

---
+++ src/main/java/com/xiliulou/electricity/query/MaintenanceRecordListQuery.java
@@ -0,0 +1,24 @@
+package com.xiliulou.electricity.query;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author : eclair
+ * @date : 2021/9/26 3:16 下午
+ */
+@Data
+@Builder
+public class MaintenanceRecordListQuery {
+    private Integer size;
+    private Integer offset;
+    private Long beginTime;
+    private Long endTime;
+    private String type;
+    private String status;
+    private Long uid;
+    private String phone;
+    private Integer electricityCabinetId;
+}
