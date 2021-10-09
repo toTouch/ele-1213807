966ad0dd6337965de71cdb4038b17db0b654c322
dongfangbubai <15371639767.163.com>
2021-10-09 17:12:47 +0800

    用户须知

--- src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminUserNoticeController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminUserNoticeController.java
@@ -2,23 +2,16 @@ package com.xiliulou.electricity.controller.admin;
 
 import com.xiliulou.core.controller.BaseController;
 import com.xiliulou.core.web.R;
-import com.xiliulou.electricity.query.FaqQuery;
 import com.xiliulou.electricity.query.UserNoticeQuery;
-import com.xiliulou.electricity.query.VersionNotificationQuery;
-import com.xiliulou.electricity.service.FaqService;
 import com.xiliulou.electricity.service.UserNoticeService;
-import com.xiliulou.electricity.tenant.TenantContextHolder;
 import com.xiliulou.electricity.validator.CreateGroup;
 import com.xiliulou.electricity.validator.UpdateGroup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
