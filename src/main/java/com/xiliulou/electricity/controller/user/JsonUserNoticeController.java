edddcccbb599e07e3ad70853b3d807d292b11fbb
dongfangbubai <15371639767.163.com>
2021-10-09 17:56:48 +0800

    用户须知

---
+++ src/main/java/com/xiliulou/electricity/controller/user/JsonUserNoticeController.java
@@ -0,0 +1,31 @@
+package com.xiliulou.electricity.controller.user;
+
+import com.xiliulou.core.controller.BaseController;
+import com.xiliulou.core.web.R;
+import com.xiliulou.electricity.query.UserNoticeQuery;
+import com.xiliulou.electricity.service.UserNoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : eclair
+ * @date : 2021/9/26 4:45 下午
+ */
+@RestController
+public class JsonUserNoticeController extends BaseController {
+    @Autowired
+    UserNoticeService userNoticeService;
+
+    @GetMapping("/user/userNotice")
+    public R queryUserNotice() {
+
+        return userNoticeService.queryUserNotice();
+    }
+
+
+
+}
