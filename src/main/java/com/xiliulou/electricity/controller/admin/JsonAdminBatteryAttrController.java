08e044d1a178f4193214859ea5e1079b9f576b4b
lucky <15371639767.163.com>
2021-11-03 20:35:04 +0800

    电池查询带时间

--- src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminBatteryAttrController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminBatteryAttrController.java
@@ -4,7 +4,9 @@ import com.xiliulou.clickhouse.service.ClickHouseService;
 import com.xiliulou.core.web.R;
 import com.xiliulou.electricity.entity.BatteryAlert;
 import com.xiliulou.electricity.entity.BatteryAttr;
+import jodd.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 /**
  * @author: Miss.Li
@@ -32,15 +35,24 @@ public class JsonAdminBatteryAttrController {
 	@GetMapping(value = "/admin/battery/attr/list")
 	public R attrList(@RequestParam("sn") String sn,
 			@RequestParam("beginTime") Long beginTime,
-			@RequestParam("endTime") Long endTime) {
+			@RequestParam("endTime") Long endTime,
+			@RequestParam(value = "gsmType", required = false) String gsmType) {
 
 		LocalDateTime beginLocalDateTime = LocalDateTime.ofEpochSecond(beginTime / 1000, 0, ZoneOffset.ofHours(8));
 		LocalDateTime endLocalDateTime = LocalDateTime.ofEpochSecond(endTime / 1000, 0, ZoneOffset.ofHours(8));
 		String begin = formatter.format(beginLocalDateTime);
 		String end = formatter.format(endLocalDateTime);
 
-		String sql = "select * from t_battery_attr where devId=? and createTime>=? AND createTime<=?";
-		return R.ok(clickHouseService.query(BatteryAttr.class, sql, sn,begin,end));
+
+		if(StringUtil.isEmpty(gsmType)){
+			String sql = "select * from t_battery_attr where devId=? and createTime>=? AND createTime<=?";
+			return R.ok(clickHouseService.query(BatteryAttr.class, sql, sn,begin,end));
+		}
+
+
+		//给加的搜索，没什么意义
+		String sql = "select * from t_battery_attr where devId=? and createTime>=? AND createTime<=? AND gsmType=?";
+		return R.ok(clickHouseService.query(BatteryAttr.class, sql, sn,begin,end,gsmType));
 	}
 
 
