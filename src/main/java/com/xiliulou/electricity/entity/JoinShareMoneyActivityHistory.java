eae2a53ce0f26f94dd8a882757760ddc96278469
lucky <15371639767.163.com>
2021-11-18 16:35:20 +0800

    邀请返现活动后台

---
+++ src/main/java/com/xiliulou/electricity/entity/JoinShareMoneyActivityHistory.java
@@ -0,0 +1,80 @@
+package com.xiliulou.electricity.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 参与邀请活动记录(JoinShareActivityRecord)实体类
+ *
+ * @author Eclair
+ * @since 2021-07-14 09:44:36
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@TableName("t_join_share_money_activity_history")
+public class JoinShareMoneyActivityHistory {
+
+    @TableId(value = "id",type = IdType.AUTO)
+    private Long id;
+    /**
+     * 活动id
+     */
+    private Integer activityId;
+
+    /**
+     * recordId
+     */
+    private Long recordId;
+    /**
+    * 邀请用户uid
+    */
+    private Long uid;
+    /**
+    * 参与用户uid
+    */
+    private Long joinUid;
+    /**
+    * 参与开始时间
+    */
+    private Long startTime;
+    /**
+    * 参与过期时间
+    */
+    private Long expiredTime;
+    /**
+    * 参与状态 1--已参与，2--邀请成功，3--已过期，4--被替换  5--已下架
+    */
+    private Integer status;
+    /**
+    * 创建时间
+    */
+    private Long createTime;
+    /**
+    * 修改时间
+    */
+    private Long updateTime;
+    /**
+    * 租户id
+    */
+    private Integer tenantId;
+
+
+    //已参与
+    public static Integer STATUS_INIT = 1;
+    //邀请成功
+    public static Integer STATUS_SUCCESS = 2;
+    //已过期
+    public static Integer STATUS_FAIL = 3;
+    //被替换
+    public static Integer STATUS_REPLACE = 4;
+    //活动已下架
+    public static Integer STATUS_OFF = 5;
+
+}
---
+++ src/main/java/com/xiliulou/electricity/service/JoinShareMoneyActivityHistoryService.java
@@ -0,0 +1,48 @@
+package com.xiliulou.electricity.service;
+
+import com.xiliulou.core.web.R;
+import com.xiliulou.electricity.entity.JoinShareMoneyActivityHistory;
+import com.xiliulou.electricity.query.JsonShareMoneyActivityHistoryQuery;
+
+/**
+ * 参与邀请活动记录(JoinShareActivityRecord)表服务接口
+ *
+ * @author makejava
+ * @since 2021-07-14 09:44:36
+ */
+public interface JoinShareMoneyActivityHistoryService {
+
+    /**
+     * 通过ID查询单条数据从数据库
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    JoinShareMoneyActivityHistory queryByIdFromDB(Long id);
+
+    /**
+     * 新增数据
+     *
+     * @param joinShareMoneyActivityHistory 实例对象
+     * @return 实例对象
+     */
+    JoinShareMoneyActivityHistory insert(JoinShareMoneyActivityHistory joinShareMoneyActivityHistory);
+
+    /**
+     * 修改数据
+     *
+     * @param joinShareMoneyActivityHistory 实例对象
+     * @return 实例对象
+     */
+    Integer update(JoinShareMoneyActivityHistory joinShareMoneyActivityHistory);
+
+	JoinShareMoneyActivityHistory queryByRecordIdAndStatus(Long id);
+
+	R userList(Integer activityId);
+
+	R queryList(JsonShareMoneyActivityHistoryQuery jsonShareMoneyActivityHistoryQuery);
+
+	void updateByActivityId(JoinShareMoneyActivityHistory joinShareMoneyActivityHistory);
+
+	void updateExpired(JoinShareMoneyActivityHistory joinShareMoneyActivityHistory);
+}
