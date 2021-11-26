3617737c34fa535530c01ed191b153d8cfc3bdb9
lucky <15371639767.163.com>
2021-11-26 19:46:56 +0800

    测试

--- src/main/java/com/xiliulou/electricity/service/UserAmountHistoryService.java
+++ src/main/java/com/xiliulou/electricity/service/UserAmountHistoryService.java
@@ -1,8 +1,8 @@
 package com.xiliulou.electricity.service;
 
 import com.xiliulou.core.web.R;
-import com.xiliulou.electricity.entity.FranchiseeSplitAccountHistory;
-import com.xiliulou.electricity.query.FranchiseeAccountQuery;
+import com.xiliulou.electricity.entity.UserAmountHistory;
+import com.xiliulou.electricity.query.UserAmountHistoryQuery;
 
 /**
  * (FranchiseeSplitAccountHistory)表服务接口
@@ -15,12 +15,12 @@ public interface UserAmountHistoryService {
     /**
      * 新增数据
      *
-     * @param franchiseeSplitAccountHistory 实例对象
+     * @param userAmountHistory 实例对象
      * @return 实例对象
      */
-    FranchiseeSplitAccountHistory insert(FranchiseeSplitAccountHistory franchiseeSplitAccountHistory);
+    UserAmountHistory insert(UserAmountHistory userAmountHistory);
 
-    R queryList(FranchiseeAccountQuery franchiseeAccountQuery);
+    R queryList(UserAmountHistoryQuery userAmountHistoryQuery);
 
-    R queryCount(FranchiseeAccountQuery franchiseeAccountQuery);
+    R queryCount(UserAmountHistoryQuery userAmountHistoryQuery);
 }
--- src/main/java/com/xiliulou/electricity/service/impl/UserAmountHistoryServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/UserAmountHistoryServiceImpl.java
@@ -2,8 +2,11 @@ package com.xiliulou.electricity.service.impl;
 
 import com.xiliulou.core.web.R;
 import com.xiliulou.electricity.entity.FranchiseeSplitAccountHistory;
+import com.xiliulou.electricity.entity.UserAmountHistory;
 import com.xiliulou.electricity.mapper.FranchiseeSplitAccountHistoryMapper;
+import com.xiliulou.electricity.mapper.UserAmountHistoryMapper;
 import com.xiliulou.electricity.query.FranchiseeAccountQuery;
+import com.xiliulou.electricity.query.UserAmountHistoryQuery;
 import com.xiliulou.electricity.service.UserAmountHistoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,30 +24,30 @@ import javax.annotation.Resource;
 @Slf4j
 public class UserAmountHistoryServiceImpl implements UserAmountHistoryService {
     @Resource
-    private FranchiseeSplitAccountHistoryMapper franchiseeSplitAccountHistoryMapper;
+    private UserAmountHistoryMapper userAmountHistoryMapper;
 
 
     /**
      * 新增数据
      *
-     * @param franchiseeSplitAccountHistory 实例对象
+     * @param userAmountHistory 实例对象
      * @return 实例对象
      */
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public FranchiseeSplitAccountHistory insert(FranchiseeSplitAccountHistory franchiseeSplitAccountHistory) {
-        this.franchiseeSplitAccountHistoryMapper.insert(franchiseeSplitAccountHistory);
-        return franchiseeSplitAccountHistory;
+    public UserAmountHistory insert(UserAmountHistory userAmountHistory) {
+        this.userAmountHistoryMapper.insert(userAmountHistory);
+        return userAmountHistory;
     }
 
     @Override
-    public R queryList(FranchiseeAccountQuery franchiseeAccountQuery) {
-        return R.ok(franchiseeSplitAccountHistoryMapper.queryList(franchiseeAccountQuery));
+    public R queryList(UserAmountHistoryQuery userAmountHistoryQuery) {
+        return R.ok(userAmountHistoryMapper.queryList(userAmountHistoryQuery));
     }
 
     @Override
-    public R queryCount(FranchiseeAccountQuery franchiseeAccountQuery) {
-        return R.ok(franchiseeSplitAccountHistoryMapper.queryCount(franchiseeAccountQuery));
+    public R queryCount(UserAmountHistoryQuery userAmountHistoryQuery) {
+        return R.ok(userAmountHistoryMapper.queryCount(userAmountHistoryQuery));
     }
 
 }
