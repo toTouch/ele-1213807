d39e11411833f0908913aaec3dd43204fa7a0952
zgw <1311510078@qq.com>
2021-12-01 11:23:30 +0800

    bug fix

--- src/main/java/com/xiliulou/electricity/service/impl/WechatAccessTokenSeviceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/WechatAccessTokenSeviceImpl.java
@@ -2,12 +2,14 @@ package com.xiliulou.electricity.service.impl;
 
 import com.xiliulou.core.wp.beanposstprocessor.WechatAccessTokenSevice;
 import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Hardy
  * @date 2021/12/1 11:13
  * @mood
  */
+@Service
 public class WechatAccessTokenSeviceImpl implements WechatAccessTokenSevice {
     @Override
     public Pair<Boolean, Object> getAccessToken(String appId, String secret) {
