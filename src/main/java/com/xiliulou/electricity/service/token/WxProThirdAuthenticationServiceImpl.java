e65ac971c9d18082bf3e38f2f429b3024bfea5cc
lucky <15371639767.163.com>
2021-12-03 10:57:28 +0800

    测试

--- src/main/java/com/xiliulou/electricity/service/token/WxProThirdAuthenticationServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/token/WxProThirdAuthenticationServiceImpl.java
@@ -440,6 +440,7 @@ public class WxProThirdAuthenticationServiceImpl implements ThirdAuthenticationS
 
 		//参加新用户活动
 		NewUserActivity newUserActivity=newUserActivityService.queryActivity();
+		log.info("newUserActivity is -->{}",newUserActivity);
 		if(Objects.nonNull(newUserActivity)){
 
 
