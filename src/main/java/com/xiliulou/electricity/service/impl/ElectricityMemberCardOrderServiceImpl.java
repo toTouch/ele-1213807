8f8443fa7654169f9b5bbc7ead3377780064073e
imagine <1325648013@qq.com>
2022-05-12 15:52:55 +0800

    0元购卡

--- src/main/java/com/xiliulou/electricity/service/impl/ElectricityMemberCardOrderServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/ElectricityMemberCardOrderServiceImpl.java
@@ -273,8 +273,6 @@ public class ElectricityMemberCardOrderServiceImpl extends ServiceImpl<Electrici
         //同一个套餐可以续费
         if (Objects.equals(franchiseeUserInfo.getCardId(), electricityMemberCardOrderQuery.getMemberId())) {
             if (now < franchiseeUserInfo.getMemberCardExpireTime() && Objects.nonNull(franchiseeUserInfo.getRemainingNumber()) && franchiseeUserInfo.getRemainingNumber()>0) {
-
-                System.out.println("时间叠加=====================================");
                 now = franchiseeUserInfo.getMemberCardExpireTime();
             }
             //TODO 使用次数暂时叠加
@@ -342,12 +340,6 @@ public class ElectricityMemberCardOrderServiceImpl extends ServiceImpl<Electrici
             franchiseeUserInfoUpdate.setId(franchiseeUserInfo.getId());
             Long memberCardExpireTime = now +
                     electricityMemberCardOrder.getValidDays() * (24 * 60 * 60 * 1000L);
-
-            System.out.println("可用天数==========================="+electricityMemberCardOrder.getValidDays());
-            System.out.println("当前时间--==========================="+now);
-
-
-            System.out.println("入库时间============================"+memberCardExpireTime);
             franchiseeUserInfoUpdate.setMemberCardExpireTime(memberCardExpireTime);
             franchiseeUserInfoUpdate.setBatteryServiceFeeGenerateTime(memberCardExpireTime);
             franchiseeUserInfoUpdate.setRemainingNumber(remainingNumber);
