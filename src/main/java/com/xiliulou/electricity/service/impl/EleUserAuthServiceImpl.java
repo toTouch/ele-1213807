1f3c98cf2a7c0072cda7168307e1334c6f19038c
huruipeng <1325648013@qq.com>
2022-06-22 15:36:23 +0800

    我的车辆

--- src/main/java/com/xiliulou/electricity/service/impl/EleUserAuthServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/EleUserAuthServiceImpl.java
@@ -227,18 +227,7 @@ public class EleUserAuthServiceImpl implements EleUserAuthService {
 			return R.fail("ELECTRICITY.0001", "未找到用户");
 		}
 
-		//租车未购买套餐
-		if (Objects.equals(franchiseeUserInfo.getRentCarStatus(), FranchiseeUserInfo.RENT_CAR_STATUS_IS_DEPOSIT) && Objects.isNull(franchiseeUserInfo.getRentCarMemberCardExpireTime())) {
-			log.error("order ERROR! not rent car member card! uid:{}", user.getUid());
-			return R.fail("100012", "未购买租车套餐");
-		}
-
 		Long now = System.currentTimeMillis();
-		if (Objects.nonNull(franchiseeUserInfo.getRentCarMemberCardExpireTime()) && franchiseeUserInfo.getRentCarMemberCardExpireTime() < now) {
-			log.error("order ERROR! rent car memberCard  is Expire ! uid:{}", user.getUid());
-			return R.fail("100013", "租车套餐已过期");
-		}
-
 		if (!Objects.equals(franchiseeUserInfo.getServiceStatus(), FranchiseeUserInfo.STATUS_IS_INIT)) {
 			serviceStatus = franchiseeUserInfo.getServiceStatus();
 		}
