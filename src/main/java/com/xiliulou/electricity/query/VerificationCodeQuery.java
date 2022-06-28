112585e4023bb52144fa4d5f467b10aa292e3be4
zhaozhilong <zhaozhilong@xiliulou.com>
2022-06-28 15:36:02 +0800

    fix(动态验证码)：参数校验异常

--- src/main/java/com/xiliulou/electricity/query/VerificationCodeQuery.java
+++ src/main/java/com/xiliulou/electricity/query/VerificationCodeQuery.java
@@ -1,8 +1,10 @@
 package com.xiliulou.electricity.query;
 
 import com.xiliulou.electricity.validator.CreateGroup;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
@@ -12,6 +14,8 @@ import javax.validation.constraints.NotNull;
  * @date 2022-06-28-14:01
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class VerificationCodeQuery {
 
