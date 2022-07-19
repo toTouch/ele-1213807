3eba4b7561814384cfff46cf99708c5591d32cf8
huruipeng <1325648013@qq.com>
2022-07-19 20:20:10 +0800

    退押金操作记录

--- src/main/java/com/xiliulou/electricity/service/impl/ElectricityMemberCardOrderServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/ElectricityMemberCardOrderServiceImpl.java
@@ -920,7 +920,7 @@ public class ElectricityMemberCardOrderServiceImpl extends ServiceImpl<Electrici
             memberCardExpireTime = oldFranchiseeUserInfo.getMemberCardExpireTime();
         }
         if (Objects.equals(memberCardOrderAddAndUpdate.getMaxUseCount(), MemberCardOrderAddAndUpdate.ZERO_USER_COUNT) || Objects.nonNull(memberCardOrderAddAndUpdate.getValidDays()) && Objects.equals(memberCardOrderAddAndUpdate.getValidDays(), MemberCardOrderAddAndUpdate.ZERO_VALIdDAY_MEMBER_CARD) && (oldFranchiseeUserInfo.getMemberCardExpireTime() - System.currentTimeMillis()) / 1000 / 60 / 60 / 24 != MemberCardOrderAddAndUpdate.ZERO_VALIdDAY_MEMBER_CARD) {
-            memberCardExpireTime = null;
+            memberCardExpireTime = System.currentTimeMillis();
         }
 
         franchiseeUserInfoUpdate.setId(oldFranchiseeUserInfo.getId());
