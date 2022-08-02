e636c8e6573b13b5af246dfabd5d67f28afdeeed
huruipeng <1325648013@qq.com>
2022-08-02 09:55:53 +0800

    换电查询订单状态

--- src/main/java/com/xiliulou/electricity/service/impl/ElectricityCabinetOrderServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/ElectricityCabinetOrderServiceImpl.java
@@ -902,7 +902,10 @@ public class ElectricityCabinetOrderServiceImpl implements ElectricityCabinetOrd
 
 
         //rent
-        if (Objects.equals(electricityCabinetOrder.getStatus(), ElectricityCabinetOrder.INIT_BATTERY_CHECK_SUCCESS)) {
+        if (Objects.equals(electricityCabinetOrder.getStatus(), ElectricityCabinetOrder.INIT_BATTERY_CHECK_SUCCESS)
+                || Objects.equals(electricityCabinetOrder.getStatus(), ElectricityCabinetOrder.INIT_OPEN_SUCCESS)
+                || Objects.equals(electricityCabinetOrder.getStatus(), ElectricityCabinetOrder.COMPLETE_OPEN_SUCCESS)
+                || Objects.equals(electricityCabinetOrder.getStatus(), ElectricityCabinetOrder.COMPLETE_BATTERY_TAKE_SUCCESS)) {
             picture = 2;
         }
 
@@ -1488,18 +1491,18 @@ public class ElectricityCabinetOrderServiceImpl implements ElectricityCabinetOrd
     private Triple<Boolean, String, String> checkUserExistsUnFinishOrder(Long uid) {
         RentBatteryOrder rentBatteryOrder = rentBatteryOrderService.queryByUidAndType(uid);
         if (Objects.nonNull(rentBatteryOrder) && Objects.equals(rentBatteryOrder.getType(), RentBatteryOrder.TYPE_USER_RENT)) {
-            return Triple.of(true,"100200", "存在未完成租电订单，不能下单");
+            return Triple.of(true, "100200", "存在未完成租电订单，不能下单");
         } else if (Objects.nonNull(rentBatteryOrder) && Objects.equals(rentBatteryOrder.getType(), RentBatteryOrder.TYPE_USER_RETURN)) {
-            return Triple.of(true,"100202", "存在未完成租电订单，不能下单");
+            return Triple.of(true, "100202", "存在未完成租电订单，不能下单");
         }
 
         //是否存在未完成的换电订单
         ElectricityCabinetOrder oldElectricityCabinetOrder = queryByUid(uid);
         if (Objects.nonNull(oldElectricityCabinetOrder)) {
-            return Triple.of(true,"100201", "存在未完成换电订单，不能下单");
+            return Triple.of(true, "100201", "存在未完成换电订单，不能下单");
         }
 
-        return Triple.of(false,null,null);
+        return Triple.of(false, null, null);
     }
 
 }
