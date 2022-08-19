b6db615fe3f87c70947659d38b123e60715996f4
huruipeng <1325648013@qq.com>
2022-08-19 15:54:13 +0800

    流量统计

--- src/main/java/com/xiliulou/electricity/vo/ElectricityCabinetTrafficVo.java
+++ src/main/java/com/xiliulou/electricity/vo/ElectricityCabinetTrafficVo.java
@@ -10,7 +10,7 @@ public class ElectricityCabinetTrafficVo {
     private Long electricityCabinetId;
     private String electricityCabinetName;
     private BigDecimal sameDayTraffic;
-    private BigDecimal sumPower;
+    private BigDecimal sumTraffic;
     private String date;
     private Long createTime;
     private Long updateTime;
