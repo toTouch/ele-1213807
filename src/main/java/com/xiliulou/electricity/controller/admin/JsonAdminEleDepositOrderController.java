1f417728b20a0c6455ae2fa338a6a049a7fad281
zgw <1311510078@qq.com>
2022-09-27 14:19:27 +0800

    bug fix

--- src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminEleDepositOrderController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminEleDepositOrderController.java
@@ -115,8 +115,7 @@ public class JsonAdminEleDepositOrderController {
                 .tenantId(tenantId)
                 .carModel(carModel)
                 .franchiseeName(franchiseeName)
-                .depositType(depositType)
-                .payType(payType).franchiseeId(franchiseeId).storeName(storeName).build();
+                .depositType(depositType).payType(payType).franchiseeId(franchiseeId).storeName(storeName).build();
         return eleDepositOrderService.queryList(eleDepositOrderQuery);
     }
 
@@ -131,7 +130,8 @@ public class JsonAdminEleDepositOrderController {
                         @RequestParam(value = "depositType", required = false) Integer depositType,
                         @RequestParam(value = "carModel", required = false) String carModel,
                         @RequestParam(value = "franchiseeName", required = false) String franchiseeName,
-                        @RequestParam(value = "payType", required = false) Integer payType) {
+        @RequestParam(value = "payType", required = false) Integer payType,
+        @RequestParam(value = "storeName", required = false) String storeName) {
 
         //租户
         Integer tenantId = TenantContextHolder.getTenantId();
@@ -176,8 +176,7 @@ public class JsonAdminEleDepositOrderController {
                 .depositType(depositType)
                 .payType(payType)
                 .tenantId(tenantId)
-                .franchiseeName(franchiseeName)
-                .franchiseeId(franchiseeId).build();
+                .franchiseeName(franchiseeName).franchiseeId(franchiseeId).storeName(storeName).build();
 
         return eleDepositOrderService.queryCount(eleDepositOrderQuery);
     }
