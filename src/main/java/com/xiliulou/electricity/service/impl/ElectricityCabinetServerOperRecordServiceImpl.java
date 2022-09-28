95887ac37cb7d2fd47bba796df712e3411e4bf69
zgw <1311510078@qq.com>
2022-09-28 17:32:09 +0800

    bug fix

--- src/main/java/com/xiliulou/electricity/service/impl/ElectricityCabinetServerOperRecordServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/ElectricityCabinetServerOperRecordServiceImpl.java
@@ -1,10 +1,23 @@
 package com.xiliulou.electricity.service.impl;
 
+import com.xiliulou.core.utils.DataUtil;
 import com.xiliulou.core.web.R;
+import com.xiliulou.electricity.entity.ElectricityCabinet;
+import com.xiliulou.electricity.entity.ElectricityCabinetServer;
 import com.xiliulou.electricity.entity.ElectricityCabinetServerOperRecord;
+import com.xiliulou.electricity.entity.Tenant;
 import com.xiliulou.electricity.mapper.ElectricityCabinetServerOperRecordMapper;
 import com.xiliulou.electricity.service.ElectricityCabinetServerOperRecordService;
+import com.xiliulou.electricity.service.ElectricityCabinetServerService;
+import com.xiliulou.electricity.service.ElectricityCabinetService;
+import com.xiliulou.electricity.service.TenantService;
+import com.xiliulou.electricity.vo.ElectricityCabinetServerOperRecordVo;
+import com.xiliulou.electricity.vo.ElectricityCabinetServerVo;
 import com.xiliulou.electricity.vo.PageDataAndCountVo;
+import java.util.ArrayList;
+import java.util.Objects;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +34,9 @@ import lombok.extern.slf4j.Slf4j;
 @Service("electricityCabinetServerOperRecordService") @Slf4j public class ElectricityCabinetServerOperRecordServiceImpl
     implements ElectricityCabinetServerOperRecordService {
     @Resource private ElectricityCabinetServerOperRecordMapper electricityCabinetServerOperRecordMapper;
+    @Autowired private ElectricityCabinetService electricityCabinetService;
+    @Autowired private ElectricityCabinetServerService electricityCabinetServerService;
+    @Autowired private TenantService tenantService;
 
     /**
      * 通过ID查询单条数据从DB
@@ -91,7 +107,37 @@ import lombok.extern.slf4j.Slf4j;
         List<ElectricityCabinetServerOperRecord> data =
             electricityCabinetServerOperRecordMapper.queryList(createUserName, eleServerId, offset, size);
 
+        List<ElectricityCabinetServerOperRecordVo> result = new ArrayList<>();
+        if (DataUtil.collectionIsUsable(data)) {
+            data.forEach(item -> {
+                ElectricityCabinetServerOperRecordVo vo = new ElectricityCabinetServerOperRecordVo();
+                BeanUtils.copyProperties(item, vo);
+
+                ElectricityCabinetServer electricityCabinetServer =
+                    electricityCabinetServerService.queryByIdFromDB(item.getEleServerId());
+                if (Objects.isNull(electricityCabinetServer)) {
+                    log.error(
+                        "QUERY ELECTRICITY_CABINET_SERVER_OPER_RECORD ERROR! electricityCabinetServer is null id={}",
+                        item.getId());
+                    return;
+                }
+
+                ElectricityCabinet electricityCabinet =
+                    electricityCabinetService.queryByIdFromCache(electricityCabinetServer.getElectricityCabinetId());
+                if (Objects.nonNull(electricityCabinet)) {
+                    vo.setEleName(electricityCabinet.getName());
+                }
+
+                Tenant tenant = tenantService.queryByIdFromCache(electricityCabinetServer.getTenantId());
+                if (Objects.nonNull(tenant)) {
+                    vo.setTenantName(tenant.getName());
+                }
+
+                result.add(vo);
+            });
+        }
+
         Long count = electricityCabinetServerOperRecordMapper.queryCount(createUserName, eleServerId);
-        return R.ok(new PageDataAndCountVo<>(data, count));
+        return R.ok(new PageDataAndCountVo<>(result, count));
     }
 }
