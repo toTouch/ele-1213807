ab66bba3e4087b4301de225a9a1341d1c473b83a
zgw <1311510078@qq.com>
2022-10-08 10:49:58 +0800

    bug fix

--- src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminElectricityCabinetServerController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminElectricityCabinetServerController.java
@@ -3,6 +3,7 @@ package com.xiliulou.electricity.controller.admin;
 import com.xiliulou.core.web.R;
 import com.xiliulou.electricity.entity.ElectricityCabinetServer;
 import com.xiliulou.electricity.service.ElectricityCabinetServerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -13,14 +14,15 @@ import javax.annotation.Resource;
  * @author zgw
  * @since 2022-09-26 11:40:36
  */
-@RestController @RequestMapping("admin/electricityCabinetServer/")
+@RestController @Slf4j
 public class JsonAdminElectricityCabinetServerController {
     /**
      * 服务对象
      */
     @Resource private ElectricityCabinetServerService electricityCabinetServerService;
 
-    @GetMapping("queryList") public R queryList(@RequestParam(value = "eleName", required = false) String eleName,
+    @GetMapping("admin/electricityCabinetServer/queryList")
+    public R queryList(@RequestParam(value = "eleName", required = false) String eleName,
         @RequestParam(value = "deviceName", required = false) String deviceName,
         @RequestParam(value = "tenantName", required = false) String tenantName,
         @RequestParam(value = "serverBeginTime", required = false) Long serverBeginTime,
@@ -31,11 +33,11 @@ public class JsonAdminElectricityCabinetServerController {
             .queryList(eleName, deviceName, tenantName, serverBeginTime, serverEndTime, offset, size);
     }
 
-    @DeleteMapping("/delete/{id}") public R deleteOne(@PathVariable("id") Long id) {
+    @DeleteMapping("admin/electricityCabinetServer/delete/{id}") public R deleteOne(@PathVariable("id") Long id) {
         return electricityCabinetServerService.deleteOne(id);
     }
 
-    @PutMapping("/update") public R updateOne(@RequestParam(value = "id") Long id,
+    @PutMapping("admin/electricityCabinetServer/update") public R updateOne(@RequestParam(value = "id") Long id,
         @RequestParam(value = "serverBeginTime") Long serverBeginTime,
         @RequestParam(value = "serverEndTime") Long serverEndTime) {
         return electricityCabinetServerService.updateOne(id, serverBeginTime, serverEndTime);
