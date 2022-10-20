13ae5e98b9885b76b8b02406cf7bd061af77914b
huruipeng <1325648013@qq.com>
2022-10-20 18:31:17 +0800

    后台编辑套餐过期将原套餐次数至为0

--- src/main/java/com/xiliulou/electricity/service/impl/ElectricityMemberCardOrderServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/ElectricityMemberCardOrderServiceImpl.java
@@ -1159,6 +1159,7 @@ public class ElectricityMemberCardOrderServiceImpl extends ServiceImpl<Electrici
 //            memberCardExpireTime = oldFranchiseeUserInfo.getMemberCardExpireTime();
 //        }
         if (Objects.equals(memberCardOrderAddAndUpdate.getMaxUseCount(), MemberCardOrderAddAndUpdate.ZERO_USER_COUNT) || Objects.nonNull(memberCardOrderAddAndUpdate.getValidDays()) && Objects.equals(memberCardOrderAddAndUpdate.getValidDays(), MemberCardOrderAddAndUpdate.ZERO_VALIdDAY_MEMBER_CARD) && (oldFranchiseeUserInfo.getMemberCardExpireTime() - System.currentTimeMillis()) / 1000 / 60 / 60 / 24 != MemberCardOrderAddAndUpdate.ZERO_VALIdDAY_MEMBER_CARD) {
+            remainingNumber=MemberCardOrderAddAndUpdate.ZERO_USER_COUNT;
             memberCardExpireTime = System.currentTimeMillis();
         }
 
