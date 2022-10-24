3292cad62a62b163ddd6c8d8c0a4b5d51de81115
zgw <1311510078@qq.com>
2022-10-24 18:59:23 +0800

    fix(套餐过期提醒):修改租车套餐名称显示问题

--- src/main/java/com/xiliulou/electricity/service/impl/ElectricityMemberCardOrderServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/ElectricityMemberCardOrderServiceImpl.java
@@ -1485,7 +1485,7 @@ public class ElectricityMemberCardOrderServiceImpl extends ServiceImpl<Electrici
 
         data.put("thing2", carMemberCardExpiringSoonQuery.getCardName());
         data.put("date4", carMemberCardExpiringSoonQuery.getRentCarMemberCardExpireTimeStr());
-        data.put("thing3", "租车套餐即将过期，为避免影响您的使用请及时续费。");
+        data.put("thing3", "租车套餐即将过期，请及时续费。");
 
         log.info("CAR MEMBER CARD EXPIRING REMINDER: param={}", carMemberCardExpiringSoonQuery);
 
@@ -1505,7 +1505,7 @@ public class ElectricityMemberCardOrderServiceImpl extends ServiceImpl<Electrici
 
         data.put("thing2", batteryMemberCardExpiringSoonQuery.getCardName());
         data.put("date4", batteryMemberCardExpiringSoonQuery.getMemberCardExpireTimeStr());
-        data.put("thing3", "电池套餐即将过期，为避免影响您的使用请及时续费。");
+        data.put("thing3", "电池套餐即将过期，请及时续费。");
 
         log.info("BATTERY MEMBER CARD EXPIRING REMINDER: param={}", batteryMemberCardExpiringSoonQuery);
 
