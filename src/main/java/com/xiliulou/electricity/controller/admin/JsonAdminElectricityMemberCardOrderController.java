3b7cd234a55b26027e23cad3c2dd078c383ca5fa
zhaozhilong <zhaozhilong@xiliulou.com>
2022-11-03 18:12:51 +0800

    fix(购卡记录导出)

--- src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminElectricityMemberCardOrderController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminElectricityMemberCardOrderController.java
@@ -178,9 +178,9 @@ public class JsonAdminElectricityMemberCardOrderController {
 			throw new CustomBusinessException("查不到订单");
 		}
 		
-		if(!SecurityUtils.isAdmin() || !Objects.equals(user.getType(), User.TYPE_USER_OPERATE) || !Objects.equals(user.getType(), User.TYPE_USER_FRANCHISEE)){
-			throw new CustomBusinessException("没有权限！");
-		}
+//		if(!SecurityUtils.isAdmin() || !Objects.equals(user.getType(), User.TYPE_USER_OPERATE) || !Objects.equals(user.getType(), User.TYPE_USER_FRANCHISEE)){
+//			throw new CustomBusinessException("没有权限！");
+//		}
 
 		Long franchiseeId=null;
 		if (Objects.equals(user.getType(), User.TYPE_USER_FRANCHISEE)) {
