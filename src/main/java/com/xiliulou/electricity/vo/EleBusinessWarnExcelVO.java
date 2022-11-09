d37066c5c196eb0cf0ef82f4c05ad62f90dcdeb2
zhaozhilong <zhaozhilong@xiliulou.com>
2022-11-09 15:17:42 +0800

    fix(异常告警导出)：调整Exel列顺序

--- src/main/java/com/xiliulou/electricity/vo/EleBusinessWarnExcelVO.java
+++ src/main/java/com/xiliulou/electricity/vo/EleBusinessWarnExcelVO.java
@@ -7,12 +7,13 @@ import lombok.Data;
 public class EleBusinessWarnExcelVO {
     @ExcelProperty("序号")
     private Integer id;
-    @ExcelProperty("描述")
-    private String errorMsg;
-    @ExcelProperty("格挡")
-    private Integer cellNo;
-    @ExcelProperty("告警时间")
-    private String reportTime;
     @ExcelProperty("柜机")
     private String cabinetName;
+    @ExcelProperty("格挡")
+    private Integer cellNo;
+    @ExcelProperty("描述")
+    private String errorMsg;
+    @ExcelProperty("告警时间")
+    private String reportTime;
+
 }
--- src/main/java/com/xiliulou/electricity/vo/EleCabinetWarnExcelVO.java
+++ src/main/java/com/xiliulou/electricity/vo/EleCabinetWarnExcelVO.java
@@ -8,12 +8,13 @@ public class EleCabinetWarnExcelVO {
 
     @ExcelProperty("序号")
     private Integer id;
+    @ExcelProperty("柜机")
+    private String cabinetName;
     @ExcelProperty("描述")
     private String errorMsg;
     @ExcelProperty("告警时间")
     private String reportTime;
-    @ExcelProperty("柜机")
-    private String cabinetName;
+
 
 
 }
