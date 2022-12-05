6d949bb73329a18f63f611dc26bdcfcc42fc584b
eclair <eclair@qq.com>
2022-12-05 18:03:47 +0800

    修改filter

--- src/main/java/com/xiliulou/electricity/filter/RequestFilter.java
+++ src/main/java/com/xiliulou/electricity/filter/RequestFilter.java
@@ -33,43 +33,50 @@ import java.util.Objects;
 @Component("newRequestInterceptor")
 @Order(10)
 public class RequestFilter implements Filter {
-
+    
     @Autowired
     CollectorRegistry collectorRegistry;
-
+    
     public static final String REQUEST_ID = "requestId";
-
+    
     private static final String REQ_TIME = "req_time";
+    
     private static final String REQ_HISTOGRAM_TIMER = "req_histogram_time";
-
-    static final Gauge PROGRESSING_REQUESTS = Gauge.build().name("progressing_requests").help("Inprogress requesets.").register();
-    static final Histogram REQUEST_LATENCY_HISTOGRAM = Histogram.build().labelNames("path", "method", "code")
-            .buckets(0.2, 0.5, 0.75, 0.9, 1, 3, 10)
-            .name("http_request_latency_histogram").help("Request latency in seconds.")
+    
+    static final Gauge PROGRESSING_REQUESTS = Gauge.build().name("progressing_requests").help("Inprogress requesets.")
             .register();
-
+    
+    static final Gauge REQUEST_LATENCY_GAUGE = Gauge.build().name("progressing_requests")
+            .labelNames("path", "method", "code").help("request_latency_gauge").register();
+    
+    static final Histogram REQUEST_LATENCY_HISTOGRAM = Histogram.build().labelNames("path", "method", "code")
+            .buckets(0.2, 0.5, 0.75, 0.9, 1, 3, 10).name("http_request_latency_histogram")
+            .help("Request latency in seconds.").register();
+    
     @PostConstruct
     public void init() {
         collectorRegistry.register(PROGRESSING_REQUESTS);
         collectorRegistry.register(REQUEST_LATENCY_HISTOGRAM);
+        collectorRegistry.register(REQUEST_LATENCY_GAUGE);
     }
-
-    public void afterCompletion(String ip, HttpServletRequest request, String requestId) {
+    
+    public void afterCompletion(String ip, HttpServletRequest request, String params,
+            HttpServletResponse httpServletResponse) {
         PROGRESSING_REQUESTS.dec();
-
+        
         Object attribute = request.getAttribute(REQ_TIME);
         Object timerObj = request.getAttribute(REQ_HISTOGRAM_TIMER);
         if (Objects.isNull(attribute) || StrUtil.isEmpty(attribute.toString())) {
             return;
         }
-
+        
         if (Objects.isNull(timerObj)) {
             return;
         }
-
+        
         Histogram.Timer timer = (Histogram.Timer) timerObj;
         timer.observeDuration();
-
+        
         Long startTime = null;
         try {
             startTime = Long.parseLong(attribute.toString());
@@ -79,11 +86,19 @@ public class RequestFilter implements Filter {
         if (Objects.isNull(startTime)) {
             return;
         }
+        
         Long uid = SecurityUtils.getUid();
-        log.info("ip={} uid={} method={} uri={} time={}", ip, uid, request.getMethod(), request.getRequestURI(), System.currentTimeMillis() - startTime);
+        long spendTimeMills = System.currentTimeMillis() - startTime;
+        
+        REQUEST_LATENCY_GAUGE.labels(request.getRequestURI(),
+                request.getMethod(), String.valueOf(httpServletResponse.getStatus())).set(spendTimeMills);
+        log.info("ip={} uid={} method={} uri={} params={} time={}", ip, uid, request.getMethod(),
+                request.getRequestURI(), params, spendTimeMills);
 
+        
+        
     }
-
+    
     private String getRequestBody(HttpServletRequest request) {
         int contentLength = request.getContentLength();
         if (contentLength <= 0) {
@@ -96,42 +111,51 @@ public class RequestFilter implements Filter {
             return "";
         }
     }
-
+    
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
         String header = httpServletRequest.getHeader(HttpHeaders.CONTENT_TYPE);
-
+        
         PROGRESSING_REQUESTS.inc();
-        Histogram.Timer requestTimer = REQUEST_LATENCY_HISTOGRAM.labels(httpServletRequest.getRequestURI(), httpServletRequest.getMethod(), String.valueOf(httpServletResponse.getStatus())).startTimer();
-
-        if (Objects.isNull(httpServletRequest.getAttribute(REQ_TIME)) || StrUtil.isEmpty(httpServletRequest.getAttribute(REQ_TIME).toString())) {
+        Histogram.Timer requestTimer = REQUEST_LATENCY_HISTOGRAM.labels(httpServletRequest.getRequestURI(),
+                httpServletRequest.getMethod(), String.valueOf(httpServletResponse.getStatus())).startTimer();
+        
+        if (Objects.isNull(httpServletRequest.getAttribute(REQ_TIME)) || StrUtil.isEmpty(
+                httpServletRequest.getAttribute(REQ_TIME).toString())) {
             httpServletRequest.setAttribute(REQ_TIME, System.currentTimeMillis());
             httpServletRequest.setAttribute(REQ_HISTOGRAM_TIMER, requestTimer);
         }
-
-        Long uid = SecurityUtils.getUid();
+        
         String ip = WebUtils.getIP(httpServletRequest);
         String requestId = IdUtil.simpleUUID();
-
+        
         httpServletRequest.setAttribute(REQUEST_ID, requestId);
-
-        if (StrUtil.isEmpty(header) || header.startsWith(MediaType.MULTIPART_FORM_DATA_VALUE) || header.startsWith(MediaType.APPLICATION_FORM_URLENCODED_VALUE)) {
-            log.info("ip={} uid={} method={} uri={} params={}", ip, uid, httpServletRequest.getMethod(), httpServletRequest.getRequestURI(), JsonUtil.toJson(httpServletRequest.getParameterMap()));
-            filterChain.doFilter(httpServletRequest, servletResponse);
-        } else {
-            httpServletRequest = new BodyReaderHttpServletRequestWrapper(httpServletRequest);
-
-            if (header.startsWith(MediaType.APPLICATION_JSON_VALUE)) {
-                log.info("ip={} uid={} method={} uri={} params={}", ip, uid, httpServletRequest.getMethod(), httpServletRequest.getRequestURI(), getRequestBody(httpServletRequest));
+        
+        String params = null;
+        try {
+            if (StrUtil.isEmpty(header) || header.startsWith(MediaType.MULTIPART_FORM_DATA_VALUE) || header.startsWith(
+                    MediaType.APPLICATION_FORM_URLENCODED_VALUE)) {
+                params = JsonUtil.toJson(httpServletRequest.getParameterMap());
+                filterChain.doFilter(httpServletRequest, servletResponse);
             } else {
-                log.info("ip={} uid={} method={} uri={} params={}", ip, uid, httpServletRequest.getMethod(), httpServletRequest.getRequestURI(), JsonUtil.toJson(httpServletRequest.getParameterMap()));
+                httpServletRequest = new BodyReaderHttpServletRequestWrapper(httpServletRequest);
+        
+                if (header.startsWith(MediaType.APPLICATION_JSON_VALUE)) {
+                    params = getRequestBody(httpServletRequest);
+                } else {
+                    params = JsonUtil.toJson(httpServletRequest.getParameterMap());
+                }
+        
+                filterChain.doFilter(httpServletRequest, servletResponse);
             }
-
-            filterChain.doFilter(httpServletRequest, servletResponse);
+        }finally {
+            afterCompletion(ip, httpServletRequest, params,httpServletResponse);
+    
         }
-
-        afterCompletion(ip, httpServletRequest, requestId);
+       
+        
     }
 }
