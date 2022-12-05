0e6e2fbf7642b1cea5225cbc18210b2a94d4f831
huruipeng <1325648013@qq.com>
2022-12-05 17:06:27 +0800

    新版停卡

--- src/main/java/com/xiliulou/electricity/service/impl/ElectricityMemberCardOrderServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/ElectricityMemberCardOrderServiceImpl.java
@@ -2100,7 +2100,6 @@ public class ElectricityMemberCardOrderServiceImpl extends ServiceImpl<Electrici
     }
 
     @Override
-    @Transactional(rollbackFor = Exception.class)
     public void systemEnableMemberCardTask() {
 
         int offset = 0;
