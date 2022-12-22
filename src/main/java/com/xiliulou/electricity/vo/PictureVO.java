7d6bfa9048c9c5a8d3f0ea4ee8e408d66e2e1c96
zhaozhilong <zhaozhilong@xiliulou.com>
2022-12-22 17:29:27 +0800

    fix(租车)

---
+++ src/main/java/com/xiliulou/electricity/vo/PictureVO.java
@@ -0,0 +1,59 @@
+package com.xiliulou.electricity.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author zzlong
+ * @email zhaozhilong@xiliulou.com
+ * @date 2022-12-22-17:25
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PictureVO {
+
+    /**
+     * Id
+     */
+    private Long id;
+    /**
+     * 业务ID
+     */
+    private Long businessId;
+    /**
+     * 图片
+     */
+    private String pictureUrl;
+
+    private String pictureOSSUrl;
+    /**
+     * 顺序
+     */
+    private Integer seq;
+    /**
+     * 图片类型 0:小程序
+     */
+    private Integer imgType;
+    /**
+     * 状态(0为启用,1为禁用)
+     */
+    private Integer status;
+    /**
+     * 0--正常 1--删除
+     */
+    private Integer delFlag;
+
+    private Integer tenantId;
+    /**
+     * 创建时间
+     */
+    private Long createTime;
+    /**
+     * 更新时间
+     */
+    private Long updateTime;
+}
