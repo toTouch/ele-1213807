8165d0edc3970a3d70a55362083812ddb55fcc01
zgw <1311510078@qq.com>
2023-01-03 20:08:19 +0800

    fix(操作记录bug修复):ota bug fix

--- src/main/java/com/xiliulou/electricity/handler/iot/impl/NormalOtaOperateHandlerIot.java
+++ src/main/java/com/xiliulou/electricity/handler/iot/impl/NormalOtaOperateHandlerIot.java
@@ -67,9 +67,9 @@ public class NormalOtaOperateHandlerIot extends AbstractElectricityIotHandler {
         
         if (EleOtaOperateRequest.TYPE_DOWNLOAD.equals(request.getOperateType()) || EleOtaOperateRequest.TYPE_SYNC
                 .equals(request.getOperateType())) {
-            insertOrUpdateEleOtaFile(electricityCabinet, request);
             //操作回调的放在redis中
             if (Objects.nonNull(request.getSuccess()) && "true".equalsIgnoreCase(request.getSuccess())) {
+                insertOrUpdateEleOtaFile(electricityCabinet, request);
                 redisService.set(CacheConstant.OTA_OPERATE_CACHE + sessionId, "ok", 30L, TimeUnit.SECONDS);
             } else {
                 redisService.set(CacheConstant.OTA_OPERATE_CACHE + sessionId, request.getMsg(), 30L, TimeUnit.SECONDS);
