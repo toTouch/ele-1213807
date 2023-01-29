1ce85cac3019f37523af42f86a50c9a702e2aed0
huruipeng <1325648013@qq.com>
2023-01-29 10:58:23 +0800

    后台套餐购买记录查询套餐购买次数

--- src/main/java/com/xiliulou/electricity/vo/ElectricityMemberCardOrderVO.java
+++ src/main/java/com/xiliulou/electricity/vo/ElectricityMemberCardOrderVO.java
@@ -14,6 +14,7 @@ public class ElectricityMemberCardOrderVO extends ElectricityMemberCardOrder {
     private String phone;
     private String franchiseeName;
     private String userName;
+    private Integer payCount;
 
     private OldUserActivityVO  oldUserActivityVO;
 }
