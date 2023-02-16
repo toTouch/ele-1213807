c2214d572a5d058504ff896c10fcede4abe2b9fb
zgw <1311510078@qq.com>
2023-02-16 09:11:09 +0800

    租车系统优化(fix):添加outerFilter

---
+++ src/main/java/com/xiliulou/electricity/filter/OuterFilter.java
@@ -0,0 +1,12 @@
+package com.xiliulou.electricity.filter;
+
+import javax.servlet.Filter;
+
+/**
+ * @author zgw
+ * @date 2023/2/16 9:03
+ * @mood
+ */
+public abstract class OuterFilter implements Filter {
+    
+}
