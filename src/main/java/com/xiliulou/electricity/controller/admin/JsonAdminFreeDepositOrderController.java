bfdaa395f7f26efe42c7e091d710631a2b85d74b
zhaozhilong <zhaozhilong@xiliulou.com>
2023-03-03 19:48:28 +0800

    fix(同步免押订单状态)

--- src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminFreeDepositOrderController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminFreeDepositOrderController.java
@@ -166,7 +166,7 @@ public class JsonAdminFreeDepositOrderController extends BaseController {
             return Triple.of(false, "ELECTRICITY.0001", "未找到用户!");
         }
         
-        if (!SecurityUtils.isAdmin() || (!Objects.equals(user.getDataType(), User.DATA_TYPE_OPERATE))) {
+        if (SecurityUtils.isAdmin() || Objects.equals(user.getDataType(), User.DATA_TYPE_OPERATE)) {
             return Triple.of(false, "ELECTRICITY.0066", "用户权限不足");
         }
         
