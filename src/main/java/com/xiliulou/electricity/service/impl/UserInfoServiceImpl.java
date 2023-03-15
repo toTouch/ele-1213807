7d9838889d03aca804110f4c78250f020bd5038c
zhaozhilong <zhaozhilong@xiliulou.com>
2023-03-15 18:00:14 +0800

    merge

--- src/main/java/com/xiliulou/electricity/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/UserInfoServiceImpl.java
@@ -39,8 +39,6 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.CollectionUtils;
-
 import javax.annotation.Resource;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
