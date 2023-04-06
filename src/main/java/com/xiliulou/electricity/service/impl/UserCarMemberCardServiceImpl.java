62cac1e97f5e800e87426098523daf8673eeb35c
zgw <1311510078@qq.com>
2023-04-06 10:44:03 +0800

    feat(车辆控制):添加车辆控制记录

--- src/main/java/com/xiliulou/electricity/service/impl/UserCarMemberCardServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/UserCarMemberCardServiceImpl.java
@@ -68,6 +68,9 @@ public class UserCarMemberCardServiceImpl implements UserCarMemberCardService {
     
     @Autowired
     private ElectricityConfigService electricityConfigService;
+    
+    @Autowired
+    private CarLockCtrlHistoryService carLockCtrlHistoryService;
 
     /**
      * 通过ID查询单条数据从DB
@@ -277,36 +280,33 @@ public class UserCarMemberCardServiceImpl implements UserCarMemberCardService {
             
             query.parallelStream().forEach(item -> {
                 if (StrUtil.isEmpty(item.getSn()) || Objects.isNull(item.getCid())) {
+                    log.error("EXPIRE BREAK POWER ERROR! illegal parameter result={}", item);
                     return;
                 }
     
-                //                ElectricityConfig config = electricityConfigService.queryFromCacheByTenantId(item.getTenantId());
-                //                if (Objects.equals(config.getIsOpenCarControl(), ElectricityConfig.ENABLE_CAR_CONTROL)) {
-                //                    R<Jt808DeviceInfoVo> result = jt808RetrofitService.controlDevice(
-                //                            new Jt808DeviceControlRequest(IdUtil.randomUUID(), item.getSn(), ElectricityCar.TYPE_LOCK));
-                //                    if (!result.isSuccess()) {
-                //                        log.error("EXPIRE BREAK POWER ERROR! car control command send error! sn={}", item.getSn());
-                //                    }
-                //                }
-    
+                ElectricityCar electricityCar = electricityCarService.selectBySn(item.getSn(), item.getTenantId());
+                if (Objects.isNull(electricityCar)) {
+                    log.error("EXPIRE BREAK POWER ERROR! electricityCar not find, sn={}", item.getSn());
+                    return;
+                }
                 ElectricityConfig electricityConfig = electricityConfigService
                         .queryFromCacheByTenantId(TenantContextHolder.getTenantId());
                 if (Objects.nonNull(electricityConfig) && Objects
                         .equals(electricityConfig.getIsOpenCarControl(), ElectricityConfig.ENABLE_CAR_CONTROL)) {
                     boolean result = electricityCarService.retryCarLockCtrl(item.getSn(), ElectricityCar.TYPE_LOCK, 3);
-        
-                    //                    CarLockCtrlHistory carLockCtrlHistory = new CarLockCtrlHistory();
-                    //                    carLockCtrlHistory.setUid(query.getUid());
-                    //                    carLockCtrlHistory.setName(query.getName());
-                    //                    carLockCtrlHistory.setPhone(query.getPhone());
-                    //                    carLockCtrlHistory.setStatus(CarLockCtrlHistory.TYPE_UN_BIND_USER_LOCK);
-                    //                    carLockCtrlHistory.setType(result ? CarLockCtrlHistory.STATUS_LOCK_SUCCESS : CarLockCtrlHistory.STATUS_LOCK_FAIL);
-                    //                    carLockCtrlHistory.setCarModelId(electricityCar.getModelId().longValue());
-                    //                    carLockCtrlHistory.setCarModel(electricityCar.getModel());
-                    //                    carLockCtrlHistory.setCreateTime(System.currentTimeMillis());
-                    //                    carLockCtrlHistory.setUpdateTime(System.currentTimeMillis());
-                    //                    carLockCtrlHistory.setTenantId(TenantContextHolder.getTenantId());
-                    //                    carLockCtrlHistoryService.insert(carLockCtrlHistory);
+                    CarLockCtrlHistory carLockCtrlHistory = new CarLockCtrlHistory();
+                    carLockCtrlHistory.setUid(item.getUid());
+                    carLockCtrlHistory.setName(item.getName());
+                    carLockCtrlHistory.setPhone(item.getPhone());
+                    carLockCtrlHistory.setStatus(CarLockCtrlHistory.TYPE_MEMBER_CARD_LOCK);
+                    carLockCtrlHistory.setType(
+                            result ? CarLockCtrlHistory.STATUS_LOCK_SUCCESS : CarLockCtrlHistory.STATUS_LOCK_FAIL);
+                    carLockCtrlHistory.setCarModelId(electricityCar.getModelId().longValue());
+                    carLockCtrlHistory.setCarModel(electricityCar.getModel());
+                    carLockCtrlHistory.setCreateTime(System.currentTimeMillis());
+                    carLockCtrlHistory.setUpdateTime(System.currentTimeMillis());
+                    carLockCtrlHistory.setTenantId(TenantContextHolder.getTenantId());
+                    carLockCtrlHistoryService.insert(carLockCtrlHistory);
                 }
             });
             
