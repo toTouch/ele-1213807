cb0764ca516d3765c75c5aead10c839ab24b20a4
zhaozhilong <zhaozhilong@xiliulou.com>
2023-05-10 10:14:49 +0800

    feat(免押&人脸核身):增加查看剩余次数接口

--- src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminFaceRecognizeDataController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminFaceRecognizeDataController.java
@@ -2,24 +2,18 @@ package com.xiliulou.electricity.controller.admin;
 
 import com.xiliulou.core.controller.BaseController;
 import com.xiliulou.core.web.R;
-import com.xiliulou.electricity.entity.FaceRecognizeData;
-import com.xiliulou.electricity.entity.User;
 import com.xiliulou.electricity.query.FaceRecognizeDataQuery;
 import com.xiliulou.electricity.service.FaceRecognizeDataService;
-import com.xiliulou.electricity.service.UserTypeService;
+import com.xiliulou.electricity.tenant.TenantContextHolder;
 import com.xiliulou.electricity.utils.SecurityUtils;
 import com.xiliulou.electricity.validator.CreateGroup;
 import com.xiliulou.electricity.validator.UpdateGroup;
 import com.xiliulou.security.bean.TokenUser;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Resource;
-import java.util.Collections;
-import java.util.List;
 import java.util.Objects;
 
 /**
@@ -146,6 +140,20 @@ public class JsonAdminFaceRecognizeDataController extends BaseController {
         }
 
         return returnTripleResult(this.faceRecognizeDataService.recharge(faceRecognizeDataQuery));
+    }
+
+    @GetMapping("/admin/faceRecognizeData/capacity")
+    public R capacity() {
+        TokenUser user = SecurityUtils.getUserInfo();
+        if (Objects.isNull(user)) {
+            return R.fail("ELECTRICITY.0001", "未找到用户");
+        }
+
+        if (!SecurityUtils.isAdmin()) {
+            return R.fail("ELECTRICITY.0066", "用户权限不足");
+        }
+
+        return R.ok(this.faceRecognizeDataService.selectByTenantId(TenantContextHolder.getTenantId()));
     }
 
 }
