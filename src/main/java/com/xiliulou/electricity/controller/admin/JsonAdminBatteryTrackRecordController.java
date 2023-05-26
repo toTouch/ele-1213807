1bf55c0a66b260ea78198229fe16779a17bf4230
caobo <caobo@xiliulou.com>
2023-05-26 15:41:41 +0800

    优化电池操作记录

--- src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminBatteryTrackRecordController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminBatteryTrackRecordController.java
@@ -2,15 +2,14 @@ package com.xiliulou.electricity.controller.admin;
 
 import cn.hutool.core.util.StrUtil;
 import com.xiliulou.core.controller.BaseController;
-import com.xiliulou.core.json.JsonUtil;
 import com.xiliulou.core.web.R;
 import com.xiliulou.electricity.service.BatteryTrackRecordService;
 import com.xiliulou.electricity.service.EleBatterySnapshotService;
-import com.xiliulou.iot.entity.ReceiverMessage;
-import com.xiliulou.iot.mns.HardwareHandlerManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
@@ -22,24 +21,22 @@ import java.util.concurrent.TimeUnit;
 @RestController
 @Slf4j
 public class JsonAdminBatteryTrackRecordController extends BaseController {
-    
+
     @Autowired
     BatteryTrackRecordService batteryTrackRecordService;
-    
+
     @Autowired
     EleBatterySnapshotService batterySnapshotService;
-    @Autowired
-    protected HardwareHandlerManager hardwareHandlerManager;
 
     @GetMapping("/admin/battery/track/record")
     public R queryTrackRecord(@RequestParam("sn") String sn, @RequestParam("size") Integer size,
             @RequestParam("offset") Integer offset, @RequestParam("startTime") Long startTime,
             @RequestParam("endTime") Long endTime) {
-        
+
         if (size > 50) {
             size = 10;
         }
-        
+
         if (offset < 0) {
             offset = 0;
         }
@@ -51,36 +48,33 @@ public class JsonAdminBatteryTrackRecordController extends BaseController {
         if (StrUtil.isEmpty(sn)) {
             return R.fail("电池编号不可以为空");
         }
-        
+
         return returnPairResult(batteryTrackRecordService.queryTrackRecord(sn, size, offset, startTime, endTime));
-        
+
     }
-    
+
     @GetMapping("/admin/battery/snapshot/list")
     public R getBatterySnapshot(@RequestParam("eId") Integer eId, @RequestParam("size") Integer size,
             @RequestParam("offset") Integer offset, @RequestParam("startTime") Long startTime,
             @RequestParam("endTime") Long endTime) {
-        
+
         if (size > 50) {
             size = 10;
         }
-        
+
         if (offset < 0) {
             offset = 0;
         }
-        
+
         if ((endTime - startTime) > TimeUnit.HOURS.toMillis(72)) {
             return R.fail("时间跨度不可以大于三天");
         }
-        
+
         if (Objects.isNull(eId)) {
             return R.fail("柜机id不可以为空");
         }
         return returnPairResult(batterySnapshotService.queryBatterySnapshot(eId, size, offset, startTime, endTime));
     }
-    @GetMapping("/admin/battery/test/command")
-    public R executeCommand(@RequestBody ReceiverMessage receiverMessage){
-        hardwareHandlerManager.chooseCommandHandlerProcessReceiveMessage(receiverMessage);
-        return R.ok();
-    }
+
+
 }
