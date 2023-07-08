376f7e2ecc85e295d59f9e468b884bd07b51bd92
kenneth <baoyu@xiliulou.com>
2023-07-08 21:49:45 +0800

    HDG-183: add code for esign

---
+++ src/main/java/com/xiliulou/electricity/constant/EleEsignConstant.java
@@ -0,0 +1,19 @@
+package com.xiliulou.electricity.constant;
+
+/**
+ * @author: Kenneth
+ * @Date: 2023/7/7 23:40
+ * @Description:
+ */
+public class EleEsignConstant {
+    public static final Integer DEL_NO = 0;
+
+    public static final Integer DEL_YES = 1;
+
+    public static final Integer ESIGN_ENABLE = 0;
+
+    public static final Integer ESIGN_DISABLE = 1;
+
+    public static final Integer ESIGN_TURN_OFF = 2;
+
+}
