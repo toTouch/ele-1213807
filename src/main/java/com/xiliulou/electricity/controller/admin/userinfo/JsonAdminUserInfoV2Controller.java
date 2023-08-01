a3d1ac6350fbb592043ebd2d1898df6163fd489e
宋小惠 <songxiaohui.sxh@sunyur.com>
2023-08-01 22:05:34 +0800

    运营端，查询能够给会员绑定的套餐

--- src/main/java/com/xiliulou/electricity/controller/admin/userinfo/JsonAdminUserInfoV2Controller.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/userinfo/JsonAdminUserInfoV2Controller.java
@@ -2,10 +2,15 @@ package com.xiliulou.electricity.controller.admin.userinfo;
 
 import com.xiliulou.core.web.R;
 import com.xiliulou.electricity.entity.UserInfo;
+import com.xiliulou.electricity.entity.car.CarRentalPackagePO;
+import com.xiliulou.electricity.exception.BizException;
 import com.xiliulou.electricity.query.UserInfoQuery;
+import com.xiliulou.electricity.query.car.CarRentalPackageQryReq;
 import com.xiliulou.electricity.reqparam.qry.userinfo.UserInfoQryReq;
 import com.xiliulou.electricity.service.UserInfoService;
+import com.xiliulou.electricity.service.car.biz.CarRentalPackageBizService;
 import com.xiliulou.electricity.tenant.TenantContextHolder;
+import com.xiliulou.electricity.vo.car.CarRentalPackageVO;
 import com.xiliulou.electricity.vo.userinfo.UserInfoVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
@@ -30,7 +35,60 @@ import java.util.stream.Collectors;
 public class JsonAdminUserInfoV2Controller {
 
     @Resource
+    private CarRentalPackageBizService carRentalPackageBizService;
+
+    @Resource
     private UserInfoService userInfoService;
+
+    /**
+     * 获取用户可以购买的套餐
+     * @param qryReq 查询数据模型
+     * @return 可购买的套餐数据集，包含赠送优惠券信息
+     */
+    @PostMapping("/queryCanPurchasePackage")
+    public R<List<CarRentalPackageVO>> queryCanPurchasePackage(@RequestBody CarRentalPackageQryReq qryReq) {
+        if (!ObjectUtils.allNotNull(qryReq, qryReq.getFranchiseeId(), qryReq.getStoreId(), qryReq.getCarModelId())) {
+            throw new BizException("ELECTRICITY.0007", "不合法的参数");
+        }
+
+        Integer tenantId = TenantContextHolder.getTenantId();
+        qryReq.setTenantId(tenantId);
+
+        List<CarRentalPackagePO> entityList = carRentalPackageBizService.queryCanPurchasePackage(qryReq, qryReq.getUid());
+
+        // 转换 VO
+        List<CarRentalPackageVO> voList = buildVOList(entityList);
+
+        return R.ok(voList);
+    }
+
+
+    /**
+     * entityList to voList
+     * @param entityList
+     * @return
+     */
+    private List<CarRentalPackageVO> buildVOList(List<CarRentalPackagePO> entityList) {
+        return entityList.stream().map(entity -> {
+            CarRentalPackageVO packageVO = new CarRentalPackageVO();
+            packageVO.setId(entity.getId());
+            packageVO.setName(entity.getName());
+            packageVO.setType(entity.getType());
+            packageVO.setTenancy(entity.getTenancy());
+            packageVO.setTenancyUnit(entity.getTenancyUnit());
+            packageVO.setRent(entity.getRent());
+            packageVO.setRentRebate(entity.getRentRebate());
+            packageVO.setRentRebateTerm(entity.getRentRebateTerm());
+            packageVO.setDeposit(entity.getDeposit());
+            packageVO.setFreeDeposit(entity.getFreeDeposit());
+            packageVO.setConfine(entity.getConfine());
+            packageVO.setConfineNum(entity.getConfineNum());
+            packageVO.setGiveCoupon(entity.getGiveCoupon());
+            packageVO.setRemark(entity.getRemark());
+            packageVO.setBatteryVoltage(entity.getBatteryVoltage());
+            return packageVO;
+        }).collect(Collectors.toList());
+    }
 
     /**
      * 根据关键字查询用户集
