ee50fb1934f5b55fc8505e255485b6c9bdabe057
baoyu <baoyu@xiliulou.com>
2023-08-07 08:53:15 +0800

    update code

--- src/main/java/com/xiliulou/electricity/service/CouponActivityPackageService.java
+++ src/main/java/com/xiliulou/electricity/service/CouponActivityPackageService.java
@@ -2,6 +2,7 @@ package com.xiliulou.electricity.service;
 
 import com.xiliulou.electricity.entity.Coupon;
 import com.xiliulou.electricity.entity.CouponActivityPackage;
+import com.xiliulou.electricity.enums.PackageTypeEnum;
 
 import java.util.List;
 
@@ -23,6 +24,14 @@ public interface CouponActivityPackageService {
 
     List<CouponActivityPackage> findPackagesByCouponIdAndType(Long couponId, Integer packageType);
 
+    /**
+     * 检查集合中的优惠券是否针对当前套餐可用。有一个不满足条件，则检查结果为无效
+     * @param coupons
+     * @param packageId
+     * @param packageType
+     * @see PackageTypeEnum
+     * @return
+     */
     Boolean checkPackageIsValid(List<Coupon> coupons, Long packageId, Integer packageType);
 
 }
