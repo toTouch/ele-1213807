ed3fb96e92a41a52e167b82aecb2211fdc88e2db
baoyu <baoyu@xiliulou.com>
2023-08-10 15:39:32 +0800

    fix issue

---
+++ src/main/java/com/xiliulou/electricity/vo/activity/ShareActivityRuleVO.java
@@ -0,0 +1,50 @@
+package com.xiliulou.electricity.vo.activity;
+
+import lombok.Data;
+
+/**
+ * @author: Kenneth
+ * @Date: 2023/8/10 15:00
+ * @Description:
+ */
+
+@Data
+public class ShareActivityRuleVO {
+
+    private Long id;
+    /**
+     * 活动id
+     */
+    private Integer activityId;
+    /**
+     * 触发人数
+     */
+    private Integer triggerCount;
+    /**
+     * 优惠券id
+     */
+    private Integer couponId;
+    /**
+     * 优惠券名称
+     */
+    private String couponName;
+
+    /**
+     * 0--正常 1--删除
+     */
+    private Integer delFlag;
+    /**
+     * 创建时间
+     */
+    private Long createTime;
+    /**
+     * 修改时间
+     */
+    private Long updateTime;
+
+    /**
+     * 租户
+     */
+    private Integer tenantId;
+
+}
