4956cc6af9dceedb26fe2479aa1f97900fa85a17
baoyu <baoyu@xiliulou.com>
2023-08-14 09:47:43 +0800

    Refine code

---
+++ src/main/java/com/xiliulou/electricity/vo/activity/ActivityPackageVO.java
@@ -0,0 +1,25 @@
+package com.xiliulou.electricity.vo.activity;
+
+import com.xiliulou.electricity.enums.PackageTypeEnum;
+import lombok.Data;
+
+/**
+ * @author: Kenneth
+ * @Date: 2023/8/12 17:01
+ * @Description:
+ */
+
+@Data
+public class ActivityPackageVO {
+
+    private Long packageId;
+
+    private String packageName;
+
+    /**
+     * 套餐类型
+     * @see PackageTypeEnum
+     */
+    private Integer packageType;
+
+}
