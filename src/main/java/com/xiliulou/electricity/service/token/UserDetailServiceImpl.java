385c4ae22354559c5c9e58b4451e2ecb1ed86c50
eclair <eclair@qq.com>
2023-08-16 16:03:39 +0800

    bug test

--- src/main/java/com/xiliulou/electricity/service/token/UserDetailServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/token/UserDetailServiceImpl.java
@@ -4,6 +4,7 @@ import com.xiliulou.electricity.entity.User;
 import com.xiliulou.electricity.service.UserService;
 import com.xiliulou.security.authentication.authorization.AuthorizationService;
 import com.xiliulou.security.bean.SecurityUser;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,21 +21,23 @@ import java.util.Objects;
  * @Description:
  */
 @Service
+@Slf4j
 public class UserDetailServiceImpl implements UserDetailsService {
-	@Autowired
-	UserService userService;
-	@Autowired
-	AuthorizationService authorizationService;
+    @Autowired
+    UserService userService;
+    @Autowired
+    AuthorizationService authorizationService;
 
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		//查询用户
-		User user = userService.queryByUserName(username);
-		if (Objects.isNull(user) || Objects.equals(user.getUserType(), User.TYPE_USER_NORMAL_WX_PRO)) {
-			throw new UsernameNotFoundException("用户名或者密码错误!");
-		}
-		Collection<? extends GrantedAuthority> authorities = authorizationService.acquireAllAuthorities(user.getUid(), user.getUserType());
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //查询用户
+        User user = userService.queryByUserName(username);
+        if (Objects.isNull(user) || Objects.equals(user.getUserType(), User.TYPE_USER_NORMAL_WX_PRO)) {
+            throw new UsernameNotFoundException("用户名或者密码错误!");
+        }
+        log.info("users={}", user);
+        Collection<? extends GrantedAuthority> authorities = authorizationService.acquireAllAuthorities(user.getUid(), user.getUserType());
 
-		return new SecurityUser(user.getName(), user.getPhone(), user.getUid(), user.getUserType(), user.getDataType(),user.getLoginPwd(), user.isLock(), authorities,user.getTenantId());
-	}
+        return new SecurityUser(user.getName(), user.getPhone(), user.getUid(), user.getUserType(), user.getDataType(), user.getLoginPwd(), user.isLock(), authorities, user.getTenantId());
+    }
 }
