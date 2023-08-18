0c02ab57535c7b64dc9e8961c509dae50cbcb945
zhangyan <yan10zhang@163.com>
2023-08-18 10:00:38 +0800

    电池需要单独查

--- src/main/java/com/xiliulou/electricity/entity/car/CarDataEntity.java
+++ src/main/java/com/xiliulou/electricity/entity/car/CarDataEntity.java
@@ -7,6 +7,7 @@ import lombok.Data;
  */
 @Data
 public class CarDataEntity {
+    private Long uid; // userID
     private String carSn;  // 车辆序列号
     private String model;  // 车辆型号
     private String franchiseeName;  // 加盟商名称
@@ -14,9 +15,6 @@ public class CarDataEntity {
     private String userName;  // 用户名称
     private String phone;  // 用户电话
     private String carStatus;  // 车辆状态
-    private String batterySn;  // 电池序列号
-    private int power;  // 电池电量
-    private double voltage;  // 电池电压
     private Long updateTime;  // 更新时间
     private Long createTime;  // 创建时间
     private Long dueTime; // 套餐到期时间
