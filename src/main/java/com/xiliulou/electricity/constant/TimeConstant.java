c102ed7dff199e01767c3ca4a505aa9f6112adbf
宋小惠 <songxiaohui.sxh@sunyur.com>
2023-08-19 16:43:34 +0800

    为了测试，过期更改为10分钟

--- src/main/java/com/xiliulou/electricity/constant/TimeConstant.java
+++ src/main/java/com/xiliulou/electricity/constant/TimeConstant.java
@@ -11,8 +11,12 @@ public interface TimeConstant {
     long DAY_MILLISECOND = 1000 * 60 * 60 * 24;
 
 
-    /** 一天的毫秒数 */
+    /** 一分钟的毫秒数 */
     long MINUTE_MILLISECOND = 1000 * 60;
+
+
+    /** 一分钟的毫秒数 */
+    long TEN_MINUTE_MILLISECOND = 1000 * 60 * 10;
 
 
 }
