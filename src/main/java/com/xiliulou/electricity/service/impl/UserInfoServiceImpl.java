770ab56a9d3ea4f14046bc1db070e9c7f3775ab2
zhaozhilong <zhaozhilong@xiliulou.com>
2023-08-26 16:38:16 +0800

    fix

--- src/main/java/com/xiliulou/electricity/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/UserInfoServiceImpl.java
@@ -1189,7 +1189,7 @@ public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> i
             //判断车电一体滞纳金
             if (Boolean.TRUE.equals(carRenalPackageSlippageBizService.isExitUnpaid(oldUserInfo.getTenantId(),oldUserInfo.getUid()))) {
                 log.warn("ORDER WARN! user exist battery service fee,uid={}", oldUserInfo.getUid());
-                return R.fail("存在滞纳金，请先缴纳", "300001");
+                return R.fail("300001","存在滞纳金，请先缴纳");
             }
         }
 
