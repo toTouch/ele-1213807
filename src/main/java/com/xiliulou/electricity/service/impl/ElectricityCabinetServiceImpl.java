ebd41e747efb555511d54752466b95c7edb61b65
zhaozhilong <zhaozhilong@xiliulou.com>
2023-08-27 16:19:16 +0800

    fix

--- src/main/java/com/xiliulou/electricity/service/impl/ElectricityCabinetServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/ElectricityCabinetServiceImpl.java
@@ -4119,7 +4119,9 @@ public class ElectricityCabinetServiceImpl implements ElectricityCabinetService 
                 long fullBatteryNumber = cabinetBoxList.stream().filter(this::isFullBattery).count();
                 //可换电数量
                 long exchangeableNumber = cabinetBoxList.stream().filter(e -> isExchangeable(e, fullyCharged)).count();
-
+log.error("=============fullyCharged{}",fullyCharged);
+log.error("=============exchangeableNumber{}",exchangeableNumber);
+log.error("=============e{}",JsonUtil.toJson(cabinetBoxList));
                 item.setNoElectricityBattery((int) emptyCellNumber);
                 item.setFullyElectricityBattery((int) fullBatteryNumber);
                 item.setExchangeBattery((int) exchangeableNumber);
