c45b01689497d0763e56d4796c12ebb2f59da57f
baoyu <baoyu@xiliulou.com>
2023-08-28 10:30:24 +0800

    fix issue

--- src/main/java/com/xiliulou/electricity/service/impl/FranchiseeServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/FranchiseeServiceImpl.java
@@ -243,6 +243,12 @@ public class FranchiseeServiceImpl implements FranchiseeService {
 
         }
 
+        //检查名称是否重复
+        User userNameExists = userService.queryByUserName(franchiseeAddAndUpdate.getName());
+        if (Objects.nonNull(userNameExists)) {
+            return R.fail("110200", "用户名已经存在！");
+        }
+
         Franchisee updateFranchisee = new Franchisee();
         BeanUtil.copyProperties(franchiseeAddAndUpdate, updateFranchisee);
         updateFranchisee.setUpdateTime(System.currentTimeMillis());
