1a9850b07856083a21a4d6b36f0c7c17bb816c75
zhaozhilong <zhaozhilong@xiliulou.com>
2023-09-01 16:36:04 +0800

    fix

--- src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminFreeDepositDataController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminFreeDepositDataController.java
@@ -57,6 +57,10 @@ public class JsonAdminFreeDepositDataController extends BaseController {
             return R.fail("ELECTRICITY.0001", "未找到用户");
         }
 
+        if (!(SecurityUtils.isAdmin() || Objects.equals(user.getDataType(), User.DATA_TYPE_OPERATE))) {
+            return R.ok();
+        }
+
         return R.ok(this.freeDepositDataService.selectByTenantId(TenantContextHolder.getTenantId()));
     }
 
