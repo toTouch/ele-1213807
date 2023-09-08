282b79dba42d0111ffc44e0a76c2245186716c6d
zhaozhilong <zhaozhilong@xiliulou.com>
2023-09-08 11:00:44 +0800

    fix

--- src/main/java/com/xiliulou/electricity/vo/ElectricityCabinetVO.java
+++ src/main/java/com/xiliulou/electricity/vo/ElectricityCabinetVO.java
@@ -189,7 +189,6 @@ public class ElectricityCabinetVO {
      */
     private Map<String, Long> batteryTypeMapes;
 
-    private Set<String> batteryTypes;
 
     //全天
     public static final String ALL_DAY = "-1";
