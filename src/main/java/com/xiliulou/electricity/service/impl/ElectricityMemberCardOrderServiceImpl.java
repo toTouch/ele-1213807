5e793509c098fb7b8ad95b7687a784414ca4d9c7
zhangyongbo <zhangyongbo@xiliulou.com>
2023-11-13 16:42:05 +0800

    修改换电套餐订单是否可退

--- src/main/java/com/xiliulou/electricity/service/impl/ElectricityMemberCardOrderServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/ElectricityMemberCardOrderServiceImpl.java
@@ -1010,7 +1010,12 @@ public class ElectricityMemberCardOrderServiceImpl extends ServiceImpl<Electrici
             vo.setRentUnit(batteryMemberCard.getRentUnit());
             vo.setValidDays(batteryMemberCard.getValidDays());
             vo.setUseCount(batteryMemberCard.getUseCount());
-            vo.setIsRefund(batteryMemberCard.getIsRefund());
+            if (Objects.equals(BatteryMemberCard.YES, batteryMemberCard.getIsRefund()) && System.currentTimeMillis() < (item.getCreateTime()
+                    + batteryMemberCard.getRefundLimit() * 24 * 60 * 60 * 1000L)) {
+                vo.setIsRefund(BatteryMemberCard.YES);
+            } else {
+                vo.setIsRefund(BatteryMemberCard.NO);
+            }
             vo.setRefundLimit(batteryMemberCard.getRefundLimit());
             vo.setSimpleBatteryType(acquireBatteryMembercardOrderSimpleBatteryType(memberCardBatteryTypeService.selectBatteryTypeByMid(item.getMemberCardId())));
 
