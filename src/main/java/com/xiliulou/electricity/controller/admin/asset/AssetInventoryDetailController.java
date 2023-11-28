f7d8cbadeaa77eeb75b6f42c3977f4ac09f7bf90
he yafeng <13319290867@163.com>
2023-11-28 11:13:20 +0800

    优化

--- src/main/java/com/xiliulou/electricity/controller/admin/asset/AssetInventoryDetailController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/asset/AssetInventoryDetailController.java
@@ -13,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -85,7 +86,7 @@ public class AssetInventoryDetailController {
      * @date 2023/11/21 13:23:50
      * @author HeYafeng
      */
-    @GetMapping("/admin/asset/inventory/detail/batchInventory")
+    @PostMapping("/admin/asset/inventory/detail/batchInventory")
     public R batchUpdate(@RequestBody @Validated(value = UpdateGroup.class) AssetInventoryDetailBatchInventoryRequest assetInventoryDetailBatchInventoryRequest) {
         TokenUser user = SecurityUtils.getUserInfo();
         if (Objects.isNull(user)) {
