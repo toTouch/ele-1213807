ec32c4c6a84f0fbba2ce7b4ae86a7d75e193ed5c
he yafeng <13319290867@163.com>
2023-11-30 13:44:12 +0800

    优化

--- src/main/java/com/xiliulou/electricity/controller/admin/asset/AssetExitWarehouseDetailController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/asset/AssetExitWarehouseDetailController.java
@@ -26,7 +26,7 @@ public class AssetExitWarehouseDetailController {
     @Autowired
     private AssetExitWarehouseDetailService assetExitWarehouseDetailService;
     
-    @GetMapping("/admin/asset/exit/warehouse/detail/search")
+    @GetMapping("/admin/asset/exit/warehouse/detail")
     public R exitWarehouseSnSearch(@RequestParam("size") long size, @RequestParam("offset") long offset, @RequestParam(value = "orderNo") String orderNo,
             @RequestParam(value = "type") Integer type) {
         if (size < 0 || size > 50) {
