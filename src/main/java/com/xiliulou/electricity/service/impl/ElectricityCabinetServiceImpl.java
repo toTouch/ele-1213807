5185b08df40be7f941bab7af63df3e7484ca916d
zhangyongbo <zhangyongbo@xiliulou.com>
2023-12-11 18:13:16 +0800

    小程序首页接口优化

--- src/main/java/com/xiliulou/electricity/service/impl/ElectricityCabinetServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/ElectricityCabinetServiceImpl.java
@@ -198,7 +198,6 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
