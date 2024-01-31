6068e643321113bc2e89a5412caa4243395daacf
baoyu <baoyu@xiliulou.com>
2024-01-31 10:12:15 +0800

    Add new package

---
+++ src/main/java/com/xiliulou/electricity/controller/admin/merchant/JsonAdminMerchantController.java
@@ -0,0 +1,11 @@
+package com.xiliulou.electricity.controller.admin.merchant;
+
+/**
+ * @author BaoYu
+ * @description:
+ * @date 2024/1/31 10:10
+ */
+
+public class JsonAdminMerchantController {
+
+}
---
+++ src/main/java/com/xiliulou/electricity/controller/user/merchant/JsonUserMerchantController.java
@@ -0,0 +1,10 @@
+package com.xiliulou.electricity.controller.user.merchant;
+
+/**
+ * @author BaoYu
+ * @description:
+ * @date 2024/1/31 10:11
+ */
+public class JsonUserMerchantController {
+
+}
