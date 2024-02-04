fea9b2dbfb597644dbcecfeb7bff35b5be2227d2
zhaozhilong <zhaozhilong@xiliulou.com>
2024-02-04 16:27:19 +0800

    feat(商户升级配置)

---
+++ src/main/java/com/xiliulou/electricity/entity/merchant/MerchantLevel.java
@@ -0,0 +1,52 @@
+package com.xiliulou.electricity.entity.merchant;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+/**
+ * 商户等级表(MerchantLevel)实体类
+ *
+ * @author Eclair
+ * @since 2024-02-04 14:35:06
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@TableName("t_merchant_level")
+public class MerchantLevel {
+    
+    private Long id;
+    
+    /**
+     * 商户等级
+     */
+    private String level;
+    
+    /**
+     * 商户等级名称
+     */
+    private String name;
+    
+    /**
+     * 规则
+     */
+    private String rule;
+    
+    private Integer delFlag;
+    
+    private Integer tenantId;
+    
+    private Long createTime;
+    
+    private Long updateTime;
+    
+    public static final Integer DEL_NORMAL = 0;
+    
+    public static final Integer DEL_DEL = 1;
+    
+}
---
+++ src/main/java/com/xiliulou/electricity/vo/merchant/MerchantLevelVO.java
@@ -0,0 +1,33 @@
+package com.xiliulou.electricity.vo.merchant;
+
+import lombok.Data;
+
+/**
+ * @author zzlong
+ * @email zhaozhilong@xiliulou.com
+ * @date 2024-02-04-15:42
+ */
+@Data
+public class MerchantLevelVO {
+    private Long id;
+    
+    /**
+     * 商户等级
+     */
+    private String level;
+    
+    /**
+     * 商户等级名称
+     */
+    private String name;
+    
+    /**
+     * 拉新人数
+     */
+    private Long invitationUserCount;
+    
+    /**
+     * 续费人数
+     */
+    private Long renewalUserCount;
+}
