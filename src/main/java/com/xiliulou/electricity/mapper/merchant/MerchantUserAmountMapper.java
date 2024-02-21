947e9a88dfaf040aa2d9c93829e554c8280d171a
maxiaodong <maxiaodong@xiliulou.com>
2024-02-21 19:44:49 +0800

    商户余额添加增加方法

--- src/main/java/com/xiliulou/electricity/mapper/merchant/MerchantUserAmountMapper.java
+++ src/main/java/com/xiliulou/electricity/mapper/merchant/MerchantUserAmountMapper.java
@@ -2,7 +2,9 @@ package com.xiliulou.electricity.mapper.merchant;
 
 import com.xiliulou.electricity.entity.merchant.MerchantUserAmount;
 import com.xiliulou.electricity.query.merchant.MerchantUserAmountQueryMode;
+import org.apache.ibatis.annotations.Param;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -15,4 +17,8 @@ public interface MerchantUserAmountMapper {
     Integer insert(MerchantUserAmount merchantUserAmount);
     
     List<MerchantUserAmount> queryList(MerchantUserAmountQueryMode joinRecordQueryMode);
+    
+    Integer addAmountByUid(@Param("income") BigDecimal income, @Param("uid") Long uid, @Param("tenantId") Long tenantId);
+    
+    Integer reduceAmountByUid(@Param("income") BigDecimal income, @Param("uid") Long uid, @Param("tenantId") Long tenantId);
 }
--- src/main/java/com/xiliulou/electricity/service/merchant/MerchantUserAmountService.java
+++ src/main/java/com/xiliulou/electricity/service/merchant/MerchantUserAmountService.java
@@ -3,6 +3,7 @@ package com.xiliulou.electricity.service.merchant;
 import com.xiliulou.electricity.entity.merchant.MerchantUserAmount;
 import com.xiliulou.electricity.query.merchant.MerchantUserAmountQueryMode;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -15,4 +16,20 @@ public interface MerchantUserAmountService {
     Integer save(MerchantUserAmount merchantUserAmount);
     
     List<MerchantUserAmount> queryList(MerchantUserAmountQueryMode joinRecordQueryMode);
+    
+    /**
+     * 返现时给商户增加金额
+     * @param amount
+     * @param uid
+     * @return
+     */
+    Integer addAmount(BigDecimal amount, Long uid, Long tenantId);
+    
+    /**
+     * 退款时从商户账户表中扣除金额
+     * @param amount
+     * @param uid
+     * @return
+     */
+    Integer reduceAmount(BigDecimal amount, Long uid, Long tenantId);
 }
