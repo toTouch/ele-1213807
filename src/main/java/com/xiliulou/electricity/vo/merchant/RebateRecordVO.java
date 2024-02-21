90ae289a01f4180ae7c91a0aefa5c677e03cb48e
zhaozhilong <zhaozhilong@xiliulou.com>
2024-02-21 10:25:28 +0800

    feat(返利)

--- src/main/java/com/xiliulou/electricity/vo/merchant/RebateRecordVO.java
+++ src/main/java/com/xiliulou/electricity/vo/merchant/RebateRecordVO.java
@@ -2,6 +2,8 @@ package com.xiliulou.electricity.vo.merchant;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 /**
  * @author zzlong
  * @email zhaozhilong@xiliulou.com
@@ -9,5 +11,109 @@ import lombok.Data;
  */
 @Data
 public class RebateRecordVO {
-
+    private Long id;
+    
+    /**
+     * 用户Id
+     */
+    private Long uid;
+    
+    /**
+     * 用户名称
+     */
+    private String name;
+    
+    /**
+     * 手机号
+     */
+    private String phone;
+    
+    /**
+     * 订单Id
+     */
+    private String orderId;
+    
+    /**
+     * 原始订单Id
+     */
+    private String originalOrderId;
+    
+    /**
+     * 套餐id
+     */
+    private Long memberCardId;
+    private String batteryMemberCardName;
+    
+    /**
+     * 返利类型
+     */
+    private Integer type;
+    
+    /**
+     * 加盟商id
+     */
+    private Long franchiseeId;
+    private String franchiseeName;
+    
+    /**
+     * 商户等级
+     */
+    private String level;
+    
+    private Long merchantId;
+    private String merchantName;
+    
+    /**
+     * 结算状态（0-未结算,1-已结算,2-已退回,3-已失效）
+     */
+    private Integer status;
+    
+    /**
+     * 渠道员
+     */
+    private Long channeler;
+    
+    /**
+     * 渠道员返现
+     */
+    private BigDecimal channelerRebate;
+    
+    /**
+     * 商户返现
+     */
+    private BigDecimal merchantRebate;
+    
+    /**
+     * 商户场地id
+     */
+    private Long placeId;
+    private String placeName;
+    
+    /**
+     * 商户场地员工
+     */
+    private Long placeUid;
+    private String placeUserName;
+    
+    /**
+     * 返现时间
+     */
+    private Long rebateTime;
+    
+    /**
+     * 结算时间
+     */
+    private Long settleTime;
+    
+    private Integer tenantId;
+    
+    /**
+     * 创建时间
+     */
+    private Long createTime;
+    
+    /**
+     * 更新时间
+     */
+    private Long updateTime;
 }
