114f5fa7a60e6e88520dc38df3f4822cf6202b99
maxiaodong <maxiaodong@xiliulou.com>
2024-02-23 13:59:12 +0800

    故障告警记录列表添加柜机id查询

--- src/main/java/com/xiliulou/electricity/controller/admin/EleHardwareFailureWarnMsgController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/EleHardwareFailureWarnMsgController.java
@@ -204,7 +204,8 @@ public class EleHardwareFailureWarnMsgController {
             @RequestParam(value = "tenantId", required = false) Integer tenantId, @RequestParam(value = "type", required = true) Integer type,
             @RequestParam(value = "deviceType", required = false) Integer deviceType, @RequestParam(value = "grade", required = false) Integer grade,
             @RequestParam(value = "signalId", required = false) Integer signalId, @RequestParam(value = "alarmStartTime", required = true) Long alarmStartTime,
-            @RequestParam(value = "alarmEndTime", required = true) Long alarmEndTime, @RequestParam(value = "alarmFlag", required = false) Integer alarmFlag, @RequestParam(value = "noLimitSignalId", required = false) Integer noLimitSignalId) {
+            @RequestParam(value = "alarmEndTime", required = true) Long alarmEndTime, @RequestParam(value = "alarmFlag", required = false) Integer alarmFlag,
+            @RequestParam(value = "noLimitSignalId", required = false) Integer noLimitSignalId, @RequestParam(value = "cabinetId", required = false) Integer cabinetId) {
         if (size > 2000) {
             size = 2000;
         }
@@ -227,7 +228,7 @@ public class EleHardwareFailureWarnMsgController {
         }
         
         
-        EleHardwareFailureWarnMsgPageRequest request = EleHardwareFailureWarnMsgPageRequest.builder().type(type).sn(sn).tenantId(tenantId).deviceType(deviceType).grade(grade)
+        EleHardwareFailureWarnMsgPageRequest request = EleHardwareFailureWarnMsgPageRequest.builder().type(type).sn(sn).tenantId(tenantId).deviceType(deviceType).grade(grade).cabinetId(cabinetId)
                 .signalId(signalId).alarmStartTime(alarmStartTime).alarmEndTime(alarmEndTime).alarmFlag(alarmFlag).size(size).offset(offset).build();
     
         // 故障告警统计分析运营商总览主动跳转到故障告警记录页面第一次的时候不添加状态的限制
