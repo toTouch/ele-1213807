4d89d09cf0079e33aa5ea6c87424c539373d33f4
maxiaodong <maxiaodong@xiliulou.com>
2024-02-27 17:10:43 +0800

    商户添加小程序查询接口

--- src/main/java/com/xiliulou/electricity/vo/merchant/MerchantVO.java
+++ src/main/java/com/xiliulou/electricity/vo/merchant/MerchantVO.java
@@ -1,6 +1,9 @@
 package com.xiliulou.electricity.vo.merchant;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -11,6 +14,9 @@ import java.util.List;
  * @desc
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MerchantVO {
     
     private Long id;
