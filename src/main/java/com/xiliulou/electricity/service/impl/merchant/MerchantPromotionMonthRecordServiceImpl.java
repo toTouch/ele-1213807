81e801527fbd4dfd79a98940b1ef85be0654a8fc
he yafeng <13319290867@163.com>
2024-03-01 17:54:57 +0800

    导出excel

--- src/main/java/com/xiliulou/electricity/service/impl/merchant/MerchantPromotionMonthRecordServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/merchant/MerchantPromotionMonthRecordServiceImpl.java
@@ -28,6 +28,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -151,11 +152,12 @@ public class MerchantPromotionMonthRecordServiceImpl implements MerchantPromotio
         
         String fileName = "商户推广费出账记录.xlsx";
         try {
+            ServletOutputStream outputStream = response.getOutputStream();
             // 告诉浏览器用什么软件可以打开此文件
             response.setHeader("content-Type", "application/vnd.ms-excel");
             // 下载文件的默认名称
             response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, StandardCharsets.UTF_8));
-            EasyExcel.write(fileName).head(getHeader())
+            EasyExcel.write(outputStream, MerchantPromotionMonthDetailVO.class).head(getHeader())
                     // 合并策略：合并相同数据的行。第一个参数表示从哪一行开始进行合并，由于表头占了两行，因此从第2行开始（索引从0开始）
                     // 第二个参数是指定哪些列要进行合并
                     .registerWriteHandler(new MergeSameRowsStrategy(2, new int[] {0, 1, 2, 3})).registerWriteHandler(HeadContentCellStyle.myHorizontalCellStyleStrategy())
