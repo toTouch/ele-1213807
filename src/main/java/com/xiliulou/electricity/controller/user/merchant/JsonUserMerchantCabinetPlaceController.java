ef458bd784d8d28eafb1e51d48c2c46c6d63898a
maxiaodong <maxiaodong@xiliulou.com>
2024-03-03 19:58:32 +0800

    场地费小程序测试

--- src/main/java/com/xiliulou/electricity/controller/user/merchant/JsonUserMerchantCabinetPlaceController.java
+++ src/main/java/com/xiliulou/electricity/controller/user/merchant/JsonUserMerchantCabinetPlaceController.java
@@ -104,15 +104,16 @@ public class JsonUserMerchantCabinetPlaceController extends BaseController {
             @RequestParam(value = "cabinetId", required = false) Long cabinetId) {
         TokenUser user = SecurityUtils.getUserInfo();
         if (Objects.isNull(user)) {
-            return R.fail("ELECTRICITY.0001", "未找到用户");
+//            return R.fail("ELECTRICITY.0001", "未找到用户");
         }
     
         Merchant merchant = merchantService.queryByUid(user.getUid());
         if (Objects.isNull(merchant)) {
             log.error("merchant place is Show Place Page merchant is null, uid={}", user.getUid());
-            return R.fail("ELECTRICITY.0001", "未找到用户");
+//            return R.fail("ELECTRICITY.0001", "未找到用户");
         }
-        
+        merchant = new Merchant();
+    
         MerchantPlaceFeeRequest request = MerchantPlaceFeeRequest.builder().merchantId(merchant.getId()).placeId(placeId).cabinetId(cabinetId).build();
         
         return R.ok(merchantPlaceFeeService.getFeeData(request));
@@ -137,6 +138,7 @@ public class JsonUserMerchantCabinetPlaceController extends BaseController {
         }
     
         merchant = new Merchant();
+        
          MerchantPlaceFeeRequest request = MerchantPlaceFeeRequest.builder().merchantId(merchant.getId()).placeId(placeId)
                 .cabinetId(cabinetId).startTime(startTime).endTime(endTime).build();
         
