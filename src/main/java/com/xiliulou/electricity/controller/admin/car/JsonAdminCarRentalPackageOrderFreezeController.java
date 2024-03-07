885ef1fd646d39fb367b40f4c5a9c72b06ff0254
宋进攀 <songjinpan@xiliulou.com>
2024-03-07 19:27:10 +0800

    第一版修改

--- src/main/java/com/xiliulou/electricity/controller/admin/car/JsonAdminCarRentalPackageOrderFreezeController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/car/JsonAdminCarRentalPackageOrderFreezeController.java
@@ -26,27 +26,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
  * 租车套餐订单冻结表 Controller
+ *
  * @author xiaohui.song
  **/
 @Slf4j
 @RestController
 @RequestMapping("/admin/car/carRentalPackageOrderFreeze")
 public class JsonAdminCarRentalPackageOrderFreezeController extends BasicController {
-
+    
     @Resource
     private CarRentalPackageOrderBizService carRentalPackageOrderBizService;
-
+    
     @Resource
     private CarRentalPackageOrderFreezeService carRentalPackageOrderFreezeService;
-
-
+    
+    
     /**
      * 审核拒绝
+     *
      * @param optReq 审核操作数据
      * @return true(成功)、false(失败)
      */
@@ -55,19 +62,20 @@ public class JsonAdminCarRentalPackageOrderFreezeController extends BasicControl
         if (!ObjectUtils.allNotNull(optReq, optReq.getOrderNo(), optReq.getReason())) {
             return R.fail("ELECTRICITY.0007", "不合法的参数");
         }
-
+        
         Integer tenantId = TenantContextHolder.getTenantId();
         TokenUser user = SecurityUtils.getUserInfo();
         if (Objects.isNull(user)) {
             log.error("not found user.");
             return R.fail("ELECTRICITY.0001", "未找到用户");
         }
-
+        
         return R.ok(carRentalPackageOrderBizService.approveFreezeRentOrder(optReq.getOrderNo(), false, optReq.getReason(), user.getUid()));
     }
-
+    
     /**
      * 审核通过
+     *
      * @param optReq 审核操作数据
      * @return true(成功)、false(失败)
      */
@@ -76,19 +84,20 @@ public class JsonAdminCarRentalPackageOrderFreezeController extends BasicControl
         if (!ObjectUtils.allNotNull(optReq, optReq.getOrderNo())) {
             return R.fail("ELECTRICITY.0007", "不合法的参数");
         }
-
+        
         Integer tenantId = TenantContextHolder.getTenantId();
         TokenUser user = SecurityUtils.getUserInfo();
         if (Objects.isNull(user)) {
             log.error("not found user.");
             return R.fail("ELECTRICITY.0001", "未找到用户");
         }
-
+        
         return R.ok(carRentalPackageOrderBizService.approveFreezeRentOrder(optReq.getOrderNo(), true, optReq.getReason(), user.getUid()));
     }
-
+    
     /**
      * 条件查询列表
+     *
      * @param queryReq 请求参数类
      * @return 冻结订单集
      */
@@ -97,29 +106,29 @@ public class JsonAdminCarRentalPackageOrderFreezeController extends BasicControl
         if (null == queryReq) {
             queryReq = new CarRentalPackageOrderFreezeQryReq();
         }
-
+        
         // 赋值租户
         Integer tenantId = TenantContextHolder.getTenantId();
         queryReq.setTenantId(tenantId);
-
+        
         // 数据权校验
         Triple<List<Integer>, List<Integer>, Boolean> permissionTriple = checkPermissionInteger();
         if (!permissionTriple.getRight()) {
             return R.ok(Collections.emptyList());
         }
-
+        
         // 转换请求体
         CarRentalPackageOrderFreezeQryModel qryModel = new CarRentalPackageOrderFreezeQryModel();
         BeanUtils.copyProperties(queryReq, qryModel);
         qryModel.setFranchiseeIdList(permissionTriple.getLeft());
         qryModel.setStoreIdList(permissionTriple.getMiddle());
-
+        
         // 调用服务
         List<CarRentalPackageOrderFreezePo> freezeEntityList = carRentalPackageOrderFreezeService.page(qryModel);
         if (CollectionUtils.isEmpty(freezeEntityList)) {
             return R.ok(Collections.emptyList());
         }
-
+        
         // 获取辅助业务信息（用户信息、套餐信息）
         Set<Long> uids = new HashSet<>();
         Set<Long> rentalPackageIds = new HashSet<>();
@@ -129,28 +138,28 @@ public class JsonAdminCarRentalPackageOrderFreezeController extends BasicControl
             rentalPackageIds.add(freezePO.getRentalPackageId());
             userIds.add(freezePO.getUpdateUid());
         });
-
+        
         // 用户信息
         Map<Long, UserInfo> userInfoMap = getUserInfoByUidsForMap(uids);
-
+        
         // 套餐信息
         Map<Long, String> packageNameMap = getCarRentalPackageNameByIdsForMap(rentalPackageIds);
         
         // 审核员信息
         Map<Long, User> userMap = getUserByUidsForMap(userIds);
-    
+        
         // 模型转换，封装返回
         List<CarRentalPackageOrderFreezeVo> freezeVoList = freezeEntityList.stream().map(freezeEntity -> {
-
+            
             CarRentalPackageOrderFreezeVo freezeVO = new CarRentalPackageOrderFreezeVo();
             BeanUtils.copyProperties(freezeEntity, freezeVO);
-
+            
             if (!userInfoMap.isEmpty()) {
                 UserInfo userInfo = userInfoMap.getOrDefault(freezeEntity.getUid(), new UserInfo());
                 freezeVO.setUserRelName(userInfo.getName());
                 freezeVO.setUserPhone(userInfo.getPhone());
             }
-
+            
             if (!packageNameMap.isEmpty()) {
                 freezeVO.setCarRentalPackageName(packageNameMap.getOrDefault(freezeEntity.getRentalPackageId(), ""));
             }
@@ -159,15 +168,16 @@ public class JsonAdminCarRentalPackageOrderFreezeController extends BasicControl
                 User user = userMap.getOrDefault(freezeEntity.getUpdateUid(), new User());
                 freezeVO.setAuditorName(user.getName());
             }
-
+            
             return freezeVO;
         }).collect(Collectors.toList());
-
+        
         return R.ok(freezeVoList);
     }
-
+    
     /**
      * 条件查询总数
+     *
      * @param qryReq 请求参数类
      * @return 总数
      */
@@ -176,25 +186,25 @@ public class JsonAdminCarRentalPackageOrderFreezeController extends BasicControl
         if (null == qryReq) {
             qryReq = new CarRentalPackageOrderFreezeQryReq();
         }
-
+        
         // 赋值租户
         Integer tenantId = TenantContextHolder.getTenantId();
         qryReq.setTenantId(tenantId);
-
+        
         // 数据权校验
         Triple<List<Integer>, List<Integer>, Boolean> permissionTriple = checkPermissionInteger();
         if (!permissionTriple.getRight()) {
             return R.ok(NumberConstant.ZERO);
         }
-
+        
         // 转换请求体
         CarRentalPackageOrderFreezeQryModel qryModel = new CarRentalPackageOrderFreezeQryModel();
         BeanUtils.copyProperties(qryReq, qryModel);
         qryModel.setFranchiseeIdList(permissionTriple.getLeft());
         qryModel.setStoreIdList(permissionTriple.getMiddle());
-
+        
         // 调用服务
         return R.ok(carRentalPackageOrderFreezeService.count(qryModel));
     }
-
+    
 }
