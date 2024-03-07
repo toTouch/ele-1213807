2c2f81ab4b913717fef2ec3ccbed4fed75a3f641
PeakLee <wxblifeng@163.com>
2024-03-07 16:04:56 +0800

    1.update:格式化代码

--- src/main/java/com/xiliulou/electricity/vo/CouponIssueOperateRecordVO.java
+++ src/main/java/com/xiliulou/electricity/vo/CouponIssueOperateRecordVO.java
@@ -10,35 +10,34 @@ import lombok.Data;
 
 @Data
 public class CouponIssueOperateRecordVO {
-
+    
     private Integer id;
-
+    
     private Integer couponId;
-
+    
     private Long uid;
-
+    
     private String name;
-
+    
     private String operateName;
     
     
     private Long issuedUid;
     
     private String issuedName;
-
+    
     private String phone;
-
+    
     private Long createTime;
-
+    
     private Long updateTime;
-
+    
     private Integer tenantId;
-
+    
     /**
-     * 优惠券使用状态 1--未使用， 2--已使用 ，3--已过期，
-     *             4--已核销， 5--使用中， 6--已失效
+     * 优惠券使用状态 1--未使用， 2--已使用 ，3--已过期， 4--已核销， 5--使用中， 6--已失效
      */
     private Integer status;
-
-
+    
+    
 }
