c264ed75acb9778de54bb7ea48db83a6e3203ad6
宋进攀 <songjinpan@xiliulou.com>
2024-03-07 19:30:44 +0800

    第一版修改

--- src/main/java/com/xiliulou/electricity/service/UserService.java
+++ src/main/java/com/xiliulou/electricity/service/UserService.java
@@ -1,10 +1,7 @@
 package com.xiliulou.electricity.service;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.extension.service.IService;
 import com.xiliulou.core.web.R;
 import com.xiliulou.electricity.entity.User;
-import com.xiliulou.electricity.entity.UserInfo;
 import com.xiliulou.electricity.query.UserInfoQuery;
 import com.xiliulou.electricity.query.UserSourceQuery;
 import com.xiliulou.electricity.query.UserSourceUpdateQuery;
@@ -23,16 +20,17 @@ import java.util.List;
  * @author makejava
  * @since 2020-11-27 11:19:51
  */
-public interface UserService extends IService<User> {
+public interface UserService {
     
     /**
      * 启用锁定用户
+     *
      * @param tenantId 租户ID
-     * @param uid 用户UID
+     * @param uid      用户UID
      * @return true、false
      */
     boolean enableLockUser(Integer tenantId, Long uid);
-
+    
     /**
      * 通过ID查询单条数据从缓存
      *
@@ -40,8 +38,8 @@ public interface UserService extends IService<User> {
      * @return 实例对象
      */
     User queryByUidFromCache(Long uid);
-
-
+    
+    
     /**
      * 新增数据
      *
@@ -49,15 +47,14 @@ public interface UserService extends IService<User> {
      * @return 实例对象
      */
     User insert(User user);
-
+    
     /**
-     * 修改数据
-     * oldUser必须包括手机号和uid
+     * 修改数据 oldUser必须包括手机号和uid
      *
      * @return 实例对象
      */
     Integer updateUser(User updateUser, User oldUser);
-
+    
     /**
      * 通过主键删除数据
      *
@@ -65,63 +62,63 @@ public interface UserService extends IService<User> {
      * @return 是否成功
      */
     Boolean deleteById(Long uid);
-
+    
     User queryByUserName(String username);
-
-    User queryByUserNameAndTenantId(String username,Integer tenantId);
-
+    
+    User queryByUserNameAndTenantId(String username, Integer tenantId);
+    
     Triple<Boolean, String, Object> addAdminUser(AdminUserQuery adminUserQuery);
-
-    User queryByUserPhone(String phone, Integer type,Integer tenantId);
-
+    
+    User queryByUserPhone(String phone, Integer type, Integer tenantId);
+    
     Pair<Boolean, Object> queryListUser(Long uid, Long size, Long offset, String name, String phone, Integer type, Long startTime, Long endTime, Integer tenantId);
-
+    
     Pair<Boolean, Object> updateAdminUser(AdminUserQuery adminUserQuery);
-
+    
     Pair<Boolean, Object> deleteAdminUser(Long uid);
-
+    
     Triple<Boolean, String, Object> updatePassword(PasswordQuery passwordQuery);
-
-	Pair<Boolean, Object> addUserAddress(String cityCode);
-
+    
+    Pair<Boolean, Object> addUserAddress(String cityCode);
+    
     Pair<Boolean, Object> getUserDetail();
-
+    
     R endLimitUser(Long uid);
-
+    
     R addInnerUser(AdminUserQuery adminUserQuery);
-
+    
     void deleteInnerUser(Long uid);
-
-	Pair<Boolean, Object> queryCount(Long uid, String name, String phone, Integer type, Long startTime, Long endTime, Integer tenantId);
-
-	Integer queryHomePageCount( Integer type, Long startTime, Long endTime, Integer tenantId);
-
-	String decryptPassword(String encryptPassword);
-
-	List<User> queryByTenantIdAndType(Integer tenantId, Integer status);
-
+    
+    Pair<Boolean, Object> queryCount(Long uid, String name, String phone, Integer type, Long startTime, Long endTime, Integer tenantId);
+    
+    Integer queryHomePageCount(Integer type, Long startTime, Long endTime, Integer tenantId);
+    
+    String decryptPassword(String encryptPassword);
+    
+    List<User> queryByTenantIdAndType(Integer tenantId, Integer status);
+    
     List<User> listUserByPhone(String phone, Integer tenantId);
-
+    
     Triple<Boolean, String, Object> deleteNormalUser(Long uid);
-
+    
     R userAutoCodeGeneration();
-
+    
     R userAutoCodeCheck(String autoCode);
     
     User queryByUserPhoneFromDB(String purePhoneNumber, Integer typeUserNormalWxPro, Integer tenantId);
-
+    
     String selectServicePhone(Integer tenantId);
-
+    
     R memberCardDetail();
-
+    
     Integer updateUserSource(User user);
-
+    
     void loginCallBack(UserSourceQuery query);
-
+    
     Integer updateUserByUid(UserSourceUpdateQuery query);
-
+    
     List<UserSourceVO> selectUserSourceByPage(UserSourceQuery userSourceQuery);
-
+    
     Integer selectUserSourcePageCount(UserSourceQuery userSourceQuery);
     
     List<UserSearchVO> search(UserInfoQuery query);
