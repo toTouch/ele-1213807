5dc83b0169b11b9f187b5f39ded5c1cb2e6e88ce
eclair <eclair@qq.com>
2024-03-19 14:35:32 +0800

    增加电池快照响应

--- src/main/java/com/xiliulou/electricity/handler/iot/impl/NormalBatterySnapshotHandler.java
+++ src/main/java/com/xiliulou/electricity/handler/iot/impl/NormalBatterySnapshotHandler.java
@@ -5,10 +5,13 @@ import com.xiliulou.electricity.constant.ElectricityIotConstant;
 import com.xiliulou.electricity.entity.EleBatterySnapshot;
 import com.xiliulou.electricity.entity.ElectricityCabinet;
 import com.xiliulou.electricity.handler.iot.AbstractElectricityIotHandler;
+import com.xiliulou.electricity.mns.EleHardwareHandlerManager;
 import com.xiliulou.electricity.service.EleBatterySnapshotService;
+import com.xiliulou.iot.entity.HardwareCommandQuery;
 import com.xiliulou.iot.entity.ReceiverMessage;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,25 +25,36 @@ import org.springframework.stereotype.Service;
 @Service(value = ElectricityIotConstant.NORMAL_BATTERY_SNAPSHOT_HANDLER)
 @Slf4j
 public class NormalBatterySnapshotHandler extends AbstractElectricityIotHandler {
-    
+
     @Autowired
     private EleBatterySnapshotService eleBatterySnapshotService;
-    
+    @Autowired
+    private EleHardwareHandlerManager eleHardwareHandlerManager;
+
     @Override
     public void postHandleReceiveMsg(ElectricityCabinet electricityCabinet, ReceiverMessage receiverMessage) {
         BatterySnapshotRequest batterySnapshotRequest = JsonUtil.fromJson(receiverMessage.getOriginContent(),
                 BatterySnapshotRequest.class);
         eleBatterySnapshotService.insert(EleBatterySnapshot.builder().createTime(batterySnapshotRequest.getReportTime())
                 .jsonBatteries(batterySnapshotRequest.getBatteryJson()).eId(electricityCabinet.getId()).build());
+
+        HardwareCommandQuery comm = HardwareCommandQuery.builder()
+                .sessionId(receiverMessage.getSessionId()).data(null)
+                .productKey(electricityCabinet.getProductKey()).deviceName(electricityCabinet.getDeviceName())
+                .command(ElectricityIotConstant.ELE_BATTERY_SNAPSHOT_RSP).build();
+        Pair<Boolean, String> sendResult = eleHardwareHandlerManager.chooseCommandHandlerProcessSend(comm);
+        if (!sendResult.getLeft()) {
+            log.error("Snapshot ERROR! sessionId={}", receiverMessage.getSessionId());
+        }
     }
-    
-    
+
+
 }
 
 @Data
 class BatterySnapshotRequest {
-    
+
     private String batteryJson;
-    
+
     private Long reportTime;
 }
