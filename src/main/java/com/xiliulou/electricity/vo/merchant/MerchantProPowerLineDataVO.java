187448ecca1000b8d39b24effddccaa9209dfa0b
宋进攀 <songjinpan@xiliulou.com>
2024-03-27 14:49:31 +0800

    3.27代码迁移

---
+++ src/main/java/com/xiliulou/electricity/vo/merchant/MerchantProPowerLineDataVO.java
@@ -0,0 +1,18 @@
+package com.xiliulou.electricity.vo.merchant;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * @author HeYafeng
+ * @description 折线图电量
+ * @date 2024/2/26 03:20:07
+ */
+@Data
+public class MerchantProPowerLineDataVO {
+    
+    private String monthDate;
+    
+    private BigDecimal power;
+}
