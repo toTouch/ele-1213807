4fb9e7caf5e8d810ceee9ebeb92f70fa0b75037f
宋进攀 <songjinpan@xiliulou.com>
2024-03-27 19:06:41 +0800

    3.27代码迁移

--- src/main/java/com/xiliulou/electricity/query/ElectricityConfigAddAndUpdateQuery.java
+++ src/main/java/com/xiliulou/electricity/query/ElectricityConfigAddAndUpdateQuery.java
@@ -4,6 +4,7 @@ import com.xiliulou.electricity.entity.FranchiseeMoveInfo;
 import com.xiliulou.electricity.validator.CreateGroup;
 import com.xiliulou.electricity.validator.UpdateGroup;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -136,6 +137,28 @@ public class ElectricityConfigAddAndUpdateQuery {
      *  冻结是否强制退资产 0--是 1--否
      */
     private Integer allowFreezeWithAssets;
+    
+    /**
+     *  柜机少电比例
+     */
+    @Range(min = 0, max = 100, message = "请输入0-100的整数", groups = {CreateGroup.class, UpdateGroup.class})
+    private Integer lowChargeRate;
+    
+    /**
+     *  柜机多电比例
+     */
+    @Range(min = 0, max = 100, message = "请输入0-100的整数", groups = {CreateGroup.class, UpdateGroup.class})
+    private Integer fullChargeRate;
+    
+    /**
+     * 渠道时限
+     */
+    private Integer channelTimeLimit;
+    
+    /**
+     * 打开微信客服 0-是 1-否
+     */
+    private Integer wxCustomer;
 }
 
 
