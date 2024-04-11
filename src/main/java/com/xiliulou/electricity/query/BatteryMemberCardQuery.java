0746c795236a418dc5557fefcde22d86a08994b9
宋进攀 <songjinpan@xiliulou.com>
2024-04-11 15:31:33 +0800

    0411 有锂430调整

--- src/main/java/com/xiliulou/electricity/query/BatteryMemberCardQuery.java
+++ src/main/java/com/xiliulou/electricity/query/BatteryMemberCardQuery.java
@@ -23,47 +23,49 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class BatteryMemberCardQuery {
+    
     private Long size;
-
+    
     private Long offset;
-
+    
     private Integer tenantId;
-
+    
     @NotNull(message = "id不能为空", groups = {UpdateGroup.class})
     private Long id;
-
+    
     @NotBlank(message = "套餐名称不能为空", groups = {CreateGroup.class, UpdateGroup.class})
     private String name;
-
+    
     @NotNull(message = "押金不能为空", groups = {CreateGroup.class, UpdateGroup.class})
     private BigDecimal deposit;
-
+    
     @NotNull(message = "租金不能为空", groups = {CreateGroup.class, UpdateGroup.class})
     private BigDecimal rentPrice;
-
-//    @NotNull(message = "租金单价不能为空", groups = {CreateGroup.class, UpdateGroup.class})
+    
+    //@NotNull(message = "租金单价不能为空", groups = {CreateGroup.class, UpdateGroup.class})
     private BigDecimal rentPriceUnit;
-
+    
     @NotNull(message = "租期不能为空", groups = {CreateGroup.class, UpdateGroup.class})
-    private Integer  validDays;
-
+    private Integer validDays;
+    
     @NotNull(message = "租期单位不能为空", groups = {CreateGroup.class, UpdateGroup.class})
     private Integer rentUnit;
-
+    
     @NotNull(message = "加盟商不能为空", groups = {CreateGroup.class, UpdateGroup.class})
     private Long franchiseeId;
-
-    @NotNull(message = "租赁类型不能为空", groups = {CreateGroup.class, UpdateGroup.class})
+    
+    //@NotNull(message = "租赁类型不能为空", groups = {CreateGroup.class, UpdateGroup.class})
     private Integer rentType;
-
+    
     @NotNull(message = "是否赠送优惠券不能为空", groups = {CreateGroup.class, UpdateGroup.class})
     private Integer sendCoupon;
-
+    
     @NotNull(message = "上架状态不能为空", groups = {CreateGroup.class, UpdateGroup.class})
     private Integer status;
-
+    
     @NotNull(message = "套餐限制不能为空", groups = {CreateGroup.class, UpdateGroup.class})
     private Integer limitCount;
+    
     /**
      * 使用次数
      */
@@ -83,36 +85,38 @@ public class BatteryMemberCardQuery {
      * 套餐绑定的所有优惠券id
      */
     private String couponIds;
-
+    
     @NotNull(message = "是否退租金不能为空", groups = {CreateGroup.class, UpdateGroup.class})
     private Integer isRefund;
+    
     /**
      * 退租金天数限制
      */
     private Integer refundLimit;
-
+    
     @NotNull(message = "是否免押不能为空", groups = {CreateGroup.class, UpdateGroup.class})
     private Integer freeDeposite;
-
-//    @NotNull(message = "滞纳金不能为空", groups = {CreateGroup.class, UpdateGroup.class})
+    
+    //    @NotNull(message = "滞纳金不能为空", groups = {CreateGroup.class, UpdateGroup.class})
     private BigDecimal serviceCharge;
-
+    
     private String remark;
-
+    
     private List<String> batteryModels;
-
+    
     private Integer delFlag;
-
+    
     private List<Long> franchiseeIds;
-
+    
     private String batteryV;
-
+    
     private Long uid;
-
+    
     private List<Integer> rentTypes;
     
     /**
      * 套餐业务类型：0，换电套餐；1，车电一体套餐, 2. 企业渠道换电套餐
+     *
      * @see BatteryMemberCardBusinessTypeEnum
      */
     private Integer businessType;
@@ -151,5 +155,5 @@ public class BatteryMemberCardQuery {
      * 不分型号
      */
     public static final String REGARDLESS_OF_MODEL = "0";
-
+    
 }
