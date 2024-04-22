207f64897111164614d96aee08a78c2920b9424f
zhaozhilong <zhaozhilong@xiliulou.com>
2024-04-22 17:42:34 +0800

    fix(租电):换电限次套餐过期，套餐当前过期时间未变

--- src/main/java/com/xiliulou/electricity/queue/EleOperateQueueHandler.java
+++ src/main/java/com/xiliulou/electricity/queue/EleOperateQueueHandler.java
@@ -853,6 +853,7 @@ public class EleOperateQueueHandler {
         if (Objects.isNull(userBatteryMemberCardPackageLatest)) {
             UserBatteryMemberCard userBatteryMemberCardUpdate = new UserBatteryMemberCard();
             userBatteryMemberCardUpdate.setUid(userBatteryMemberCard.getUid());
+            userBatteryMemberCardUpdate.setOrderExpireTime(System.currentTimeMillis());
             userBatteryMemberCardUpdate.setMemberCardExpireTime(System.currentTimeMillis());
             userBatteryMemberCardService.updateByUid(userBatteryMemberCardUpdate);
         }
