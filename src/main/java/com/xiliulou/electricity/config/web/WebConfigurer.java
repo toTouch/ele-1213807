b374eccff0411178593c80806aeaaf7a951151df
maxiaodong <maxiaodong@xiliulou.com>
2024-04-24 16:54:46 +0800

    商户

--- src/main/java/com/xiliulou/electricity/config/web/WebConfigurer.java
+++ src/main/java/com/xiliulou/electricity/config/web/WebConfigurer.java
@@ -1,6 +1,7 @@
 package com.xiliulou.electricity.config.web;
 
 import com.xiliulou.electricity.interceptor.AdminSupperInterceptor;
+import com.xiliulou.electricity.interceptor.EnterprisePackageInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,9 +20,13 @@ public class WebConfigurer implements WebMvcConfigurer {
 	@Resource
 	private AdminSupperInterceptor adminSupperInterceptor;
 	
+	@Resource
+	private EnterprisePackageInterceptor enterprisePackageInterceptor;
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(adminSupperInterceptor).addPathPatterns("/admin/inner/super/**");
+		registry.addInterceptor(enterprisePackageInterceptor).addPathPatterns("/user/cloudBeanUse/**", "/user/enterprise/**", "/user/enterpriseInfo/**", "/user/cloudBean/**");
 	}
 
 }
