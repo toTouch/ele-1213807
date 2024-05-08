2cc1dd02d98932d7ad8454cb4a16fd2206f6f254
宋小惠 <songxiaohui.sxh@sunyur.com>
2024-05-08 09:34:52 +0800

    迁移绑定相关的逻辑

--- src/main/java/com/xiliulou/electricity/service/UserOauthBindService.java
+++ src/main/java/com/xiliulou/electricity/service/UserOauthBindService.java
@@ -44,7 +44,7 @@ public interface UserOauthBindService {
     
     List<UserOauthBind> selectListOauthByOpenIdAndSource(String openid, int source, Integer tenantId);
 
-    UserOauthBind queryByUserPhone(String phone, int source,Integer tenantId);
+    UserOauthBind queryByUserPhone(Long uid, String phone, int source,Integer tenantId);
 
     Pair<Boolean, Object> queryListByCondition(Integer size, Integer offset, Long uid, String thirdId, String phone,Integer tenantId);
 
@@ -60,6 +60,25 @@ public interface UserOauthBindService {
     
     UserOauthBind selectByUidAndPhone(String phone,Long uid,Integer tenantId);
     
+    /**
+     * 根据手机号、类型、租户查询用户
+     * @param phone 手机号
+     * @param source 类型
+     * @param tenantId 租户ID
+     * @return 绑定集
+     */
+    List<UserOauthBind> listUserByPhone(String phone,Integer source,Integer tenantId);
+    
+    /**
+     *
+     * @param phone
+     * @param source
+     * @param tenantId
+     * @return
+     *
+     * @see UserOauthBindService#listUserByPhone(String, Integer, Integer)
+     */
+    @Deprecated
     UserOauthBind selectUserByPhone(String phone,Integer source,Integer tenantId);
     
     Integer updateOpenIdByUid(String openId, Integer status, Long uid, Integer tenantId);
