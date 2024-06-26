42f2ba09795a5cea0159f7ff7d72838b45ce4feb
LiFeng <wxblifeng@163.com>
2024-06-26 17:52:07 +0800

    添加幂等性锁

--- src/main/java/com/xiliulou/electricity/service/userinfo/overdue/impl/UserInfoOverdueRemarkServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/userinfo/overdue/impl/UserInfoOverdueRemarkServiceImpl.java
@@ -4,20 +4,16 @@ package com.xiliulou.electricity.service.userinfo.overdue.impl;
 import com.xiliulou.cache.redis.RedisService;
 import com.xiliulou.electricity.constant.CacheConstant;
 import com.xiliulou.electricity.entity.userinfo.overdue.UserInfoOverdueRemark;
-import com.xiliulou.electricity.mapper.userinfo.overdue.UserInfoOverdueRemarkMapper;
 import com.xiliulou.electricity.query.userinfo.overdue.OverdueRemarkReq;
 import com.xiliulou.electricity.service.userinfo.overdue.UserInfoOverdueRemarkService;
 import com.xiliulou.electricity.tenant.TenantContextHolder;
 import com.xiliulou.electricity.tx.userinfo.overdue.UserInfoOverdueTxService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.tuple.Pair;
 import org.apache.commons.lang3.tuple.Triple;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Objects;
 
 /**
  * <p>
@@ -30,10 +26,9 @@ import java.util.Objects;
  * @author <a href="mailto:wxblifeng@163.com">PeakLee</a>
  * @since V1.0 2024/6/25
  **/
+@Slf4j
 @Service
 public class UserInfoOverdueRemarkServiceImpl implements UserInfoOverdueRemarkService {
-    
-    private static final Logger log = LoggerFactory.getLogger(UserInfoOverdueRemarkServiceImpl.class);
     
     private final RedisService redisService;
     
@@ -45,28 +40,28 @@ public class UserInfoOverdueRemarkServiceImpl implements UserInfoOverdueRemarkSe
     }
     
     @Override
-    public Triple<Boolean,String, String> insertOrUpdate(OverdueRemarkReq request) {
-        String lockKey = String.format(CacheConstant.CACHE_USERINFO_OVERDUE_REMARK_SAVE_LOCK,request.getType(),request.getUid());
-        if (redisService.getExpireTime(lockKey) > 0){
-            return Triple.of(false,"ELECTRICITY.0034","操作频繁");
+    public Triple<Boolean, String, String> insertOrUpdate(OverdueRemarkReq request) {
+        String lockKey = String.format(CacheConstant.CACHE_USERINFO_OVERDUE_REMARK_SAVE_LOCK, request.getType(), request.getUid());
+        if (!redisService.setNx(lockKey, String.valueOf(request.getUid()), 5 * 1000L, false)) {
+            return Triple.of(false, "ELECTRICITY.0033", "操作频繁");
         }
-        redisService.expire(lockKey,2000L,false);
         
         try {
             
             Integer tenantId = TenantContextHolder.getTenantId();
             UserInfoOverdueRemark remark = new UserInfoOverdueRemark();
             remark.setUid(request.getUid());
-            remark.setRemark(StringUtils.defaultIfBlank(request.getRemark(),""));
+            remark.setRemark(StringUtils.defaultIfBlank(request.getRemark(), ""));
             remark.setType(request.getType());
             remark.setTenantId(tenantId.longValue());
             
             userInfoOverdueTxService.insertOrUpdate(remark);
             
-        }finally {
+        } finally {
             redisService.delete(lockKey);
         }
         
-        return Triple.of(true,null,null);
+        return Triple.of(true, null, null);
     }
+    
 }
