d55a1f19eaa1f36278d4466fad8a53a82ac03f2b
宋进攀 <15927100469@163.com>
2024-07-26 17:03:57 +0800

    fix：返利配置相关更新接口加锁

---
+++ src/main/java/com/xiliulou/electricity/utils/SpinLockRedisService.java
@@ -0,0 +1,50 @@
+package com.xiliulou.electricity.utils;
+
+import com.xiliulou.cache.redis.RedisService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Description ...
+ * @Author: SongJP
+ * @Date: 2024/7/26 15:33
+ */
+@Component
+@Slf4j
+public class SpinLockRedisService extends RedisService {
+    
+    final Long DEFAULT_EXPIRE_TIME = 5 * 1000L;
+    
+    final Long DEFAULT_MAX_SPIN_TIME = 5 * 1000L;
+    
+    @Resource
+    private RedisTemplate<String, String> redisTemplate;
+    
+    public boolean tryLockWithSpin(String lockKey) {
+        return tryLockWithSpin(lockKey, DEFAULT_EXPIRE_TIME, DEFAULT_MAX_SPIN_TIME);
+    }
+    
+    public boolean tryLockWithSpin(String lockKey, long expireTime, long maxSpinTime) {
+        long startTime = System.currentTimeMillis();
+        
+        while (System.currentTimeMillis() - startTime < maxSpinTime) {
+            Boolean result = redisTemplate.opsForValue().setIfAbsent(lockKey, "1", expireTime, TimeUnit.MILLISECONDS);
+            if (result != null && result) {
+                return true;
+            }
+            
+            try {
+                Thread.sleep(100L);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        
+        log.warn("GET LOCK FAIL, That's a dangerous situation, lockKey:{}", lockKey);
+        return false;
+    }
+}
