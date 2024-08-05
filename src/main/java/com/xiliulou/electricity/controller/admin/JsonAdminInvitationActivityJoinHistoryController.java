9c8c48e1020c3d208575d9a20220be179f24fe63
zhangbozhi <zbz1206@outlook.com>
2024-08-05 16:00:02 +0800

    feat: 7月份小需求优化 需求6
    https://benyun.feishu.cn/wiki/X2Ghwr1pAiYBl3k6TNpco6RVndI

--- src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminInvitationActivityJoinHistoryController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminInvitationActivityJoinHistoryController.java
@@ -3,26 +3,19 @@ package com.xiliulou.electricity.controller.admin;
 import com.xiliulou.core.web.R;
 import com.xiliulou.electricity.query.InvitationActivityJoinHistoryQuery;
 import com.xiliulou.electricity.query.InvitationActivityRecordQuery;
-import com.xiliulou.electricity.request.InvitationActivityJoinHistoryRequest;
 import com.xiliulou.electricity.request.activity.InvitationActivityAnalysisRequest;
 import com.xiliulou.electricity.service.InvitationActivityJoinHistoryService;
 import com.xiliulou.electricity.service.UserDataScopeService;
 import com.xiliulou.electricity.tenant.TenantContextHolder;
 import com.xiliulou.electricity.utils.SecurityUtils;
-import com.xiliulou.electricity.vo.activity.InvitationActivityAnalysisAdminVO;
 import com.xiliulou.security.bean.TokenUser;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-import java.util.Collections;
 import java.util.Objects;
-
-import static org.bouncycastle.asn1.x500.style.RFC4519Style.uid;
 
 /**
  * @author zzlong
@@ -38,25 +31,13 @@ public class JsonAdminInvitationActivityJoinHistoryController {
 
     @Autowired
     UserDataScopeService userDataScopeService;
-    
-    /**
-     * 邀请人记录--查看参与详情
-     *
-     * @param size 默认10
-     * @param offset 偏移量
-     * @param beginTime
-     * @param endTime
-     * @param activityId 活动id
-     * @param id
-     * @param status 活动状态
-     * @param joinUid 参与人uid
-     * @param activityName
-     * @param payCount
-     * @return
-     */
+
     @GetMapping("/admin/invitationActivityJoinHistory/page")
-    public R page(@RequestParam("size") long size, @RequestParam("offset") long offset,
-            InvitationActivityJoinHistoryRequest activityJoinHistoryRequest) {
+    public R page(@RequestParam("size") long size, @RequestParam("offset") long offset, @RequestParam(value = "beginTime", required = false) Long beginTime,
+            @RequestParam(value = "endTime", required = false) Long endTime, @RequestParam(value = "activityId", required = false) Long activityId,
+            @RequestParam(value = "id", required = false) Long id, @RequestParam(value = "status", required = false) Integer status,
+            @RequestParam(value = "joinUid", required = false) Long joinUid, @RequestParam(value = "activityName", required = false) String activityName,
+            @RequestParam(value = "payCount", required = false) Integer payCount) {
         if (size < 0 || size > 50) {
             size = 10L;
         }
@@ -69,63 +50,30 @@ public class JsonAdminInvitationActivityJoinHistoryController {
         if (Objects.isNull(user)) {
             return R.fail("ELECTRICITY.0001", "未找到用户");
         }
-        if (Objects.isNull(activityJoinHistoryRequest.getUid())) {
-            return R.ok(Collections.emptyList());
-        }
         
         InvitationActivityJoinHistoryQuery query = InvitationActivityJoinHistoryQuery.builder()
                 .size(size)
-                .offset(offset)
-                .joinUid(activityJoinHistoryRequest.getJoinUid())
-                .recordId(activityJoinHistoryRequest.getId())
-                .beginTime(activityJoinHistoryRequest.getBeginTime())
-                .endTime(activityJoinHistoryRequest.getEndTime())
-                .uid(activityJoinHistoryRequest.getUid())
-                .activityId(activityJoinHistoryRequest.getActivityId())
-                .activityName(activityJoinHistoryRequest.getActivityName())
-                .status(activityJoinHistoryRequest.getStatus())
-                .payCount(activityJoinHistoryRequest.getPayCount())
+                .offset(offset).joinUid(joinUid).recordId(id).beginTime(beginTime).endTime(endTime).activityId(activityId).activityName(activityName).status(status)
+                .payCount(payCount)
                 .tenantId(TenantContextHolder.getTenantId())
                 .build();
 
         return R.ok(invitationActivityJoinHistoryService.selectByPage(query));
     }
-    
-    /**
-     * 邀请人记录--查看参与详情
-     *
-     * @param beginTime
-     * @param endTime
-     * @param activityId 活动id
-     * @param id
-     * @param status 活动状态
-     * @param joinUid 参与人uid
-     * @param activityName
-     * @param payCount
-     * @return
-     */
+
     @GetMapping("/admin/invitationActivityJoinHistory/queryCount")
-    public R count(@RequestParam(value = "activityId", required = false) Long activityId,
-            InvitationActivityJoinHistoryRequest activityJoinHistoryRequest) {
+    public R count(@RequestParam(value = "activityId", required = false) Long activityId, @RequestParam(value = "id", required = false) Long id,
+            @RequestParam(value = "beginTime", required = false) Long beginTime, @RequestParam(value = "endTime", required = false) Long endTime,
+            @RequestParam(value = "status", required = false) Integer status, @RequestParam(value = "joinUid", required = false) Long joinUid,
+            @RequestParam(value = "activityName", required = false) String activityName, @RequestParam(value = "payCount", required = false) Integer payCount) {
 
         TokenUser user = SecurityUtils.getUserInfo();
         if (Objects.isNull(user)) {
             return R.fail("ELECTRICITY.0001", "未找到用户");
         }
-        if (Objects.isNull(activityJoinHistoryRequest.getUid())) {
-            return R.ok(Collections.emptyList());
-        }
         
-        InvitationActivityJoinHistoryQuery query = InvitationActivityJoinHistoryQuery.builder()
-                .joinUid(activityJoinHistoryRequest.getJoinUid())
-                .recordId(activityJoinHistoryRequest.getId())
-                .beginTime(activityJoinHistoryRequest.getBeginTime())
-                .endTime(activityJoinHistoryRequest.getEndTime())
-                .uid(activityJoinHistoryRequest.getUid())
-                .activityId(activityJoinHistoryRequest.getActivityId())
-                .activityName(activityJoinHistoryRequest.getActivityName())
-                .status(activityJoinHistoryRequest.getStatus())
-                .payCount(activityJoinHistoryRequest.getPayCount())
+        InvitationActivityJoinHistoryQuery query = InvitationActivityJoinHistoryQuery.builder().joinUid(joinUid).recordId(id).beginTime(beginTime).endTime(endTime)
+                .activityId(activityId).activityName(activityName).status(status).payCount(payCount)
                 .tenantId(TenantContextHolder.getTenantId())
                 .build();
 
@@ -146,15 +94,6 @@ public class JsonAdminInvitationActivityJoinHistoryController {
         TokenUser user = SecurityUtils.getUserInfo();
         if (Objects.isNull(user)) {
             return R.fail("ELECTRICITY.0001", "未找到用户");
-        }
-        if (Objects.isNull(activityId)) {
-            InvitationActivityAnalysisAdminVO invitationActivityAnalysisAdminVO = new InvitationActivityAnalysisAdminVO();
-            invitationActivityAnalysisAdminVO.setTotalShareCount(0);
-            invitationActivityAnalysisAdminVO.setTotalInvitationCount(0);
-            invitationActivityAnalysisAdminVO.setFirstTotalIncome(BigDecimal.ZERO);
-            invitationActivityAnalysisAdminVO.setRenewTotalIncome(BigDecimal.ZERO);
-            invitationActivityAnalysisAdminVO.setTotalIncome(BigDecimal.ZERO);
-            return R.ok();
         }
         
         InvitationActivityAnalysisRequest request = InvitationActivityAnalysisRequest.builder().uid(uid).activityId(activityId).timeType(timeType).beginTime(beginTime).endTime(endTime).build();
