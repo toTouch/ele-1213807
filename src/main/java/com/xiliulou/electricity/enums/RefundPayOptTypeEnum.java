d97f35579cdea79e8c6b4ccf73502792755ac025
caobotao <caobotao.cbt@sunyur.com>
2024-08-08 18:08:05 +0800

    add

--- src/main/java/com/xiliulou/electricity/enums/RefundPayOptTypeEnum.java
+++ src/main/java/com/xiliulou/electricity/enums/RefundPayOptTypeEnum.java
@@ -17,7 +17,7 @@ public enum RefundPayOptTypeEnum implements BasicEnum<String, String> {
     
     CAR_DEPOSIT_REFUND_CALL_BACK("CAR_DEPOSIT_REFUND_CALL_BACK", "租车套餐押金退还"),
     CAR_RENT_REFUND_CALL_BACK("CAR_RENT_REFUND_CALL_BACK", "租车套餐租金退还"),
-    BATTERY_DEPOSIT_REFUND_CALL_BACK("BATTERY_DEPOSIT_REFUND_CALL_BACK", "电池套餐租金退还"),
+    BATTERY_DEPOSIT_REFUND_CALL_BACK("BATTERY_DEPOSIT_REFUND_CALL_BACK", "电池套餐押金退还"),
     BATTERY_RENT_REFUND_CALL_BACK("wxRefundPayBatteryRentServiceImpl", "电池套餐租金退还"),
     ;
 
