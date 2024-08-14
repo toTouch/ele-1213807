6202c52a64f0a2fba5d81666e9d0c97045888413
renhang <1031948347@qq.com>
2024-08-14 11:14:57 +0800

    fix(舒适换电): 日志

--- src/main/java/com/xiliulou/electricity/service/impl/ElectricityCabinetChooseCellConfigServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/ElectricityCabinetChooseCellConfigServiceImpl.java
@@ -185,6 +185,9 @@ public class ElectricityCabinetChooseCellConfigServiceImpl implements Electricit
         // 每个位置的数据反序列
         List<Integer> positionCellList = StrUtil.isNotBlank(positionCell) ? JsonUtil.fromJsonArray(positionCell, Integer.class) : new ArrayList<>();
         List<ElectricityCabinetBox> boxes = comfortExchangeBox.stream().filter(item -> positionCellList.contains(Integer.valueOf(item.getCellNo()))).collect(Collectors.toList());
+        log.info("COMFORT EXCHANGE GET FULL INFO! getPositionFullCell.comfortExchangeBox is {}, positionCellBox is {}", JsonUtil.toJson(comfortExchangeBox),
+                CollUtil.isNotEmpty(boxes) ? JsonUtil.toJson(boxes) : "null");
+        
         if (CollUtil.isEmpty(boxes)) {
             log.info("COMFORT EXCHANGE GET FULL INFO! box is null, positionCell is {}", positionCell);
             return Pair.of(false, null);
@@ -310,6 +313,9 @@ public class ElectricityCabinetChooseCellConfigServiceImpl implements Electricit
     private static Pair<Boolean, String> getPositionEmptyCell(List<ElectricityCabinetBox> comfortExchangeBox, String positionCell) {
         List<Integer> positionCellList = StrUtil.isNotBlank(positionCell) ? JsonUtil.fromJsonArray(positionCell, Integer.class) : new ArrayList<>();
         List<ElectricityCabinetBox> boxes = comfortExchangeBox.stream().filter(item -> positionCellList.contains(Integer.valueOf(item.getCellNo()))).collect(Collectors.toList());
+        log.info("COMFORT EXCHANGE GET EMPTY INFO! getPositionEmptyCell.comfortExchangeBox is {}, positionCellBox is {}", JsonUtil.toJson(comfortExchangeBox),
+                CollUtil.isNotEmpty(boxes) ? JsonUtil.toJson(boxes) : "null");
+        
         if (CollUtil.isEmpty(boxes)) {
             return Pair.of(false, null);
         }
