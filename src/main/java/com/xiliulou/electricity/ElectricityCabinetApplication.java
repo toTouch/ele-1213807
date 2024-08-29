f39797a3891cd3fa3d6ca85037b6d781711e5751
renhang <1031948347@qq.com>
2024-08-29 09:12:25 +0800

    fix(免押代扣): 导包

--- src/main/java/com/xiliulou/electricity/ElectricityCabinetApplication.java
+++ src/main/java/com/xiliulou/electricity/ElectricityCabinetApplication.java
@@ -13,6 +13,7 @@ import com.xiliulou.faceid.EnableFaceid;
 import com.xiliulou.feishu.EnableFeishu;
 import com.xiliulou.iot.EnableIot;
 import com.xiliulou.mq.EnableMq;
+import com.xiliulou.pay.EnableFengYun;
 import com.xiliulou.pay.EnableFreeDeposit;
 import com.xiliulou.pay.EnablePay;
 import com.xiliulou.storage.EnableStorage;
