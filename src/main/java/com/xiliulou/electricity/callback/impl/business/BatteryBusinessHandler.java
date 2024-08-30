d338eb76ef0fa38b2c39687f3e8cd52e2367df91
LiFeng <wxblifeng@163.com>
2024-08-30 17:49:45 +0800

    add: 免押代扣回调

--- src/main/java/com/xiliulou/electricity/callback/impl/business/BatteryBusinessHandler.java
+++ src/main/java/com/xiliulou/electricity/callback/impl/business/BatteryBusinessHandler.java
@@ -79,91 +79,98 @@ public class BatteryBusinessHandler implements BusinessHandler {
     
     @Override
     public boolean freeDeposit(FreeDepositOrder order) {
-        
-        Long uid = order.getUid();
-        UserInfo userInfo = userInfoService.queryByUidFromCache(uid);
-        if (Objects.isNull(userInfo)) {
-            log.warn("userInfo is null, uid is {}", uid);
-            return true;
+        try {
+            Long uid = order.getUid();
+            UserInfo userInfo = userInfoService.queryByUidFromCache(uid);
+            if (Objects.isNull(userInfo)) {
+                log.warn("userInfo is null, uid is {}", uid);
+                return true;
+            }
+            
+            UserBatteryDeposit userBatteryDeposit = userBatteryDepositService.selectByUidFromCache(uid);
+            if (Objects.isNull(userBatteryDeposit)) {
+                log.warn("handlerFreeDepositSuccess warn! userBatteryDeposit is null, uid is {}", uid);
+                return true;
+            }
+            EleDepositOrder eleDepositOrder = eleDepositOrderService.queryByOrderId(userBatteryDeposit.getOrderId());
+            if (Objects.isNull(eleDepositOrder)) {
+                log.warn("handlerFreeDepositSuccess warn! eleDepositOrder is null, orderId is {}", userBatteryDeposit.getOrderId());
+                return true;
+            }
+            // 更新押金订单状态
+            EleDepositOrder eleDepositOrderUpdate = new EleDepositOrder();
+            eleDepositOrderUpdate.setId(eleDepositOrder.getId());
+            eleDepositOrderUpdate.setStatus(EleDepositOrder.STATUS_SUCCESS);
+            eleDepositOrderUpdate.setUpdateTime(System.currentTimeMillis());
+            eleDepositOrderService.update(eleDepositOrderUpdate);
+            
+            // 绑定加盟商、更新押金状态
+            UserInfo userInfoUpdate = new UserInfo();
+            userInfoUpdate.setUid(uid);
+            userInfoUpdate.setFranchiseeId(eleDepositOrder.getFranchiseeId());
+            userInfoUpdate.setBatteryDepositStatus(UserInfo.BATTERY_DEPOSIT_STATUS_YES);
+            userInfoUpdate.setUpdateTime(System.currentTimeMillis());
+            userInfoService.updateByUid(userInfoUpdate);
+            
+            // 绑定电池型号
+            List<String> batteryTypeList = memberCardBatteryTypeService.selectBatteryTypeByMid(eleDepositOrder.getMid());
+            if (CollectionUtils.isNotEmpty(batteryTypeList)) {
+                userBatteryTypeService.batchInsert(userBatteryTypeService.buildUserBatteryType(batteryTypeList, userInfo));
+            }
+        }catch (Exception e){
+            log.error("battery freeDeposit error!", e);
+            return false;
         }
-        
-        UserBatteryDeposit userBatteryDeposit = userBatteryDepositService.selectByUidFromCache(uid);
-        if (Objects.isNull(userBatteryDeposit)) {
-            log.warn("handlerFreeDepositSuccess warn! userBatteryDeposit is null, uid is {}", uid);
-            return true;
-        }
-        EleDepositOrder eleDepositOrder = eleDepositOrderService.queryByOrderId(userBatteryDeposit.getOrderId());
-        if (Objects.isNull(eleDepositOrder)) {
-            log.warn("handlerFreeDepositSuccess warn! eleDepositOrder is null, orderId is {}", userBatteryDeposit.getOrderId());
-            return true;
-        }
-        // 更新押金订单状态
-        EleDepositOrder eleDepositOrderUpdate = new EleDepositOrder();
-        eleDepositOrderUpdate.setId(eleDepositOrder.getId());
-        eleDepositOrderUpdate.setStatus(EleDepositOrder.STATUS_SUCCESS);
-        eleDepositOrderUpdate.setUpdateTime(System.currentTimeMillis());
-        eleDepositOrderService.update(eleDepositOrderUpdate);
-        
-        // 绑定加盟商、更新押金状态
-        UserInfo userInfoUpdate = new UserInfo();
-        userInfoUpdate.setUid(uid);
-        userInfoUpdate.setFranchiseeId(eleDepositOrder.getFranchiseeId());
-        userInfoUpdate.setBatteryDepositStatus(UserInfo.BATTERY_DEPOSIT_STATUS_YES);
-        userInfoUpdate.setUpdateTime(System.currentTimeMillis());
-        userInfoService.updateByUid(userInfoUpdate);
-        
-        // 绑定电池型号
-        List<String> batteryTypeList = memberCardBatteryTypeService.selectBatteryTypeByMid(eleDepositOrder.getMid());
-        if (CollectionUtils.isNotEmpty(batteryTypeList)) {
-            userBatteryTypeService.batchInsert(userBatteryTypeService.buildUserBatteryType(batteryTypeList, userInfo));
-        }
-        
         return true;
     }
     
     @Override
     public boolean unfree(FreeDepositOrder order) {
-        // 更新退款订单
-        EleRefundOrder eleRefundOrder = eleRefundOrderService.selectLatestRefundDepositOrder(order.getOrderId());
-        EleRefundOrder eleRefundOrderUpdate = new EleRefundOrder();
-        eleRefundOrderUpdate.setId(eleRefundOrder.getId());
-        eleRefundOrderUpdate.setUpdateTime(System.currentTimeMillis());
-        eleRefundOrderUpdate.setStatus(EleRefundOrder.STATUS_SUCCESS);
-        eleRefundOrderUpdate.setUpdateTime(System.currentTimeMillis());
-        eleRefundOrderService.update(eleRefundOrderUpdate);
-        
-        UserInfo userInfo = userInfoService.queryByUidFromCache(order.getUid());
-        Long uid = userInfo.getUid();
-        
-        UserInfo updateUserInfo = new UserInfo();
-        updateUserInfo.setUid(userInfo.getUid());
-        updateUserInfo.setBatteryDepositStatus(UserInfo.BATTERY_DEPOSIT_STATUS_NO);
-        updateUserInfo.setUpdateTime(System.currentTimeMillis());
-        userInfoService.updateByUid(updateUserInfo);
-        
-        // 更新用户套餐订单为已失效
-        electricityMemberCardOrderService.batchUpdateStatusByOrderNo(userBatteryMemberCardService.selectUserBatteryMemberCardOrder(uid),
-                ElectricityMemberCardOrder.USE_STATUS_EXPIRE);
-        
-        userBatteryMemberCardService.unbindMembercardInfoByUid(userInfo.getUid());
-        userBatteryDepositService.logicDeleteByUid(userInfo.getUid());
-        
-        InsuranceUserInfo insuranceUserInfo = insuranceUserInfoService.selectByUidAndTypeFromCache(uid, FranchiseeInsurance.INSURANCE_TYPE_BATTERY);
-        if (Objects.nonNull(insuranceUserInfo)) {
-            insuranceUserInfoService.deleteById(insuranceUserInfo);
+        try {
+            // 更新退款订单
+            EleRefundOrder eleRefundOrder = eleRefundOrderService.selectLatestRefundDepositOrder(order.getOrderId());
+            EleRefundOrder eleRefundOrderUpdate = new EleRefundOrder();
+            eleRefundOrderUpdate.setId(eleRefundOrder.getId());
+            eleRefundOrderUpdate.setUpdateTime(System.currentTimeMillis());
+            eleRefundOrderUpdate.setStatus(EleRefundOrder.STATUS_SUCCESS);
+            eleRefundOrderUpdate.setUpdateTime(System.currentTimeMillis());
+            eleRefundOrderService.update(eleRefundOrderUpdate);
             
-            // 更新用户保险订单为已失效
-            insuranceOrderService.updateUseStatusForRefund(insuranceUserInfo.getInsuranceOrderId(), InsuranceOrder.INVALID);
+            UserInfo userInfo = userInfoService.queryByUidFromCache(order.getUid());
+            Long uid = userInfo.getUid();
+            
+            UserInfo updateUserInfo = new UserInfo();
+            updateUserInfo.setUid(userInfo.getUid());
+            updateUserInfo.setBatteryDepositStatus(UserInfo.BATTERY_DEPOSIT_STATUS_NO);
+            updateUserInfo.setUpdateTime(System.currentTimeMillis());
+            userInfoService.updateByUid(updateUserInfo);
+            
+            // 更新用户套餐订单为已失效
+            electricityMemberCardOrderService.batchUpdateStatusByOrderNo(userBatteryMemberCardService.selectUserBatteryMemberCardOrder(uid),
+                    ElectricityMemberCardOrder.USE_STATUS_EXPIRE);
+            
+            userBatteryMemberCardService.unbindMembercardInfoByUid(userInfo.getUid());
+            userBatteryDepositService.logicDeleteByUid(userInfo.getUid());
+            
+            InsuranceUserInfo insuranceUserInfo = insuranceUserInfoService.selectByUidAndTypeFromCache(uid, FranchiseeInsurance.INSURANCE_TYPE_BATTERY);
+            if (Objects.nonNull(insuranceUserInfo)) {
+                insuranceUserInfoService.deleteById(insuranceUserInfo);
+                
+                // 更新用户保险订单为已失效
+                insuranceOrderService.updateUseStatusForRefund(insuranceUserInfo.getInsuranceOrderId(), InsuranceOrder.INVALID);
+            }
+            
+            userInfoService.unBindUserFranchiseeId(userInfo.getUid());
+            
+            // 修改企业用户代付状态为代付过期
+            enterpriseChannelUserService.updatePaymentStatusForRefundDeposit(userInfo.getUid(), EnterprisePaymentStatusEnum.PAYMENT_TYPE_EXPIRED.getCode());
+            
+            // 删除用户分组
+            userInfoGroupDetailService.handleAfterRefundDeposit(userInfo.getUid());
+        }catch (Exception e){
+            log.error("battery unfree error!", e);
+            return false;
         }
-        
-        userInfoService.unBindUserFranchiseeId(userInfo.getUid());
-        
-        // 修改企业用户代付状态为代付过期
-        enterpriseChannelUserService.updatePaymentStatusForRefundDeposit(userInfo.getUid(), EnterprisePaymentStatusEnum.PAYMENT_TYPE_EXPIRED.getCode());
-        
-        // 删除用户分组
-        userInfoGroupDetailService.handleAfterRefundDeposit(userInfo.getUid());
-        
         return true;
     }
     
