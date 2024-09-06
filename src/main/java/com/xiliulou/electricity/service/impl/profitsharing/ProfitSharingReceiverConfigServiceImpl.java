2d2c4f62518c5828dc4cc01f42c6655d6aa38d1b
caobotao <caobotao.cbt@sunyur.com>
2024-09-06 14:23:46 +0800

    操作记录bug

--- src/main/java/com/xiliulou/electricity/service/impl/profitsharing/ProfitSharingReceiverConfigServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/profitsharing/ProfitSharingReceiverConfigServiceImpl.java
@@ -421,7 +421,7 @@ public class ProfitSharingReceiverConfigServiceImpl implements ProfitSharingRece
         String newScale = newConfig.getScale().multiply(new BigDecimal(100)).stripTrailingZeros().toPlainString() + "%";
         
         Map<String, String> record = Maps.newHashMapWithExpectedSize(1);
-        record.put("account", old.getAccount() + "/" + old.getReceiverName());
+        record.put("account", old.getReceiverName());
         record.put("scale", newScale);
         record.put("remark", newConfig.getRemark());
         
@@ -434,7 +434,7 @@ public class ProfitSharingReceiverConfigServiceImpl implements ProfitSharingRece
     
     private void operateDeleteRecord(ProfitSharingReceiverConfig newConfig) {
         Map<String, String> record = Maps.newHashMapWithExpectedSize(1);
-        record.put("account", newConfig.getAccount() + "/" + newConfig.getReceiverName());
+        record.put("account", newConfig.getReceiverName());
         operateRecordUtil.record(null, record);
     }
     
