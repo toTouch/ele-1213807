cdd72c13790fa19d68e06655af3e3e1fee72f55c
宋进攀 <15927100469@163.com>
2024-09-09 22:48:54 +0800

    fix：定时任务取消调试

--- src/main/java/com/xiliulou/electricity/ElectricityCabinetApplication.java
+++ src/main/java/com/xiliulou/electricity/ElectricityCabinetApplication.java
@@ -24,6 +24,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author: eclair
@@ -51,6 +52,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableFreeDeposit
 @EnableFengYun
 @EnableEsign
+@EnableScheduling
 @Slf4j
 public class ElectricityCabinetApplication implements CommandLineRunner {
     
