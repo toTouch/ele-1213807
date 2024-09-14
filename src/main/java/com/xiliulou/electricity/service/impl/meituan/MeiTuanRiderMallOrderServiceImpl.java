085500016baf885c9dc6f430cb4404e7ad1a7a87
he yafeng <13319290867@163.com>
2024-09-14 16:22:45 +0800

    美团骑手商城：filter

--- src/main/java/com/xiliulou/electricity/service/impl/meituan/MeiTuanRiderMallOrderServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/meituan/MeiTuanRiderMallOrderServiceImpl.java
@@ -191,6 +191,9 @@ public class MeiTuanRiderMallOrderServiceImpl implements MeiTuanRiderMallOrderSe
             Long memberCardId = meiTuanRiderMallOrder.getPackageId();
             
             BatteryMemberCard batteryMemberCard = batteryMemberCardService.queryByIdFromCache(memberCardId);
+            
+            log.info("MeiTuan order redeem, batteryMemberCard={}, memberCardId={}", batteryMemberCard, memberCardId);
+            
             if (Objects.isNull(batteryMemberCard) || Objects.isNull(batteryMemberCard.getTenantId()) || !Objects.equals(batteryMemberCard.getTenantId(), tenantId)) {
                 log.warn("MeiTuan order redeem fail! not found batteryMemberCard,uid={}, memberCardId={}", uid, memberCardId);
                 return Triple.of(false, "ELECTRICITY.00121", "电池套餐不存在");
