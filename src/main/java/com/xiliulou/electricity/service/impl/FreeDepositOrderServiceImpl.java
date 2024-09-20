f6547a704b8b36ac86d9275f846c1dedda9d2c30
renhang <1031948347@qq.com>
2024-09-20 12:56:52 +0800

    fix(免押代扣): 免押电兼容旧版本

--- src/main/java/com/xiliulou/electricity/service/impl/FreeDepositOrderServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/FreeDepositOrderServiceImpl.java
@@ -1046,6 +1046,7 @@ public class FreeDepositOrderServiceImpl implements FreeDepositOrderService {
                 .orderId(eleDepositOrder.getOrderId()).phone(freeQuery.getPhoneNumber()).realName(freeQuery.getRealName()).createTime(System.currentTimeMillis())
                 .updateTime(System.currentTimeMillis()).payStatus(FreeDepositOrder.PAY_STATUS_INIT).storeId(eleDepositOrder.getStoreId())
                 .franchiseeId(eleDepositOrder.getFranchiseeId()).tenantId(TenantContextHolder.getTenantId()).transAmt(eleDepositOrder.getPayAmount().doubleValue())
+                .payTransAmt(eleDepositOrder.getPayAmount().doubleValue())
                 .type(FreeDepositOrder.TYPE_ZHIFUBAO).depositType(FreeDepositOrder.DEPOSIT_TYPE_BATTERY).build();
         
         PxzCommonRequest<PxzFreeDepositOrderRequest> query = new PxzCommonRequest<>();
