5db907006b997d55e7c295df50957ab3579f5b0a
maxiaodong <maxiaodong@xiliulou.com>
2024-09-23 17:40:58 +0800

    故障告警短信对接消息中心改动

--- src/main/java/com/xiliulou/electricity/request/SendReceiverRequest.java
+++ src/main/java/com/xiliulou/electricity/request/SendReceiverRequest.java
@@ -1,6 +1,7 @@
 package com.xiliulou.electricity.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class SendReceiverRequest {
     
     /**
