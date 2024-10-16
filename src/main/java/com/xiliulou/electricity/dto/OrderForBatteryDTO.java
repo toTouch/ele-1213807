72d9741bb1005011da44d61c33620970733cacf0
宋进攀 <15927100469@163.com>
2024-10-16 11:03:34 +0800

    modify：租借电池及电表异常--DTO类增加无参、全参构造注解

--- src/main/java/com/xiliulou/electricity/dto/OrderForBatteryDTO.java
+++ src/main/java/com/xiliulou/electricity/dto/OrderForBatteryDTO.java
@@ -1,7 +1,9 @@
 package com.xiliulou.electricity.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @Description ...
@@ -10,6 +12,8 @@ import lombok.Data;
  */
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderForBatteryDTO {
     
     private String orderId;
