15b3638cf1f048c23708a07e6445cc6515ae7865
maxiaodong <maxiaodong@xiliulou.com>
2024-10-22 11:23:26 +0800

    商户推广费用户统计分析接口优化：自测

--- src/main/java/com/xiliulou/electricity/service/impl/merchant/MerchantPromotionFeeServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/merchant/MerchantPromotionFeeServiceImpl.java
@@ -596,7 +596,6 @@ public class MerchantPromotionFeeServiceImpl implements MerchantPromotionFeeServ
     
     private List<MerchantStatisticsUserVO> buildMerchantJoinRecordCommonCount(Integer type, Long uid, Long startTime, Long endTime, Integer status, boolean channelEmployeeFlag) {
         if (channelEmployeeFlag) {
-            int result = 0;
             //昨日扫码人数：扫码绑定时间=昨日0点～今日0点；
             MerchantPromotionScanCodeQueryModel scanCodeQueryModel = MerchantPromotionScanCodeQueryModel.builder().tenantId(TenantContextHolder.getTenantId())
                     .channelEmployeeUid(SecurityUtils.getUid()).status(status).type(PromotionFeeQueryTypeEnum.MERCHANT.getCode()).uid(uid).build();
