41fc064f7fefefaf894a411ac21dd89e6774748e
he yafeng <13319290867@163.com>
2024-10-23 13:50:26 +0800

    1.修改邀请人记录优化 2.商户邀请成功时间默认为createTime

--- src/main/java/com/xiliulou/electricity/service/merchant/MerchantJoinRecordService.java
+++ src/main/java/com/xiliulou/electricity/service/merchant/MerchantJoinRecordService.java
@@ -125,4 +125,6 @@ public interface MerchantJoinRecordService {
     List<MerchantScanCodeRecordVO> listScanCodeRecordPage(MerchantScanCodeRecordPageRequest merchantScanCodeRecordPageRequest);
     
     Integer countScanCodeRecord(MerchantScanCodeRecordPageRequest merchantScanCodeRecordPageRequest);
+    
+    MerchantJoinRecord queryRemoveSuccessRecord(Long joinUid, Long inviterUid, Integer tenantId);
 }
