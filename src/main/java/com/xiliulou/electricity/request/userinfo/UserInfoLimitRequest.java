47c86d1b4b9cbf5ec138b516949d0a4b932a5a3b
he yafeng <13319290867@163.com>
2024-10-29 16:54:38 +0800

    换电套餐购买次数限制

--- src/main/java/com/xiliulou/electricity/request/userinfo/UserInfoLimitRequest.java
+++ src/main/java/com/xiliulou/electricity/request/userinfo/UserInfoLimitRequest.java
@@ -1,7 +1,9 @@
 package com.xiliulou.electricity.request.userinfo;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
@@ -9,6 +11,9 @@ import javax.validation.constraints.NotNull;
  * @author HeYafeng
  * @date 2024/10/24 16:23:46
  */
+
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Data
 public class UserInfoLimitRequest {
