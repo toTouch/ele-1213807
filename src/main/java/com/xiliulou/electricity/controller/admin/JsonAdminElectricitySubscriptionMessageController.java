e8a3b2486af771b5393b1ed548910c4c669381da
he yafeng <13319290867@163.com>
2024-11-04 19:04:09 +0800

    客服手机号新接口加权限

--- src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminElectricitySubscriptionMessageController.java
+++ src/main/java/com/xiliulou/electricity/controller/admin/JsonAdminElectricitySubscriptionMessageController.java
@@ -3,6 +3,7 @@ package com.xiliulou.electricity.controller.admin;
 import com.xiliulou.cache.redis.RedisService;
 import com.xiliulou.core.web.R;
 import com.xiliulou.electricity.entity.ElectricitySubscriptionMessage;
+import com.xiliulou.electricity.entity.User;
 import com.xiliulou.electricity.query.ServicePhoneQuery;
 import com.xiliulou.electricity.request.ServicePhoneRequest;
 import com.xiliulou.electricity.service.ElectricityConfigService;
@@ -11,7 +12,9 @@ import com.xiliulou.electricity.service.ServicePhoneService;
 import com.xiliulou.electricity.service.UserService;
 import com.xiliulou.electricity.tenant.TenantContextHolder;
 import com.xiliulou.electricity.utils.OperateRecordUtil;
+import com.xiliulou.electricity.utils.SecurityUtils;
 import com.xiliulou.electricity.utils.ValidList;
+import com.xiliulou.security.bean.TokenUser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -110,11 +113,30 @@ public class JsonAdminElectricitySubscriptionMessageController {
     
     @GetMapping("admin/servicePhone/all")
     public R getServicePhones() {
+        TokenUser user = SecurityUtils.getUserInfo();
+        if (Objects.isNull(user)) {
+            return R.fail("ELECTRICITY.0001", "未找到用户");
+        }
+    
+        if (!(SecurityUtils.isAdmin() || Objects.equals(user.getDataType(), User.DATA_TYPE_OPERATE) || Objects.equals(user.getDataType(), User.DATA_TYPE_FRANCHISEE))) {
+            return R.fail("ELECTRICITY.0066", "用户权限不足");
+        }
+        
+        
         return R.ok(servicePhoneService.listPhones(TenantContextHolder.getTenantId()));
     }
     
     @PostMapping("admin/servicePhone/update")
     public R insertOrUpdate(@RequestBody @Validated ValidList<ServicePhoneRequest> requestList) {
+        TokenUser user = SecurityUtils.getUserInfo();
+        if (Objects.isNull(user)) {
+            return R.fail("ELECTRICITY.0001", "未找到用户");
+        }
+    
+        if (!(SecurityUtils.isAdmin() || Objects.equals(user.getDataType(), User.DATA_TYPE_OPERATE) || Objects.equals(user.getDataType(), User.DATA_TYPE_FRANCHISEE))) {
+            return R.fail("ELECTRICITY.0066", "用户权限不足");
+        }
+        
         return servicePhoneService.insertOrUpdate(requestList);
     }
     
