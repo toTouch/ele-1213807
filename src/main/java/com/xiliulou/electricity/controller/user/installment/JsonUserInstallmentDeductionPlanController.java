43339466d748a95b2409ab1866eb68fe823bf982
宋进攀 <15927100469@163.com>
2024-12-06 09:36:17 +0800

    modify：分期调整--查询带扣计划分新旧接口

--- src/main/java/com/xiliulou/electricity/controller/user/installment/JsonUserInstallmentDeductionPlanController.java
+++ src/main/java/com/xiliulou/electricity/controller/user/installment/JsonUserInstallmentDeductionPlanController.java
@@ -1,6 +1,7 @@
 package com.xiliulou.electricity.controller.user.installment;
 
 import com.xiliulou.core.web.R;
+import com.xiliulou.electricity.entity.installment.InstallmentDeductionPlan;
 import com.xiliulou.electricity.query.installment.InstallmentDeductionPlanQuery;
 import com.xiliulou.electricity.service.installment.InstallmentDeductionPlanService;
 import com.xiliulou.electricity.vo.installment.InstallmentDeductionPlanAssemblyVO;
@@ -24,13 +25,20 @@ public class JsonUserInstallmentDeductionPlanController {
     
     private InstallmentDeductionPlanService installmentDeductionPlanService;
     
+    @Deprecated
     @GetMapping("/listDeductionPlanForRecord")
+    public R<List<InstallmentDeductionPlan>> listDeductionPlanByAgreementNo(@RequestParam(value = "externalAgreementNo") String externalAgreementNo) {
+        InstallmentDeductionPlanQuery query = new InstallmentDeductionPlanQuery();
+        query.setExternalAgreementNo(externalAgreementNo);
+        
+        return installmentDeductionPlanService.listDeductionPlanByAgreementNo(query);
+    }
+    
+    @GetMapping("/listDeductionPlanForRecordUser")
     public R<List<InstallmentDeductionPlanAssemblyVO>> listDeductionPlanForRecordUser(@RequestParam(value = "externalAgreementNo") String externalAgreementNo) {
         InstallmentDeductionPlanQuery query = new InstallmentDeductionPlanQuery();
         query.setExternalAgreementNo(externalAgreementNo);
         
         return installmentDeductionPlanService.listDeductionPlanForRecordUser(query);
     }
-    
-    
 }
