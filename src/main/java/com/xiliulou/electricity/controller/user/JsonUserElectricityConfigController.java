7ee86e6e258383c3b68aee841a9d60714d3e41b3
renhang <renhang@xiliulou.com>
2025-01-06 13:45:41 +0800

    fix(用户端获取配置): 用户端获取配置

--- src/main/java/com/xiliulou/electricity/controller/user/JsonUserElectricityConfigController.java
+++ src/main/java/com/xiliulou/electricity/controller/user/JsonUserElectricityConfigController.java
@@ -3,14 +3,17 @@ package com.xiliulou.electricity.controller.user;
 import com.xiliulou.core.web.R;
 import com.xiliulou.electricity.service.ElectricityConfigService;
 import com.xiliulou.electricity.tenant.TenantContextHolder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author zzlong
  * @email zhaozhilong@xiliulou.com
  * @date 2023-02-06-15:19
  */
+@RestController
 public class JsonUserElectricityConfigController {
     
     @Autowired
