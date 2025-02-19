1e4c29e1cb4cbd927b5d865e029c2257c5ea80cb
renhang <renhang@xiliulou.com>
2025-02-19 17:21:49 +0800

    feat: 排序字段修改

--- src/main/java/com/xiliulou/electricity/service/impl/installment/InstallmentRecordServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/installment/InstallmentRecordServiceImpl.java
@@ -329,7 +329,7 @@ public class InstallmentRecordServiceImpl implements InstallmentRecordService {
             return R.fail("402045", "用户不存在代扣计划");
         }
 
-        Optional<InstallmentDeductionPlan> optional = deductionPlans.stream().sorted(Comparator.comparing(InstallmentDeductionPlan::getId)).filter(e -> Objects.equals(e.getStatus(), DEDUCTION_PLAN_STATUS_INIT) || Objects.equals(e.getStatus(), DEDUCTION_PLAN_STATUS_FAIL)).findFirst();
+        Optional<InstallmentDeductionPlan> optional = deductionPlans.stream().sorted(Comparator.comparing(InstallmentDeductionPlan::getIssue)).filter(e -> Objects.equals(e.getStatus(), DEDUCTION_PLAN_STATUS_INIT) || Objects.equals(e.getStatus(), DEDUCTION_PLAN_STATUS_FAIL)).findFirst();
 
         if (optional.isPresent()) {
             InstallmentDeductionPlan updatePlan = new InstallmentDeductionPlan();
