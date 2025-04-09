f0e3189fc0bf114f7e92919c8aa74f328679313c
SJP <15927100469@163.com>
2025-04-09 18:01:42 +0800

    fix：套餐冻结增加是否开启租户配置的校验

--- src/main/java/com/xiliulou/electricity/service/impl/car/biz/CarRentalPackageOrderBizServiceImpl.java
+++ src/main/java/com/xiliulou/electricity/service/impl/car/biz/CarRentalPackageOrderBizServiceImpl.java
@@ -2016,6 +2016,10 @@ public class CarRentalPackageOrderBizServiceImpl implements CarRentalPackageOrde
             throw new BizException("300060", "套餐冻结服务，需提前退还租赁的资产，请重新操作");
         }
         
+        if (SystemDefinitionEnum.WX_APPLET.getCode().equals(systemDefinitionEnum.getCode()) && Objects.equals(ElectricityConfig.NOT_DISABLE_MEMBER_CARD, electricityConfig.getDisableMemberCard())) {
+            throw new BizException("100470", "未开启冻结套餐功能");
+        }
+        
         R<Boolean> checkFreezeLimit = carRentalPackageOrderCheckBizService.checkFreezeLimit(tenantId, uid, applyTerm, hasAssets, checkFreezeDays);
         
         if (!checkFreezeLimit.isSuccess()) {
